d78677ef0077e3ff9a08f747b5ac669f06a7f846
Sergey Pepyakin <s.pepyakin@gmail.com>
2018-01-24 21:32:46 +0300

    Fuzz loading/validation against wabt.

---
+++ fuzz/fuzz_targets/load.rs
@@ -0,0 +1,13 @@
+#![no_main]
+#[macro_use] extern crate libfuzzer_sys;
+extern crate wasmi;
+extern crate wabt;
+
+fuzz_target!(|data: &[u8]| {
+    let wasmi_result = wasmi::load_from_buffer(data);
+
+	// TODO: Do validation only!
+	let wabt_result = wabt::wasm2wat(data);
+
+	assert_eq!(wasmi_result.is_ok(), wabt_result.is_ok());
+});
