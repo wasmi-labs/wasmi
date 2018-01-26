1dbb678d3434f7292301ced9db9a785960de5c80
Sergey Pepyakin <s.pepyakin@gmail.com>
2018-01-26 11:53:30 +0300

    Add issue number for TODO

--- fuzz/fuzz_targets/load.rs
+++ fuzz/fuzz_targets/load.rs
@@ -6,7 +6,7 @@ extern crate wabt;
 fuzz_target!(|data: &[u8]| {
     let wasmi_result = wasmi::load_from_buffer(data);
 
-	// TODO: Do validation only!
+	// TODO: Do validation only! https://github.com/pepyakin/wasmi/issues/16
 	let wabt_result = wabt::wasm2wat(data);
 
 	assert_eq!(wasmi_result.is_ok(), wabt_result.is_ok());
