eda488233f49bb7356bed91f932b8c2a93a39171
Sergey Pepyakin <s.pepyakin@gmail.com>
2018-02-05 17:36:53 +0300

    Fix fuzz (#35)
    * Add checking for fuzz.
    
    * Use wabt's validation instead of wasm2wat.
    
    Fixes #16
    Fixes #34
    
    * Check fuzz with nightly.
    
    * Install nightly toolchain
    
    * Travis Driven Development #1
    
    * Travis Driven Development #2

--- fuzz/fuzz_targets/load.rs
+++ fuzz/fuzz_targets/load.rs
@@ -1,13 +1,13 @@
 #![no_main]
-#[macro_use] extern crate libfuzzer_sys;
-extern crate wasmi;
+#[macro_use]
+extern crate libfuzzer_sys;
 extern crate wabt;
+extern crate wasmi;
 
 fuzz_target!(|data: &[u8]| {
-	let wasmi_result = wasmi::load_from_buffer(data);
-
-	// TODO: Do validation only! https://github.com/pepyakin/wasmi/issues/16
-	let wabt_result = wabt::wasm2wat(data);
+	let wasmi_result = wasmi::Module::from_buffer(data);
+	let wabt_result =
+		wabt::Module::read_binary(data, &Default::default()).and_then(|m| m.validate());
 
 	assert_eq!(wasmi_result.is_ok(), wabt_result.is_ok());
 });
