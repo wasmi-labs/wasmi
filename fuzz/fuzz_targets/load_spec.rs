6c40a5b0b6e22b0b94826576f31e0c2a5f84faa9
Sergey Pepyakin <s.pepyakin@gmail.com>
2018-02-16 01:57:40 +0300

    Add fuzzing against spec interpreter.

---
+++ fuzz/fuzz_targets/load_spec.rs
@@ -0,0 +1,37 @@
+#![no_main]
+#[macro_use]
+extern crate libfuzzer_sys;
+extern crate wabt;
+extern crate wasmi;
+extern crate tempdir;
+
+use std::fs::File;
+use std::io::Write;
+use std::process::Command;
+
+fuzz_target!(|data: &[u8]| {
+	let wasmi_result = wasmi::Module::from_buffer(data);
+
+	let temp_dir = tempdir::TempDir::new("spec").unwrap();
+
+	let mut seed_path = temp_dir.path().to_path_buf();
+	seed_path.push("test.wasm");
+
+	{
+		let mut seedfile =
+			File::create(&seed_path).expect("open temporary file for writing to store fuzzer input");
+		seedfile.write_all(data).expect(
+			"write fuzzer input to temporary file",
+		);
+		seedfile.flush().expect(
+			"flush fuzzer input to temporary file before starting wasm-opt",
+		);
+	}
+
+    let wasm_result = Command::new("wasm")
+        .arg(seed_path)
+        .status()
+        .expect("failed to execute `wasm`");
+
+	assert_eq!(wasmi_result.is_ok(), wasm_result.success());
+});
