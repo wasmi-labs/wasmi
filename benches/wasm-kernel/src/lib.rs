ed4645b23a64380f1bf0ae865ed504bfa800bcf3
Sergey Pepyakin <s.pepyakin@gmail.com>
2018-06-18 14:27:12 +0300

    Add rev_complement test
    # Conflicts:
    #	benches/src/lib.rs
    #	benches/wasm-kernel/src/lib.rs

--- benches/wasm-kernel/src/lib.rs
+++ benches/wasm-kernel/src/lib.rs
@@ -8,14 +8,7 @@ extern crate tiny_keccak;
 
 use tiny_keccak::Keccak;
 
-#[no_mangle]
-#[panic_implementation]
-pub fn panic_fmt(_info: &::core::panic::PanicInfo) -> ! {
-	use core::intrinsics;
-	unsafe {
-		intrinsics::abort();
-	}
-}
+mod rev_complement;
 
 pub struct TinyKeccakTestData {
 	data: &'static [u8],
@@ -46,5 +39,49 @@ pub extern "C" fn bench_tiny_keccak(test_data: *const TinyKeccakTestData) {
 		let mut keccak = Keccak::new_keccak256();
 		keccak.update((*test_data).data);
 		keccak.finalize((*test_data).result);
+	}
+}
+
+pub struct RevComplementTestData {
+	input: Box<[u8]>,
+	output: Box<[u8]>,
+}
+
+#[no_mangle]
+pub extern "C" fn prepare_rev_complement(size: usize) -> *mut RevComplementTestData {
+	let input = vec![0; size];
+	let output = vec![0; size];
+
+	let test_data = Box::new(
+		RevComplementTestData {
+			input: input.into_boxed_slice(),
+			output: output.into_boxed_slice(),
+		}
+	);
+
+	// Basically leak the pointer to the test data. This shouldn't be harmful since `prepare` is called
+	// only once per bench run (not for the iteration), and afterwards whole memory instance is discarded.
+	Box::into_raw(test_data)
+}
+
+#[no_mangle]
+pub extern "C" fn rev_complement_input_ptr(test_data: *mut RevComplementTestData) -> *mut u8 {
+	unsafe {
+		(*test_data).input.as_mut_ptr()
+	}
+}
+
+#[no_mangle]
+pub extern "C" fn rev_complement_output_ptr(test_data: *mut RevComplementTestData) -> *const u8 {
+	unsafe {
+		(*test_data).output.as_mut_ptr()
+	}
+}
+
+#[no_mangle]
+pub extern "C" fn bench_rev_complement(test_data: *mut RevComplementTestData) {
+	unsafe {
+		let result = rev_complement::run(&*(*test_data).input);
+		(*test_data).output.copy_from_slice(&result);
 	}
 }
