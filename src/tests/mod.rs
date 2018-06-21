75406dd8ff0551c87bdea67d73b77532f0aa4db3
Leonardo Yvens <leoyvens@gmail.com>
2018-06-21 09:47:49 -0300

    Use transmute instead of casts In RuntimeValue conversion. (#102)
    Casts have arithmetic semantics, and under some build configurations
    Rust will panic when encountering an arithmetic overflow.
    Use a transmute instead since it's what we mean.
    The previous code worked, but still I added a test for good measure.

--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -17,6 +17,19 @@ fn assert_error_properties() {
 	assert_std_err_impl::<Error>();
 }
 
+/// Test that converting an u32 (u64) that does not fit in an i32 (i64)
+/// to a RuntimeValue and back works as expected and the number remains unchanged.
+#[test]
+fn unsigned_to_runtime_value() {
+	use super::RuntimeValue;
+
+	let overflow_i32: u32 = ::std::i32::MAX as u32 + 1;
+	assert_eq!(RuntimeValue::from(overflow_i32).try_into::<u32>().unwrap(), overflow_i32);
+
+	let overflow_i64: u64 = ::std::i64::MAX as u64 + 1;
+	assert_eq!(RuntimeValue::from(overflow_i64).try_into::<u64>().unwrap(), overflow_i64);
+}
+
 pub fn parse_wat(source: &str) -> Module {
 	let wasm_binary = wabt::wat2wasm(source).expect("Failed to parse wat source");
 	Module::from_buffer(wasm_binary).expect("Failed to load parsed module")
