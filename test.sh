7f5c55992ec4ccc8c4a29ad8658e2de769459fa8
Adam Reichold <adam.reichold@rhebo.com>
2019-06-10 10:56:35 +0200

    Add ARMv7 as CI test target

--- test.sh
+++ test.sh
@@ -2,8 +2,14 @@
 
 set -eux
 
+EXTRA_ARGS=""
+
+if [ -n "${TARGET-}" ]; then
+    EXTRA_ARGS="--target=${TARGET} -- --test-threads=1"
+fi
+
 cd $(dirname $0)
 
-time cargo test --all
+time cargo test --all ${EXTRA_ARGS}
 
 cd -
