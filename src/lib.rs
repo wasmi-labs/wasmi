a24175e6ba187f56e8c56486c6d474fc244daf6e
Reuven Podmazo <reuven@enigma.co>
2020-07-22 19:41:55 +0300

    simplified the Error methods a bit

--- src/lib.rs
+++ src/lib.rs
@@ -289,10 +289,7 @@ impl Error {
     pub fn as_host_error(&self) -> Option<&dyn host::HostError> {
         match self {
             Error::Host(host_err) => Some(&**host_err),
-            Error::Trap(trap) => match trap.kind() {
-                TrapKind::Host(host_err) => Some(&**host_err),
-                _ => None,
-            },
+            Error::Trap(Trap { kind: TrapKind::Host(host_err) }) => Some(&**host_err),
             _ => None,
         }
     }
@@ -308,10 +305,7 @@ impl Error {
     pub fn into_host_error(self) -> Option<Box<dyn host::HostError>> {
         match self {
             Error::Host(host_err) => Some(host_err),
-            Error::Trap(trap) => match trap.into_kind() {
-                TrapKind::Host(host_err) => Some(host_err),
-                _ => None,
-            },
+            Error::Trap(Trap { kind: TrapKind::Host(host_err) }) => Some(host_err),
             _ => None,
         }
     }
@@ -327,10 +321,7 @@ impl Error {
     pub fn try_into_host_error(self) -> Result<Box<dyn host::HostError>, Self> {
         match self {
             Error::Host(host_err) => Ok(host_err),
-            Error::Trap(trap) => match trap.into_kind() {
-                TrapKind::Host(host_err) => Ok(host_err),
-                other => Err(Error::Trap(Trap::new(other))),
-            },
+            Error::Trap(Trap { kind: TrapKind::Host(host_err) }) => Ok(host_err),
             other => Err(other),
         }
     }
