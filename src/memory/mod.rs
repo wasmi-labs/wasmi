32133ecc6eb7e3620a6b4507808efe8306a65bd0
Pierre Krieger <pierre.krieger1708@gmail.com>
2021-02-07 20:53:29 +0100

    Rustfmt

--- src/memory/mod.rs
+++ src/memory/mod.rs
@@ -3,7 +3,7 @@ use crate::value::LittleEndianConvert;
 use crate::Error;
 use alloc::{rc::Rc, string::ToString, vec::Vec};
 use core::{
-    cell::{Cell, RefCell, Ref, RefMut},
+    cell::{Cell, Ref, RefCell, RefMut},
     cmp, fmt,
     ops::Range,
     u32,
