1e77db17a8a0e30acbff8f84af2aea6e478779c7
Robin Freyler <robin.freyler@gmail.com>
2022-01-01 11:17:26 +0100

    add TestDescriptor::spanned API for better debugging output

--- tests/spec/v1/descriptor.rs
+++ tests/spec/v1/descriptor.rs
@@ -1,7 +1,10 @@
 #![allow(dead_code)] // TODO: remove
 
 use anyhow::Result;
-use std::fs;
+use std::{
+    fmt::{self, Display},
+    fs,
+};
 
 /// The desciptor of a Wasm spec test suite run.
 #[derive(Debug)]
@@ -40,5 +43,32 @@ impl TestDescriptor {
     /// Returns the contents of the Wasm spec test `.wast` file.
     pub fn file(&self) -> &str {
         &self.file
+    }
+
+    /// Creates a [`TestSpan`] which can be used to print the location within the `.wast` test file.
+    pub fn spanned(&self, span: Span) -> TestSpan {
+        TestSpan {
+            path: self.path(),
+            contents: self.file(),
+            span,
+        }
+    }
+}
+
+/// Useful for printing the location where the `.wast` parse is located.
+#[derive(Debug)]
+pub struct TestSpan<'a> {
+    /// The file path of the `.wast` test.
+    path: &'a str,
+    /// The file contents of the `.wast` test.
+    contents: &'a str,
+    /// The line and column within the `.wast` test file.
+    span: wast::Span,
+}
+
+impl Display for TestSpan<'_> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let (line, col) = span.linecol_in(self.contents);
+        write!(f, "{}:{}:{}", path, line, col)
     }
 }
