268e02b67bd2c021a95b9bf4d2a37b0af04e2ee6
Robin Freyler <robin.freyler@gmail.com>
2022-01-17 21:25:32 +0100

    add plenty of tests for static TypedFunc

--- tests/e2e/v1/func.rs
+++ tests/e2e/v1/func.rs
@@ -13,11 +13,29 @@ fn test_setup() -> Store<()> {
     Store::new(&engine, ())
 }
 
-#[test]
-fn add2_works() {
+/// Asserts that `lhs` and `rhs` tuples are equal.
+///
+/// We need to define the following macro for the comparison of tuples with
+/// 16 elements since `PartialEq` and `Debug` do not seem to be implemented
+/// for tuples of that size.
+macro_rules! assert_eq_tuple {
+    ( $lhs:ident, $rhs:ident; $($n:tt),* $(,)? ) => {
+        $(
+            assert_eq!($lhs.$n, $rhs.$n);
+        )*
+    }
+}
+
+fn setup_add2() -> (Store<()>, Func) {
     let mut store = test_setup();
     // This host function represents a simple binary addition.
     let add2 = Func::wrap(&mut store, |lhs: i32, rhs: i32| lhs + rhs);
+    (store, add2)
+}
+
+#[test]
+fn dynamic_add2_works() {
+    let (mut store, add2) = setup_add2();
     let result_add2 = {
         let mut result = [Value::I32(0)];
         add2.call(&mut store, &[Value::I32(1), Value::I32(2)], &mut result)
@@ -28,10 +46,23 @@ fn add2_works() {
 }
 
 #[test]
-fn add3_works() {
+fn static_add2_works() {
+    let (mut store, add2) = setup_add2();
+    let typed_add2 = add2.typed::<(i32, i32), i32, _>(&mut store).unwrap();
+    let result = typed_add2.call(&mut store, (1, 2)).unwrap();
+    assert_eq!(result, 3);
+}
+
+fn setup_add3() -> (Store<()>, Func) {
     let mut store = test_setup();
     // This host function performance a three-way addition.
     let add3 = Func::wrap(&mut store, |v0: i32, v1: i32, v2: i32| v0 + v1 + v2);
+    (store, add3)
+}
+
+#[test]
+fn dynamic_add3_works() {
+    let (mut store, add3) = setup_add3();
     let result_add3 = {
         let mut result = [Value::I32(0)];
         add3.call(
@@ -46,10 +77,23 @@ fn add3_works() {
 }
 
 #[test]
-fn duplicate_works() {
+fn static_add3_works() {
+    let (mut store, add3) = setup_add3();
+    let typed_add3 = add3.typed::<(i32, i32, i32), i32, _>(&mut store).unwrap();
+    let result = typed_add3.call(&mut store, (1, 2, 3)).unwrap();
+    assert_eq!(result, 6);
+}
+
+fn setup_duplicate() -> (Store<()>, Func) {
     let mut store = test_setup();
     // This host function takes one `i32` argument and returns it twice.
     let duplicate = Func::wrap(&mut store, |value: i32| (value, value));
+    (store, duplicate)
+}
+
+#[test]
+fn dynamic_duplicate_works() {
+    let (mut store, duplicate) = setup_duplicate();
     let result_duplicate = {
         let mut result = [Value::I32(0), Value::I32(0)];
         duplicate
@@ -61,7 +105,14 @@ fn duplicate_works() {
 }
 
 #[test]
-fn many_params_works() {
+fn static_duplicate_works() {
+    let (mut store, duplicate) = setup_duplicate();
+    let typed_duplicate = duplicate.typed::<i32, (i32, i32), _>(&mut store).unwrap();
+    let result = typed_duplicate.call(&mut store, 10).unwrap();
+    assert_eq!(result, (10, 10));
+}
+
+fn setup_many_params() -> (Store<()>, Func) {
     let mut store = test_setup();
     // Function taking 16 arguments (maximum) and doing nothing.
     let func = Func::wrap(
@@ -83,6 +134,38 @@ fn many_params_works() {
          _14: i32,
          _15: i32| (),
     );
+    (store, func)
+}
+
+type I32x16 = (
+    i32,
+    i32,
+    i32,
+    i32,
+    i32,
+    i32,
+    i32,
+    i32,
+    i32,
+    i32,
+    i32,
+    i32,
+    i32,
+    i32,
+    i32,
+    i32,
+);
+
+/// Returns a `(i32, ...)` tuple with 16 elements that have ascending values.
+///
+/// This is required as input or output of many of the following tests.
+fn ascending_tuple() -> I32x16 {
+    (0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15)
+}
+
+#[test]
+fn dynamic_many_params_works() {
+    let (mut store, func) = setup_many_params();
     func.call(
         &mut store,
         &[
@@ -109,41 +192,84 @@ fn many_params_works() {
 }
 
 #[test]
-fn many_results_works() {
+fn static_many_params_works() {
+    let (mut store, func) = setup_many_params();
+    type Inputs = (
+        i32,
+        i32,
+        i32,
+        i32,
+        i32,
+        i32,
+        i32,
+        i32,
+        i32,
+        i32,
+        i32,
+        i32,
+        i32,
+        i32,
+        i32,
+        i32,
+    );
+    let typed_func = func.typed::<Inputs, (), _>(&mut store).unwrap();
+    let inputs = ascending_tuple();
+    let result = typed_func.call(&mut store, inputs);
+    assert_matches!(result, Ok(()));
+}
+
+fn setup_many_results() -> (Store<()>, Func) {
     let mut store = test_setup();
-    // Function taking no arguments and returning 16 results as tuple (maximum).
-    let func = Func::wrap(&mut store, || {
-        (0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15)
-    });
-    let mut results = [Value::I32(0); 16];
-    func.call(&mut store, &[], &mut results).unwrap();
-    assert_eq!(
-        results,
-        [
-            Value::I32(0),
-            Value::I32(1),
-            Value::I32(2),
-            Value::I32(3),
-            Value::I32(4),
-            Value::I32(5),
-            Value::I32(6),
-            Value::I32(7),
-            Value::I32(8),
-            Value::I32(9),
-            Value::I32(10),
-            Value::I32(11),
-            Value::I32(12),
-            Value::I32(13),
-            Value::I32(14),
-            Value::I32(15),
-        ]
-    )
+    // Function taking 16 arguments (maximum) and doing nothing.
+    let func = Func::wrap(&mut store, || ascending_tuple());
+    (store, func)
 }
 
 #[test]
-fn many_params_many_results_works() {
+fn dynamic_many_results_works() {
+    let (mut store, func) = setup_many_results();
+    let mut results = [Value::I32(0); 16];
+    func.call(&mut store, &[], &mut results).unwrap();
+    let mut i = 0;
+    let expected = [0; 16].map(|_| {
+        let value = Value::I32(i as _);
+        i += 1;
+        value
+    });
+    assert_eq!(results, expected)
+}
+
+#[test]
+fn static_many_results_works() {
+    let (mut store, func) = setup_many_results();
+    let typed_func = func
+        .typed::<(), (
+            i32,
+            i32,
+            i32,
+            i32,
+            i32,
+            i32,
+            i32,
+            i32,
+            i32,
+            i32,
+            i32,
+            i32,
+            i32,
+            i32,
+            i32,
+            i32,
+        ), _>(&mut store)
+        .unwrap();
+    let result = typed_func.call(&mut store, ()).unwrap();
+    let expected = ascending_tuple();
+    assert_eq_tuple!(result, expected; 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15);
+}
+
+fn setup_many_params_many_results() -> (Store<()>, Func) {
     let mut store = test_setup();
-    // Function taking no arguments and returning 16 results as tuple (maximum).
+    // Function taking 16 arguments (maximum) and doing nothing.
     let func = Func::wrap(
         &mut store,
         |v0: i32,
@@ -167,6 +293,12 @@ fn many_params_many_results_works() {
             )
         },
     );
+    (store, func)
+}
+
+#[test]
+fn dynamic_many_params_many_results_works() {
+    let (mut store, func) = setup_many_params_many_results();
     let mut results = [Value::I32(0); 16];
     let inputs = [
         Value::I32(0),
@@ -188,6 +320,33 @@ fn many_params_many_results_works() {
     ];
     func.call(&mut store, &inputs, &mut results).unwrap();
     assert_eq!(&results, &inputs)
+}
+
+#[test]
+fn static_many_params_many_results_works() {
+    let (mut store, func) = setup_many_params_many_results();
+    type InOut = (
+        i32,
+        i32,
+        i32,
+        i32,
+        i32,
+        i32,
+        i32,
+        i32,
+        i32,
+        i32,
+        i32,
+        i32,
+        i32,
+        i32,
+        i32,
+        i32,
+    );
+    let typed_func = func.typed::<InOut, InOut, _>(&mut store).unwrap();
+    let inputs = ascending_tuple();
+    let result = typed_func.call(&mut store, inputs).unwrap();
+    assert_eq_tuple!(result, inputs; 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15);
 }
 
 #[test]
