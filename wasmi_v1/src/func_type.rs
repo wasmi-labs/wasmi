85794b685b60ff720e94873d1b6a717e697652ed
Robin Freyler <robin.freyler@gmail.com>
2022-01-24 00:24:05 +0100

    make FuncType::params_results crate private

--- wasmi_v1/src/func_type.rs
+++ wasmi_v1/src/func_type.rs
@@ -46,7 +46,7 @@ impl FuncType {
     }
 
     /// Returns the pair of parameter and result types of the function type.
-    pub fn params_results(&self) -> (&[ValueType], &[ValueType]) {
+    pub(crate) fn params_results(&self) -> (&[ValueType], &[ValueType]) {
         self.params_results.split_at(self.len_params)
     }
 }
