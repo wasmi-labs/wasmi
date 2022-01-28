aa3a55d907cfc189890c95ed58b7ab65cb68c787
Robin Freyler <robin.freyler@gmail.com>
2022-01-28 11:57:14 +0100

    introduce ControlFlowStack data structure and use it in FunctionBuilder

---
+++ wasmi_v1/src/engine/func_builder/control_stack.rs
@@ -0,0 +1,73 @@
+use super::ControlFrame;
+
+/// The stack of control flow frames.
+#[derive(Debug, Default)]
+pub struct ControlFlowStack {
+    frames: Vec<ControlFrame>,
+}
+
+impl ControlFlowStack {
+    /// Returns the current depth of the stack of the [`ControlFlowStack`].
+    pub fn len(&self) -> usize {
+        self.frames.len()
+    }
+
+    /// Pushes a new control flow frame to the [`ControlFlowStack`].
+    pub fn push_frame(&mut self, frame: ControlFrame) {
+        self.frames.push(frame)
+    }
+
+    /// Pops the last control flow frame from the [`ControlFlowStack`].
+    ///
+    /// # Panics
+    ///
+    /// If the [`ControlFlowStack`] is empty.
+    pub fn pop_frame(&mut self) -> ControlFrame {
+        self.frames
+            .pop()
+            .expect("tried to pop control flow frame from empty control flow stack")
+    }
+
+    /// Returns the first control flow frame on the control stack.
+    ///
+    /// # Note
+    ///
+    /// Usually the first control flow frame is the control flow block of the
+    /// function body itself. Branching to it is equal to returning from the
+    /// function following WebAssembly semantics.
+    pub fn first(&self) -> &ControlFrame {
+        self.frames.first().expect(
+            "tried to return the first control flow \
+                frame from empty control flow stack",
+        )
+    }
+
+    /// Returns the last control flow frame on the control stack.
+    pub fn last_mut(&mut self) -> &mut ControlFrame {
+        self.frames.last_mut().expect(
+            "tried to exclusively peek the last control flow \
+            frame from an empty control flow stack",
+        )
+    }
+
+    /// Returns an exclusive reference to the control flow frame at the given `depth`.
+    ///
+    /// A `depth` of 0 is equal to calling [`ControlFlowStack::last_mut`].
+    ///
+    /// # Panics
+    ///
+    /// If `depth` exceeds the length of the stack of control flow frames.
+    pub fn nth_back_mut(&mut self, depth: u32) -> &mut ControlFrame {
+        let len = self.len();
+        self.frames
+            .iter_mut()
+            .nth_back(depth as usize)
+            .unwrap_or_else(|| {
+                panic!(
+                    "tried to peek the {}-th control flow frame \
+                    but there are only {} control flow frames",
+                    depth, len
+                )
+            })
+    }
+}
--- wasmi_v1/src/engine/func_builder/mod.rs
+++ wasmi_v1/src/engine/func_builder/mod.rs
@@ -1,6 +1,7 @@
 mod control_frame;
+mod control_stack;
 
-use self::control_frame::ControlFrame;
+use self::{control_frame::ControlFrame, control_stack::ControlFlowStack};
 use crate::{
     module2::{BlockType, FuncIdx, ModuleResources},
     Engine,
@@ -17,7 +18,7 @@ pub struct FunctionBuilder<'engine, 'parser> {
     /// The immutable `wasmi` module resources.
     res: ModuleResources<'parser>,
     /// The control flow frame stack that represents the Wasm control flow.
-    control_frames: Vec<ControlFrame>,
+    control_frames: ControlFlowStack,
     /// The amount of local variables of the currently compiled function.
     len_locals: usize,
     /// The maximum stack height of the translated Wasm function.
@@ -45,7 +46,7 @@ impl<'engine, 'parser> FunctionBuilder<'engine, 'parser> {
             engine,
             func,
             res,
-            control_frames: Vec::new(),
+            control_frames: ControlFlowStack::default(),
             len_locals: 0,
             max_stack_height: 0,
             reachable: true,
