7c6d98f46de9ce93876ed4ad46ce60c2c7439426
Robin Freyler <robin.freyler@gmail.com>
2022-06-29 11:21:48 +0200

    further simplify execution test code

--- wasmi_v1/src/engine/inner/execute/tests/mod.rs
+++ wasmi_v1/src/engine/inner/execute/tests/mod.rs
@@ -9,7 +9,7 @@ use self::utils::{
     load_wasm_from_file,
     wat2wasm,
 };
-use crate::Extern;
+use crate::{Extern, Func, Instance, Store};
 use assert_matches::assert_matches;
 use wasmi_core::Value;
 
@@ -19,13 +19,18 @@ macro_rules! load_test_instance {
     }};
 }
 
+/// Loads the exported function with the given `func_name`.
+fn load_func(store: &Store<()>, instance: &Instance, func_name: &str) -> Func {
+    instance
+        .get_export(&store, func_name)
+        .and_then(Extern::into_func)
+        .unwrap()
+}
+
 #[test]
 fn test_add() {
     let (mut store, instance) = load_test_instance!("wat/add.wat");
-    let add = instance
-        .get_export(&store, "add")
-        .and_then(Extern::into_func)
-        .unwrap();
+    let add = load_func(&store, &instance, "add");
     let mut result = [Value::I32(0)];
     add.call(&mut store, &[Value::I32(1), Value::I32(2)], &mut result)
         .unwrap();
@@ -35,12 +40,9 @@ fn test_add() {
 #[test]
 fn test_swap() {
     let (mut store, instance) = load_test_instance!("wat/swap.wat");
-    let add = instance
-        .get_export(&store, "swap")
-        .and_then(Extern::into_func)
-        .unwrap();
+    let swap = load_func(&store, &instance, "swap");
     let mut result = [Value::I32(0), Value::I32(0)];
-    add.call(&mut store, &[Value::I32(1), Value::I32(2)], &mut result)
+    swap.call(&mut store, &[Value::I32(1), Value::I32(2)], &mut result)
         .unwrap();
     assert_matches!(result, [Value::I32(2), Value::I32(1)]);
 }
