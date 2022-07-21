2e2f92cd26b8f05ded7ffbd92ff922d8dbec3eff
Robin Freyler <robin.freyler@gmail.com>
2022-07-21 11:06:23 +0200

    add intermediate fish script CI

---
+++ ci.sh
@@ -0,0 +1,13 @@
+echo "starting local CI ..."
+echo "    Formatting ..."
+cargo +nightly fmt;
+echo "    Building ..."
+cargo build --workspace;
+echo "    Clippy ..."
+cargo clippy --workspace -- -D warnings;
+echo "    Docs ..."
+cargo doc --workspace --all-features --no-deps --document-private-items;
+echo "    Testing Spec ..."
+cargo test --release --quiet spec::v1;
+echo "    Testing Package ..."
+cargo test --package wasmi_v1 --quiet;
