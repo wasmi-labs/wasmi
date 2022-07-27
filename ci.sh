2bcd0c43867532f0ce79a8293d96d11d6efc8149
Robin Freyler <robin.freyler@gmail.com>
2022-07-27 13:27:53 +0200

    improve ci.sh script

--- ci.sh
+++ ci.sh
@@ -1,15 +1,19 @@
-echo "starting local CI ..."
-echo "    Formatting ..."
-cargo +nightly fmt;
-echo "    Building ..."
-cargo build --workspace;
-echo "    Building no_std ..."
-cargo build --workspace --exclude wasmi_cli --no-default-features --target thumbv7em-none-eabi
-echo "    Clippy ..."
-cargo clippy --workspace -- -D warnings;
-echo "    Docs ..."
-cargo doc --workspace --all-features --no-deps --document-private-items;
-echo "    Testing Spec ..."
-cargo test --release --quiet;
-echo "    Testing Package ..."
-cargo test --package wasmi --quiet;
+#!/bin/bash
+
+echo "starting local CI ..." &&
+echo "    Formatting ..." &&
+cargo +nightly fmt &&
+echo "    Building ..." &&
+cargo build --workspace &&
+echo "    Building no_std ..." &&
+cargo build --workspace --exclude wasmi_cli --no-default-features --target thumbv7em-none-eabi &&
+echo "    Clippy ..." &&
+cargo clippy --workspace -- -D warnings &&
+echo "    Docs ..." &&
+cargo doc --workspace --all-features --no-deps --document-private-items &&
+echo "    Testing Spec ..." &&
+cargo test --release --quiet &&
+echo "    Testing Package ..." &&
+cargo test --package wasmi --quiet &&
+echo "-------------------------" &&
+echo "CI passed"
