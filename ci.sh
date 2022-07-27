d7a6a9c0320fede1d7b34a841c51a95213a94541
Robin Freyler <robin.freyler@gmail.com>
2022-07-27 13:28:51 +0200

    make ci.sh use fixed toolchains (nightly and stable)

--- ci.sh
+++ ci.sh
@@ -4,16 +4,16 @@ echo "starting local CI ..." &&
 echo "    Formatting ..." &&
 cargo +nightly fmt &&
 echo "    Building ..." &&
-cargo build --workspace &&
+cargo +stable build --workspace &&
 echo "    Building no_std ..." &&
-cargo build --workspace --exclude wasmi_cli --no-default-features --target thumbv7em-none-eabi &&
+cargo +stable build --workspace --exclude wasmi_cli --no-default-features --target thumbv7em-none-eabi &&
 echo "    Clippy ..." &&
-cargo clippy --workspace -- -D warnings &&
+cargo +stable clippy --workspace -- -D warnings &&
 echo "    Docs ..." &&
-cargo doc --workspace --all-features --no-deps --document-private-items &&
+cargo +stable doc --workspace --all-features --no-deps --document-private-items &&
 echo "    Testing Spec ..." &&
-cargo test --release --quiet &&
+cargo +stable test --release --quiet &&
 echo "    Testing Package ..." &&
-cargo test --package wasmi --quiet &&
+cargo +stable test --package wasmi --quiet &&
 echo "-------------------------" &&
 echo "CI passed"
