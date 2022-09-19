f9150b29a22529fee3d52e1fef1c9fc55362f810
yjhmelody <yjh465402634@gmail.com>
2022-09-19 10:51:10 +0800

    revert some phantomData types

--- wasmi_v1/src/linker.rs
+++ wasmi_v1/src/linker.rs
@@ -239,7 +239,7 @@ pub struct Linker<T> {
     /// Helps to avoid heap memory allocations at the cost of a small
     /// memory overhead.
     externals: Vec<Extern>,
-    marker: PhantomData<T>,
+    marker: PhantomData<fn() -> T>,
 }
 
 impl<T> Debug for Linker<T> {
