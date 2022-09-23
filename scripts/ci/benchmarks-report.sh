78c93be19510366271c7ed73bf6dd7ddadddf256
Sergejs Kostjucenko <85877331+sergejparity@users.noreply.github.com>
2022-09-23 12:12:22 +0300

    CI: refactor benchmarking pipeline (#464)
    * refactor benchmark ci
    
    * add cargo-criterion installation
    
    * improve formatting
    
    * improve formatting and add comments
    
    * add description to jq filter
    
    * fix wording
    
    * change temp file location
    
    * align time columns right

--- scripts/ci/benchmarks-report.sh
+++ scripts/ci/benchmarks-report.sh
@@ -1,63 +1,18 @@
 #!/bin/bash
 
-# Takes raw reports from
-# "cargo bench --bench benches -- --noplot --save-baseline master" output as 1st argument
-# "cargo bench --bench benches -- --noplot --baseline master" output as 2nd argument
-# Parses them to json and posts formatted results to PR on a GitHub as a comment
+# This script takes as an argument benchmark report JSON file produced
+# by the command "cargo criterion --message-format=json". Executed first
+# against a 'master' branch and on a PR commit afterwards.
+# Formats it using 'jq' with filters defined in the './scripts/ci/benchmark-filter.jq'.
+# And posts formatted results to a PR on a GitHub as an issue comment.
+
 set -eu
 set -o pipefail
 
 PR_COMMENTS_URL="https://api.github.com/repos/paritytech/wasmi/issues/${CI_COMMIT_BRANCH}/comments"
 
-# master report to json
-echo "PARSING MASTER REPORT"
-sed -e 's/^Found.*//g' \
-    -e 's/^\s\+[[:digit:]].*$//g' \
-    -e 's/\//_/g' \
-    -e 's/^[a-z0-9_]\+/"&": {/g' \
-    -e 's/time:\s\+\[.\{10\}/"time": "/g' \
-    -e 's/.\{10\}\]/"},/g' \
-    -e '1s/^/{\n/g' \
-    -e '/^$/d' \
-    -e 's/  */ /g' \
-    -e 's/^ *\(.*\) *$/\1/' $1 \
-    | sed -z 's/.$//' \
-    | sed -e '$s/.$/}/g' \
-    | tee target/criterion/output_master.json
-
-# PR report to json
-sed -e 's/^Found.*//g' \
-    -e 's/^\s\+[[:digit:]].*//g' \
-    -e 's/\//_/g' \
-    -e 's/^[a-z0-9_]\+/"&": {/g' \
-    -e 's/time:\s\+\[.\{10\}/"time": "/g' \
-    -e 's/.\{10\}\]$/",/g' \
-    -e 's/change:\s.\{10\}/"change":"/g' \
-    -e 's/\s[-+].*$/",/g' \
-    -e 's/\(No\|Ch\).*$/"perf_change":":white_circle:"},/' \
-    -e 's/Performance has regressed./"perf_change":":red_circle:"},/' \
-    -e 's/Performance has improved./"perf_change":":green_circle:"},/' \
-    -e '1s/^/{\n/g' \
-    -e '/^$/d' \
-    -e 's/  */ /g' \
-    -e 's/^ *\(.*\) *$/\1/' $2 \
-    | sed -z 's/.$//' \
-    | sed -e '$s/.$/}/g' \
-    | tee target/criterion/output_pr.json
-
-cd target/criterion
-
-# Prepare report table
-for d in */; do
-    d=${d::-1}
-    echo -n "| \`${d}\` "\
-         "| $(cat output_master.json | jq .${d}.time | tr -d '"') "\
-         "| $(cat output_pr.json | jq .${d}.time | tr -d '"') "\
-         "| $(cat output_pr.json | jq .${d}.perf_change | tr -d '"') "\
-         "$(cat output_pr.json | jq .${d}.change | tr -d '"') |\n" >> bench-final-report.txt
-done
-
-RESULT=$(cat bench-final-report.txt)
+# Format benchmarks into a table
+RESULT=$(jq -s -f ../scripts/ci/benchmark-filter.jq $1 | tr -d '"' | tr -d '\n')
 
 # Check whether comment from paritytech-cicd-pr already exists
 EXISTING_COMMENT_URL=$(curl --silent $PR_COMMENTS_URL \
@@ -77,7 +32,7 @@ fi
 echo "Comment will be posted here $PR_COMMENTS_URL"
 
 # POST/PATCH comment to the PR
-curl -X ${REQUEST_TYPE} ${PR_COMMENTS_URL} -v \
+curl -X ${REQUEST_TYPE} ${PR_COMMENTS_URL} \
     -H "Cookie: logged_in=no" \
     -H "Authorization: token ${GITHUB_PR_TOKEN}" \
     -H "Content-Type: application/json; charset=utf-8" \
@@ -85,7 +40,7 @@ curl -X ${REQUEST_TYPE} ${PR_COMMENTS_URL} -v \
 \"body\": \
 \"## CRITERION BENCHMARKS ## \n\n \
 |BENCHMARK|MASTER|PR|Diff|\n \
-|---|---|---|---|\n \
+|---|---:|---:|---|\n \
 ${RESULT}\n\n \
 [Link to pipeline](${CI_JOB_URL}) \" \
 }"
