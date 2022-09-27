6d6037059d365f7c754b9010a14e6d10e3e42a63
Robin Freyler <robin.freyler@gmail.com>
2022-09-27 23:30:58 +0200

    add back proper size_of_instruction unit test

--- crates/wasmi/src/engine/bytecode/tests.rs
+++ crates/wasmi/src/engine/bytecode/tests.rs
@@ -2,5 +2,8 @@ use super::*;
 
 #[test]
 fn size_of_instruction() {
+    assert_eq!(core::mem::size_of::<Instruction>(), 16);
     assert_eq!(core::mem::size_of::<DropKeep>(), 4);
+    assert_eq!(core::mem::size_of::<BranchParams>(), 8);
+    assert_eq!(core::mem::size_of::<BranchOffset>(), 4);
 }
