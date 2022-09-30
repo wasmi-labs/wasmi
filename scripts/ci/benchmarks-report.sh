90d4e746e42fb00cb32e6fcf6df6db418d258af3
Sergejs Kostjucenko <sergejs@parity.io>
2022-09-30 16:54:03 +0300

    fix error

--- scripts/ci/benchmarks-report.sh
+++ scripts/ci/benchmarks-report.sh
@@ -47,6 +47,8 @@ pushd ./target/ci/criterion
 
 # Format benchmarks details into a table
 RESULT=$(for d in */; do
+            BENCH_ID=$(jq .full_id ${d}master/benchmark.json | tr -d '"')
+
             MASTER_TIME=$(jq .slope.point_estimate ${d}master/estimates.json)
             PR_TIME=$(jq .slope.point_estimate ${d}new/estimates.json)
             PERF_CHANGE=$(get_performance_change_status "$(grep -A 3 -e $(echo "${d::-1}" | tr "_" ".") ../bench-pr)")
@@ -59,7 +61,7 @@ RESULT=$(for d in */; do
 
             WT_OVERHEAD=$(echo "($WASM_PR_TIME-$PR_TIME)/$PR_TIME*100" | bc -l | xargs printf "%.0f")
 
-            echo -n "<tr><td nowrap><tt>$(jq .full_id ${d}master/benchmark.json)<\/td>"\
+            echo -n "<tr><td nowrap><tt>$BENCH_ID<\/td>"\
                 "<td nowrap> $(format_time $MASTER_TIME)<\/td>" \
                 "<td nowrap> $(format_time $PR_TIME)<\/td>" \
                 "<td nowrap> $PERF_CHANGE $(echo $DIFF*100 | bc -l | xargs printf "%.2f%%")<\/td>" \
