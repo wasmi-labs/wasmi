bb5f1446bd4696f5d7ff0c2a6d6e6592974fcdf0
Sergejs Kostjucenko <85877331+sergejparity@users.noreply.github.com>
2022-09-30 10:47:31 +0300

    CI: Add Wasmtime based Wasm benchmark automation (#483)
    * add wasmtime benchmark
    
    * fix ci
    
    * disable criterion default-features
    
    * temp fix
    
    * temp fix
    
    * fix typo
    
    * fix report script
    
    * fix report formatting
    
    * fix report formatting
    
    * fix report formatting
    
    * format table as html
    
    * fix format table as html
    
    * fix format table as html
    
    * debug
    
    * debug
    
    * fix syntax
    
    * add performance change indicator
    
    * change column name
    
    * add comments and cleanup
    
    * remove temporary test ref

--- scripts/ci/benchmarks-report.sh
+++ scripts/ci/benchmarks-report.sh
@@ -1,18 +1,61 @@
 #!/bin/bash
 
-# This script takes as an argument benchmark report JSON file produced
-# by the command "cargo criterion --message-format=json". Executed first
-# against a 'master' branch and on a PR commit afterwards.
-# Formats it using 'jq' with filters defined in the './scripts/ci/benchmark-filter.jq'.
-# And posts formatted results to a PR on a GitHub as an issue comment.
+# This script prepares table with results of benchmarks and posts it to the
+# GitHub's PR as an issue comment.
 
 set -eu
 set -o pipefail
 
+# Transform timing details into more readable format
+function format_time {
+    if (( $(echo $1'<'1000 | bc -l) ))
+      then printf "%.2f ns" $1
+    elif (( $(echo $1'<'1000000 | bc -l) ))
+      then printf "%.2f µs" $(echo $1/1000 | bc -l )
+    else
+      printf "%.2f ms" $(echo $1/1000000 | bc -l )
+    fi
+}
+
+# Derive performance change status from benchmarks raw command prompt log
+function get_performance_change_status {
+    if echo $1 | grep -e "Performance has regressed" >> /dev/null
+      then echo ":red_circle:"
+    elif echo $1 | grep -e "Performance has improved" >> /dev/null
+      then echo ":green_circle:"
+    elif echo $1 | grep -e "No change in performance detected" >> /dev/null
+      then echo ":white_circle:"
+    elif echo $1 | grep -e "Change within noise threshold" >> /dev/null
+      then echo ":white_circle:"
+    fi
+}
+
 PR_COMMENTS_URL="https://api.github.com/repos/paritytech/wasmi/issues/${CI_COMMIT_BRANCH}/comments"
 
-# Format benchmarks into a table
-RESULT=$(jq -s -f ./scripts/ci/benchmark-filter.jq $1 | tr -d '"' | tr -d '\n')
+pushd ./target/ci/criterion
+
+# Format benchmarks details into a table
+RESULT=$(for d in */; do
+            MASTER_TIME=$(jq .slope.point_estimate ${d}master/estimates.json)
+            PR_TIME=$(jq .slope.point_estimate ${d}new/estimates.json)
+            PERF_CHANGE=$(get_performance_change_status "$(grep -A 3 -e $(echo "${d::-1}" | tr "_" ".") ../bench-pr)")
+            DIFF=$(jq .mean.point_estimate ${d}change/estimates.json)
+
+            WASM_MASTER_TIME=$(jq .slope.point_estimate ../wasmtime-criterion/${d}master-wasm/estimates.json)
+            WASM_PR_TIME=$(jq .slope.point_estimate ../wasmtime-criterion/${d}new/estimates.json)
+            WASM_PERF_CHANGE=$(get_performance_change_status "$(grep -A 3 -e $(echo "${d::-1}" | tr "_" ".") ../wasmtime-pr)")
+            WASM_DIFF=$(jq .mean.point_estimate ../wasmtime-criterion/${d}change/estimates.json)
+
+            echo -n "<tr><td><tt>${d::-1}<\/td>"\
+                "<td> $(format_time $MASTER_TIME)<\/td>" \
+                "<td> $(format_time $PR_TIME)<\/td>" \
+                "<td> $PERF_CHANGE $(echo $DIFF*100 | bc -l | xargs printf "%.2f") %<\/td>" \
+                "<td> $(format_time $WASM_MASTER_TIME)<\/td>" \
+                "<td> $(format_time $WASM_PR_TIME)<\/td>" \
+                "<td> $WASM_PERF_CHANGE $(echo $WASM_DIFF*100 | bc -l | xargs printf "%.2f") %<\/td><\/tr>"
+        done)
+
+popd
 
 # Check whether comment from paritytech-cicd-pr already exists
 EXISTING_COMMENT_URL=$(curl --silent $PR_COMMENTS_URL \
@@ -38,9 +81,19 @@ curl -X ${REQUEST_TYPE} ${PR_COMMENTS_URL} \
     -H "Content-Type: application/json; charset=utf-8" \
     -d $"{ \
 \"body\": \
-\"## CRITERION BENCHMARKS ## \n\n \
-|BENCHMARK|MASTER|PR|Diff|\n \
-|---|---:|---:|---|\n \
-${RESULT}\n\n \
+\"## BENCHMARKS ## \n\n \
+<table> \
+<thead> \
+<tr> \
+<th><\/th><th colspan=3>NATIVE<\/th><th colspan=3>WASMTIME<\/th> \
+<\/tr> \
+<tr> \
+<th>BENCHMARK<\/th><th>MASTER<\/th><th>PR<\/th><th>DIFF<\/th><th>MASTER<\/th><th>PR<\/th><th>DIFF<\/th> \
+<\/tr> \
+<\/thead> \
+<tbody> \
+${RESULT} \
+<\/tbody> \
+<\/table> \n\n \
 [Link to pipeline](${CI_JOB_URL}) \" \
 }"
