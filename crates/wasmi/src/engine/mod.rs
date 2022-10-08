4a4b815892721268cd7eecb31962dcf555fa5494
Robin Freyler <robin.freyler@gmail.com>
2022-10-08 21:28:39 +0200

    move arena definitions into their own crate: wasmi_arena

--- crates/wasmi/src/engine/mod.rs
+++ crates/wasmi/src/engine/mod.rs
@@ -36,7 +36,6 @@ use self::{
 };
 use super::{func::FuncEntityInternal, AsContextMut, Func};
 use crate::{
-    arena::{GuardedEntity, Index},
     core::{Trap, TrapCode},
     FuncType,
 };
@@ -44,6 +43,7 @@ use alloc::sync::Arc;
 use core::sync::atomic::{AtomicU32, Ordering};
 pub use func_types::DedupFuncType;
 use spin::mutex::Mutex;
+use wasmi_arena::{GuardedEntity, Index};
 
 /// The outcome of a `wasmi` function execution.
 #[derive(Debug, Copy, Clone)]
