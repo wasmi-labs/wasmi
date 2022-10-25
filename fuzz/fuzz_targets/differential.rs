7357ed510b826ea77d72f6fb9af01aaaa54dd71c
Robin Freyler <robin.freyler@gmail.com>
2022-10-25 13:43:26 +0200

    make initial differential fuzz testing work for wasmi

--- fuzz/fuzz_targets/differential.rs
+++ fuzz/fuzz_targets/differential.rs
@@ -1,18 +1,117 @@
 #![no_main]
-use libfuzzer_sys::fuzz_target;
+use libfuzzer_sys::{
+    arbitrary::{Result, Unstructured},
+    fuzz_target,
+    Corpus,
+};
 use wasmi::{
     core::{Trap, TrapCode, Value},
     Engine,
-    Error,
     Instance,
     Linker,
     Module,
     Store,
 };
 
-fuzz_target!(|module: wasm_smith::Module| {
-    let _ = execute_wasmi(&module);
+fuzz_target!(|bytes: &[u8]| -> Corpus {
+    let mut unstructured = Unstructured::new(bytes);
+    let mut fuzzy_module = match wasm_smith::Module::new(FuzzConfig, &mut unstructured) {
+        Ok(module) => module,
+        Err(_) => return Corpus::Reject,
+    };
+    let _fuel_global = fuzzy_module.ensure_termination(100_000);
+    let _ = execute_wasmi(&fuzzy_module);
+    Corpus::Keep
 });
+
+#[derive(Debug)]
+pub struct FuzzConfig;
+
+impl wasm_smith::Config for FuzzConfig {
+    fn min_memories(&self) -> u32 {
+        0
+    }
+    /// `wasmi` cannot handle more than one linear memory per Wasm module.
+    fn max_memories(&self) -> usize {
+        1
+    }
+    fn min_tables(&self) -> u32 {
+        0
+    }
+    /// `wasmi` cannot handle more than one table per Wasm module.
+    fn max_tables(&self) -> usize {
+        1
+    }
+    /// We generate at least one function so that there is always something to execute.
+    fn min_funcs(&self) -> usize {
+        1
+    }
+    /// We restrict to at most 32 pages of each 2^16 bytes so that
+    /// the fuzzer does not accidentally run out of memory.
+    ///
+    /// 32 pages will provide a maximum of 2MB of memory per Wasm instance.
+    fn max_memory_pages(&self, _is_64: bool) -> u64 {
+        32
+    }
+    /// We export everything so that we can access it from the outside fuzzer.
+    fn export_everything(&self) -> bool {
+        true
+    }
+    /// Required for fuzzing so that multiple Wasm engines produce the same results reliably.
+    fn canonicalize_nans(&self) -> bool {
+        true
+    }
+    /// We require maximum memory sizes so that the fuzzer does not accidentally
+    /// run out of memory.
+    fn memory_max_size_required(&self) -> bool {
+        true
+    }
+    /// We require maximum table sizes so that the fuzzer does not accidentally
+    /// run out of memory.
+    fn table_max_size_required(&self) -> bool {
+        true
+    }
+    /// `wasmi` support the Wasm `multi-value` proposal.
+    fn multi_value_enabled(&self) -> bool {
+        true
+    }
+    /// `wasmi` support the Wasm `saturating-float-to-int` proposal.
+    fn saturating_float_to_int_enabled(&self) -> bool {
+        true
+    }
+    /// `wasmi` support the Wasm `sign-extension` proposal.
+    fn sign_extension_ops_enabled(&self) -> bool {
+        true
+    }
+    /// `wasmi` does not yet support the Wasm `bulk-memory` proposal.
+    fn bulk_memory_enabled(&self) -> bool {
+        false
+    }
+    /// `wasmi` does not yet support the Wasm `reference-types` proposal.
+    fn reference_types_enabled(&self) -> bool {
+        false
+    }
+    /// `wasmi` does not yet support the Wasm `simd` proposal.
+    fn simd_enabled(&self) -> bool {
+        false
+    }
+    /// `wasmi` does not yet support the Wasm `relaxed-simd` proposal.
+    fn relaxed_simd_enabled(&self) -> bool {
+        false
+    }
+    /// `wasmi` does not yet support the Wasm `expections` proposal.
+    fn exceptions_enabled(&self) -> bool {
+        false
+    }
+    /// `wasmi` does not yet support the Wasm `memory64` proposal.
+    fn memory64_enabled(&self) -> bool {
+        false
+    }
+    /// `wasmi` does not yet support the Wasm `treads` proposal.
+    fn threads_enabled(&self) -> bool {
+        false
+    }
+}
 
 /// The execution state for the `wasmi` engine.
 ///
@@ -21,6 +120,7 @@ fuzz_target!(|module: wasm_smith::Module| {
 /// This state is the result of a `wasmi` module invokation and is
 /// compared to the other Wasm engines in differential fuzz testing.
 #[derive(Debug)]
+#[allow(dead_code)] // TODO: remove
 pub struct WasmiState {
     /// The Wasm store holding all the Wasm state.
     store: Store<()>,
@@ -37,6 +137,7 @@ pub struct WasmiState {
 
 /// A function call outcome.
 #[derive(Debug)]
+#[allow(dead_code)] // TODO: remove
 pub struct CallOutcome {
     /// The function name of the call.
     name: String,
@@ -46,6 +147,7 @@ pub struct CallOutcome {
 
 /// The results of a valid call.
 #[derive(Debug)]
+#[allow(dead_code)] // TODO: remove
 pub struct CallResults {
     results: Vec<Value>,
 }
@@ -100,8 +202,8 @@ impl From<TrapCode> for CallError {
 /// - If the Wasm module fails to instantiate.
 /// - If the execution of any of the Wasm functions trap.
 #[allow(unused)]
-fn execute_wasmi(module: &wasm_smith::Module) -> Result<WasmiState, Error> {
-    let wasm_bytes = module.to_bytes();
+fn execute_wasmi(fuzzy_module: &wasm_smith::Module) -> Result<WasmiState, CallError> {
+    let wasm_bytes = fuzzy_module.to_bytes();
     let engine = Engine::default();
     let module = Module::new(&engine, &mut &wasm_bytes[..])?;
     let mut store = Store::new(&engine, ());
