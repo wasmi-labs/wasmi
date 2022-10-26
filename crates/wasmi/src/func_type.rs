d643a789ac97fe02f9d258efe94e9e8eb2a04ba9
Robin Freyler <robin.freyler@gmail.com>
2022-10-26 10:18:22 +0200

    cleanup FuncType display tests

--- crates/wasmi/src/func_type.rs
+++ crates/wasmi/src/func_type.rs
@@ -91,72 +91,85 @@ impl FuncType {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use core::borrow::Borrow;
+
+    fn assert_func_type(func_type: impl Borrow<FuncType>, expected: &str) {
+        assert_eq!(format!("{}", func_type.borrow()), String::from(expected),);
+    }
 
     #[test]
     fn display_0in_0out() {
-        let func_type = FuncType::new([], []);
-        assert_eq!(format!("{func_type}"), String::from("fn()"),);
+        assert_func_type(FuncType::new([], []), "fn()");
+    }
+
+    #[test]
+    fn display_1in_0out() {
+        assert_func_type(FuncType::new([ValueType::I32], []), "fn(i32)");
+    }
+
+    #[test]
+    fn display_0in_1out() {
+        assert_func_type(FuncType::new([], [ValueType::I32]), "fn() -> i32");
     }
 
     #[test]
     fn display_1in_1out() {
-        let func_type = FuncType::new([ValueType::I32], [ValueType::I32]);
-        assert_eq!(format!("{func_type}"), String::from("fn(i32) -> i32"),);
+        assert_func_type(
+            FuncType::new([ValueType::I32], [ValueType::I32]),
+            "fn(i32) -> i32",
+        );
     }
 
     #[test]
     fn display_4in_0out() {
-        let func_type = FuncType::new(
-            [
-                ValueType::I32,
-                ValueType::I64,
-                ValueType::F32,
-                ValueType::F64,
-            ],
-            [],
-        );
-        assert_eq!(
-            format!("{func_type}"),
-            String::from("fn(i32, i64, f32, f64)"),
+        assert_func_type(
+            FuncType::new(
+                [
+                    ValueType::I32,
+                    ValueType::I64,
+                    ValueType::F32,
+                    ValueType::F64,
+                ],
+                [],
+            ),
+            "fn(i32, i64, f32, f64)",
         );
     }
 
     #[test]
     fn display_0in_4out() {
-        let func_type = FuncType::new(
-            [],
-            [
-                ValueType::I32,
-                ValueType::I64,
-                ValueType::F32,
-                ValueType::F64,
-            ],
-        );
-        assert_eq!(
-            format!("{func_type}"),
-            String::from("fn() -> (i32, i64, f32, f64)"),
+        assert_func_type(
+            FuncType::new(
+                [],
+                [
+                    ValueType::I32,
+                    ValueType::I64,
+                    ValueType::F32,
+                    ValueType::F64,
+                ],
+            ),
+            "fn() -> (i32, i64, f32, f64)",
         );
     }
 
     #[test]
     fn display_4in_4out() {
-        let func_type = FuncType::new(
-            [
-                ValueType::I32,
-                ValueType::I64,
-                ValueType::F32,
-                ValueType::F64,
-            ],
-            [
-                ValueType::I32,
-                ValueType::I64,
-                ValueType::F32,
-                ValueType::F64,
-            ],
-        );
-        assert_eq!(
-            format!("{func_type}"),
-            String::from("fn(i32, i64, f32, f64) -> (i32, i64, f32, f64)"),
+        assert_func_type(
+            FuncType::new(
+                [
+                    ValueType::I32,
+                    ValueType::I64,
+                    ValueType::F32,
+                    ValueType::F64,
+                ],
+                [
+                    ValueType::I32,
+                    ValueType::I64,
+                    ValueType::F32,
+                    ValueType::F64,
+                ],
+            ),
+            "fn(i32, i64, f32, f64) -> (i32, i64, f32, f64)",
         );
     }
 }
