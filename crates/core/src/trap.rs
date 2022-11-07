2777152d58758627956cc981005e85fbcebf2786
Robin Freyler <robin.freyler@gmail.com>
2022-11-07 12:44:08 +0100

    apply clippy suggestion

--- crates/core/src/trap.rs
+++ crates/core/src/trap.rs
@@ -99,7 +99,7 @@ impl Trap {
     {
         self.reason
             .as_host()
-            .and_then(|reason| reason.downcast_ref::<T>())
+            .and_then(<(dyn HostError + 'static)>::downcast_ref)
     }
 
     /// Creates a new `Trap` representing an explicit program exit with a classic `i32`
