f14694f19857c7b5db024d9a2f30e6dc7247132f
Robin Freyler <robin.freyler@gmail.com>
2022-11-30 16:56:04 +0100

    CLI: improve feedback when running a Wasm module without exported functions (#584)
    * improve feedback when running a Wasm module without exported functions
    
    * use function instead of closure
    
    * apply rustfmt

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -105,11 +105,7 @@ fn extract_wasm_func(
         Some(func_name) => Ok(func_name),
         None => {
             let exported_funcs = display_exported_funcs(module);
-            bail!(
-                "missing function name argument for {wasm_file}\n\n\
-                    The Wasm module exports the following functions:\n\n\
-                    {exported_funcs}"
-            )
+            bail!("missing function name argument for {wasm_file}\n\n{exported_funcs}")
         }
     }
 }
@@ -140,11 +136,7 @@ fn load_wasm_func(
         .and_then(|ext| ext.into_func())
         .ok_or_else(|| {
             let exported_funcs = display_exported_funcs(module);
-            anyhow!(
-                "could not find function \"{func_name}\" in {wasm_file}\n\n\
-                    The Wasm module exports the following functions:\n\n\
-                    {exported_funcs}"
-            )
+            anyhow!("could not find function \"{func_name}\" in {wasm_file}\n\n{exported_funcs}")
         })?;
     Ok((func, func_name.into(), store))
 }
@@ -169,12 +161,16 @@ fn exported_funcs(module: &wasmi::Module) -> Vec<(&str, FuncType)> {
 ///
 /// [`Module`]: [`wasmi::Module`]
 fn display_exported_funcs(module: &wasmi::Module) -> String {
-    let exported_funcs = exported_funcs(module)
-        .into_iter()
-        .map(|(name, func_type)| display_exported_func(name, &func_type));
-    let mut buffer = String::new();
+    let exported_funcs = exported_funcs(module);
+    if exported_funcs.is_empty() {
+        return String::from("No exported functions found for the Wasm module.");
+    }
+    let mut buffer = String::from("The Wasm module exports the following functions:\n\n");
     let f = &mut buffer;
-    for func in exported_funcs {
+    for func in exported_funcs
+        .into_iter()
+        .map(|(name, func_type)| display_exported_func(name, &func_type))
+    {
         writeln!(f, " - {func}").unwrap();
     }
     buffer
@@ -281,10 +277,7 @@ fn print_execution_start(wasm_file: &str, func_name: &str, func_args: &[Value]) 
 
 /// Prints the results of the Wasm computation in a human readable form.
 fn print_pretty_results(results: &[Value]) {
-    let pretty_results = results
-        .iter()
-        .map(|value| value.to_string())
-        .collect::<Vec<_>>();
+    let pretty_results = results.iter().map(Value::to_string).collect::<Vec<_>>();
     match pretty_results.len() {
         1 => {
             println!("{}", pretty_results[0]);
