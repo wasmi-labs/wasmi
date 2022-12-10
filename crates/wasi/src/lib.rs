c1e8487c4ba26f8989cc1ee369f2c480cd82a75b
Yuyi Wang <Strawberry_Str@hotmail.com>
2022-12-10 19:30:23 +0800

    Re-export ambient_authority. (#592)

--- crates/wasi/src/lib.rs
+++ crates/wasi/src/lib.rs
@@ -4,6 +4,7 @@ pub mod snapshots;
 pub use self::guest_memory::WasmiGuestMemory;
 pub use snapshots::preview_1::define_wasi;
 pub use wasi_cap_std_sync::{
+    ambient_authority,
     clocks,
     file::{filetype_from, get_fd_flags, File},
     net,
