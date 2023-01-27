aecd7a573649a81f708b0ad44b90b6f4c1c9485d
Robin Freyler <robin.freyler@gmail.com>
2023-01-27 15:11:39 +0100

    add additional conversion checks for safety

--- crates/wasmi/src/externref.rs
+++ crates/wasmi/src/externref.rs
@@ -117,6 +117,11 @@ fn externref_null_to_zero() {
 
 impl From<UntypedValue> for ExternRef {
     fn from(untyped: UntypedValue) -> Self {
+        if untyped == UntypedValue::from(0u64) {
+            // Not sure if this early return and check
+            // is needed but better safe than sorry!
+            return Self::null()
+        }
         // Safety: This operation is safe since there are no invalid
         //         bit patterns for [`ExternRef`] instances. Therefore
         //         this operation cannot produce invalid [`ExternRef`]
--- crates/wasmi/src/func/mod.rs
+++ crates/wasmi/src/func/mod.rs
@@ -450,6 +450,11 @@ fn funcref_null_to_zero() {
 
 impl From<UntypedValue> for FuncRef {
     fn from(untyped: UntypedValue) -> Self {
+        if untyped == UntypedValue::from(0u64) {
+            // Not sure if this early return and check
+            // is needed but better safe than sorry!
+            return Self::null()
+        }
         // Safety: This operation is safe since there are no invalid
         //         bit patterns for [`FuncRef`] instances. Therefore
         //         this operation cannot produce invalid [`FuncRef`]
