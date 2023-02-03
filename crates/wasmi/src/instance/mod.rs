26d71166dc1fc96d11292a6e28686d30d1b2cecd
Robin Freyler <robin.freyler@gmail.com>
2023-02-03 23:00:50 +0100

    move Export and ExportsIter into their own module

--- crates/wasmi/src/instance/mod.rs
+++ crates/wasmi/src/instance/mod.rs
@@ -1,3 +1,4 @@
+pub use self::exports::{Export, ExportsIter};
 use super::{
     engine::DedupFuncType,
     AsContext,
@@ -21,14 +22,11 @@ use crate::{
     WasmParams,
     WasmResults,
 };
-use alloc::{
-    boxed::Box,
-    collections::{btree_map, BTreeMap},
-    sync::Arc,
-    vec::Vec,
-};
-use core::iter::FusedIterator;
+use alloc::{boxed::Box, collections::BTreeMap, sync::Arc, vec::Vec};
 use wasmi_arena::ArenaIndex;
+
+mod builder;
+mod exports;
 
 /// A raw index to a module instance entity.
 #[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord)]
@@ -134,104 +132,6 @@ impl InstanceEntity {
         ExportsIter::new(self.exports.iter())
     }
 }
-
-/// An exported WebAssembly value.
-///
-/// This type is primarily accessed from the [`Instance::exports`] method
-/// and describes what names and items are exported from a Wasm [`Instance`].
-#[derive(Debug, Clone)]
-pub struct Export<'instance> {
-    /// The name of the exported item.
-    name: &'instance str,
-    /// The definition of the exported item.
-    definition: Extern,
-}
-
-impl<'instance> Export<'instance> {
-    /// Creates a new [`Export`] with the given `name` and `definition`.
-    pub(crate) fn new(name: &'instance str, definition: Extern) -> Export<'instance> {
-        Self { name, definition }
-    }
-
-    /// Returns the name by which this export is known.
-    pub fn name(&self) -> &'instance str {
-        self.name
-    }
-
-    /// Return the [`ExternType`] of this export.
-    ///
-    /// # Panics
-    ///
-    /// If `ctx` does not own this [`Export`].
-    pub fn ty(&self, ctx: impl AsContext) -> ExternType {
-        self.definition.ty(ctx)
-    }
-
-    /// Consume this [`Export`] and return the underlying [`Extern`].
-    pub fn into_extern(self) -> Extern {
-        self.definition
-    }
-
-    /// Returns the underlying [`Func`], if the [`Export`] is a function or `None` otherwise.
-    pub fn into_func(self) -> Option<Func> {
-        self.definition.into_func()
-    }
-
-    /// Returns the underlying [`Table`], if the [`Export`] is a table or `None` otherwise.
-    pub fn into_table(self) -> Option<Table> {
-        self.definition.into_table()
-    }
-
-    /// Returns the underlying [`Memory`], if the [`Export`] is a linear memory or `None` otherwise.
-    pub fn into_memory(self) -> Option<Memory> {
-        self.definition.into_memory()
-    }
-
-    /// Returns the underlying [`Global`], if the [`Export`] is a global variable or `None` otherwise.
-    pub fn into_global(self) -> Option<Global> {
-        self.definition.into_global()
-    }
-}
-
-/// An iterator over the [`Extern`] declarations of an [`Instance`].
-#[derive(Debug)]
-pub struct ExportsIter<'instance> {
-    iter: btree_map::Iter<'instance, Box<str>, Extern>,
-}
-
-impl<'instance> ExportsIter<'instance> {
-    /// Creates a new [`ExportsIter`].
-    fn new(iter: btree_map::Iter<'instance, Box<str>, Extern>) -> Self {
-        Self { iter }
-    }
-
-    /// Prepares an item to match the expected iterator `Item` signature.
-    #[allow(clippy::borrowed_box)]
-    fn convert_item((name, export): (&'instance Box<str>, &'instance Extern)) -> Export {
-        Export::new(name, *export)
-    }
-}
-
-impl<'instance> Iterator for ExportsIter<'instance> {
-    type Item = Export<'instance>;
-
-    fn size_hint(&self) -> (usize, Option<usize>) {
-        self.iter.size_hint()
-    }
-
-    fn next(&mut self) -> Option<Self::Item> {
-        self.iter.next().map(Self::convert_item)
-    }
-}
-
-impl DoubleEndedIterator for ExportsIter<'_> {
-    fn next_back(&mut self) -> Option<Self::Item> {
-        self.iter.next_back().map(Self::convert_item)
-    }
-}
-
-impl ExactSizeIterator for ExportsIter<'_> {}
-impl FusedIterator for ExportsIter<'_> {}
 
 /// A module instance entity builder.
 #[derive(Debug)]
