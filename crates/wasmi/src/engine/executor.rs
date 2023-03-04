d4f86ef5f396b6cc41ee24c8485ab963662390e6
Robin Freyler <robin.freyler@gmail.com>
2023-03-04 11:16:37 +0100

    replace inlines

--- crates/wasmi/src/engine/executor.rs
+++ crates/wasmi/src/engine/executor.rs
@@ -650,7 +650,7 @@ impl<'ctx, 'engine> Executor<'ctx, 'engine> {
     ///
     /// - If the [`StoreInner`] ran out of fuel.
     /// - If the `exec` closure traps.
-    #[inline(always)]
+    #[inline]
     #[cold]
     fn consume_fuel_with_mode<T, E>(
         &mut self,
@@ -678,7 +678,7 @@ impl<'ctx, 'engine> Executor<'ctx, 'engine> {
     ///
     /// - If the [`StoreInner`] ran out of fuel.
     /// - If the `exec` closure traps.
-    #[inline(always)]
+    #[inline]
     fn consume_fuel_with_lazy<T, E>(
         &mut self,
         delta: u64,
@@ -702,7 +702,7 @@ impl<'ctx, 'engine> Executor<'ctx, 'engine> {
     ///
     /// - If the [`StoreInner`] ran out of fuel.
     /// - If the `exec` closure traps.
-    #[inline(always)]
+    #[inline]
     fn consume_fuel_with_eager<T, E>(
         &mut self,
         delta: u64,
