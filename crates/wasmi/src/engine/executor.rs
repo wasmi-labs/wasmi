17d8c096e6a6dfbd52eb37d2d34c04a59af2cb41
Robin Freyler <robin.freyler@gmail.com>
2023-03-06 10:56:47 +0100

    use inline

--- crates/wasmi/src/engine/executor.rs
+++ crates/wasmi/src/engine/executor.rs
@@ -604,7 +604,7 @@ impl<'ctx, 'engine> Executor<'ctx, 'engine> {
     ///
     /// - If the [`StoreInner`] ran out of fuel.
     /// - If the `exec` closure traps.
-    #[inline(always)]
+    #[inline]
     fn consume_fuel_with<T, E>(
         &mut self,
         delta: impl FnOnce(&FuelCosts) -> u64,
@@ -627,7 +627,7 @@ impl<'ctx, 'engine> Executor<'ctx, 'engine> {
     ///
     /// - If the [`StoreInner`] ran out of fuel.
     /// - If the `exec` closure traps.
-    #[inline(always)]
+    #[inline]
     fn consume_fuel_with_mode<T, E>(
         &mut self,
         mode: FuelConsumptionMode,
@@ -654,7 +654,7 @@ impl<'ctx, 'engine> Executor<'ctx, 'engine> {
     ///
     /// - If the [`StoreInner`] ran out of fuel.
     /// - If the `exec` closure traps.
-    #[inline(always)]
+    #[inline]
     fn consume_fuel_with_lazy<T, E>(
         &mut self,
         delta: u64,
@@ -678,7 +678,7 @@ impl<'ctx, 'engine> Executor<'ctx, 'engine> {
     ///
     /// - If the [`StoreInner`] ran out of fuel.
     /// - If the `exec` closure traps.
-    #[inline(always)]
+    #[inline]
     fn consume_fuel_with_eager<T, E>(
         &mut self,
         delta: u64,
