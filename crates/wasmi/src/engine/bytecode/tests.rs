e2001965d5a52350d4f23759f4dca5076fb65dfe
Robin Freyler <robin.freyler@gmail.com>
2023-05-19 22:46:14 +0200

    extend wasmi bytecode utils tests

--- crates/wasmi/src/engine/bytecode/tests.rs
+++ crates/wasmi/src/engine/bytecode/tests.rs
@@ -7,4 +7,13 @@ fn size_of_instruction() {
     assert_eq!(size_of::<DropKeep>(), 3);
     assert_eq!(size_of::<BranchParams>(), 6);
     assert_eq!(size_of::<BranchOffset>(), 3);
+    assert_eq!(size_of::<BlockFuel>(), 3);
+    assert_eq!(size_of::<BranchTableTargets>(), 3);
+    assert_eq!(size_of::<DataSegmentIdx>(), 3);
+    assert_eq!(size_of::<ElementSegmentIdx>(), 3);
+    assert_eq!(size_of::<FuncIdx>(), 3);
+    assert_eq!(size_of::<GlobalIdx>(), 3);
+    assert_eq!(size_of::<TableIdx>(), 3);
+    assert_eq!(size_of::<SignatureIdx>(), 3);
+    assert_eq!(size_of::<LocalDepth>(), 3);
 }
