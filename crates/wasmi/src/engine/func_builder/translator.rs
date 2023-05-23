b3544f23abef537d0aa76d4b86400bf022a828a5
Robin Freyler <robin.freyler@gmail.com>
2023-05-23 16:28:40 +0200

    Apply `global.get` const optimization to `f32` values (#719)
    * apply global.get const opt to f32 values
    
    * update docs

--- crates/wasmi/src/engine/func_builder/translator.rs
+++ crates/wasmi/src/engine/func_builder/translator.rs
@@ -429,15 +429,20 @@ impl<'parser> FuncTranslator<'parser> {
     ) -> Result<Option<Instruction>, TranslationError> {
         if let (Mutability::Const, Some(init_expr)) = (global_type.mutability(), init_value) {
             if let Some(value) = init_expr.eval_const() {
+                // We can optimize `global.get` to the constant value.
                 if global_type.content() == ValueType::I32 {
                     return Ok(Some(Instruction::i32_const(i32::from(value))));
+                }
+                if global_type.content() == ValueType::F32 {
+                    return Ok(Some(Instruction::f32_const(F32::from(value))));
                 }
                 if global_type.content() == ValueType::I64 {
                     if let Ok(value) = i32::try_from(i64::from(value)) {
                         return Ok(Some(Instruction::I64Const32(value)));
                     }
                 }
-                // We can optimize `global.get` to the constant value.
+                // No optimized case was applicable so we have to allocate
+                // a constant value in the const pool and reference it.
                 let cref = engine.alloc_const(value)?;
                 return Ok(Some(Instruction::ConstRef(cref)));
             }
