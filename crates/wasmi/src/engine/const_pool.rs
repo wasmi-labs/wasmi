52a2714f22aa84a5a00362505c957f5c209c6de5
Robin Freyler <robin.freyler@gmail.com>
2023-05-29 13:43:59 +0200

    add #[repr(transparent)]

--- crates/wasmi/src/engine/const_pool.rs
+++ crates/wasmi/src/engine/const_pool.rs
@@ -6,6 +6,7 @@ use alloc::{
 use wasmi_core::UntypedValue;
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord)]
+#[repr(transparent)]
 pub struct ConstRef(u32);
 
 impl TryFrom<usize> for ConstRef {
