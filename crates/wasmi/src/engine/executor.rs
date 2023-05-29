640abf5460ca0092c30a46e0ad57ae38963b8c8b
Robin Freyler <robin.freyler@gmail.com>
2023-05-29 11:15:25 +0200

    get rid of experimental inline annotations

--- crates/wasmi/src/engine/executor.rs
+++ crates/wasmi/src/engine/executor.rs
@@ -848,7 +848,6 @@ impl<'ctx, 'engine> Executor<'ctx, 'engine> {
     /// - This is required for some instructions that do not fit into
     ///   a single instruction word and store a [`DropKeep`] value in
     ///   another instruction word.
-    #[inline]
     fn fetch_drop_keep(&self, offset: usize) -> DropKeep {
         let mut addr: InstructionPtr = self.ip;
         addr.add(offset);
@@ -868,7 +867,6 @@ impl<'ctx, 'engine> Executor<'ctx, 'engine> {
     /// - This is required for some instructions that do not fit into
     ///   a single instruction word and store a [`TableIdx`] value in
     ///   another instruction word.
-    #[inline]
     fn fetch_table_idx(&self, offset: usize) -> TableIdx {
         let mut addr: InstructionPtr = self.ip;
         addr.add(offset);
