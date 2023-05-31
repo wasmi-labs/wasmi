e3d37cbc25dd3bb2d6edeff009bfc72025b9d425
Robin Freyler <robin.freyler@gmail.com>
2023-05-31 21:26:31 +0200

    add unary i64 instructions

--- crates/wasmi/src/engine/bytecode2/mod.rs
+++ crates/wasmi/src/engine/bytecode2/mod.rs
@@ -1114,10 +1114,16 @@ pub enum Instruction {
 
     /// `i32` count-leading-zeros (clz) instruction.
     I32Clz(UnaryInstr),
+    /// `i64` count-leading-zeros (clz) instruction.
+    I64Clz(UnaryInstr),
     /// `i32` count-trailing-zeros (ctz) instruction.
     I32Ctz(UnaryInstr),
+    /// `i64` count-trailing-zeros (ctz) instruction.
+    I64Ctz(UnaryInstr),
     /// `i32` pop-count instruction.
     I32Popcnt(UnaryInstr),
+    /// `i64` pop-count instruction.
+    I64Popcnt(UnaryInstr),
 
     /// `i32` add instruction: `r0 = r1 + r2`
     I32Add(BinInstr),
