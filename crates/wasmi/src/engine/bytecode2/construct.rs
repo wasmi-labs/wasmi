6c542906f9150425332468219c1ca7e93785753a
Robin Freyler <robin.freyler@gmail.com>
2023-06-09 01:07:36 +0200

    add macro to generate instruction constructors

--- crates/wasmi/src/engine/bytecode2/construct.rs
+++ crates/wasmi/src/engine/bytecode2/construct.rs
@@ -1,33 +1,41 @@
 use super::{BinInstr, BinInstrImm16, Const16, Instruction, Register, UnaryInstr};
 
+macro_rules! constructor_for {
+    (
+        $(
+            fn $fn_name:ident($mode:ident) -> Self::$op_code:ident;
+        )* $(,)?
+    ) => {
+        $( constructor_for! { @impl fn $fn_name($mode) -> Self::$op_code } )*
+    };
+    ( @impl fn $fn_name:ident(binary) -> Self::$op_code:ident ) => {
+        #[doc = concat!("Creates a new [`Instruction::", stringify!($op_code), "`].")]
+        pub fn $fn_name(result: Register, lhs: Register, rhs: Register) -> Self {
+            Self::$op_code(BinInstr::new(result, lhs, rhs))
+        }
+    };
+    ( @impl fn $fn_name:ident(binary_imm) -> Self::$op_code:ident ) => {
+        #[doc = concat!("Creates a new [`Instruction::", stringify!($op_code), "`].")]
+        pub fn $fn_name(result: Register, lhs: Register) -> Self {
+            Self::$op_code(UnaryInstr::new(result, lhs))
+        }
+    };
+    ( @impl fn $fn_name:ident(binary_imm16) -> Self::$op_code:ident ) => {
+        #[doc = concat!("Creates a new [`Instruction::", stringify!($op_code), "`].")]
+        pub fn $fn_name(result: Register, lhs: Register, rhs: Const16) -> Self {
+            Self::$op_code(BinInstrImm16::new(result, lhs, rhs))
+        }
+    };
+}
+
 impl Instruction {
-    /// Creates a new [`Instruction::I32Add`].
-    pub fn i32_add(result: Register, lhs: Register, rhs: Register) -> Self {
-        Self::I32Add(BinInstr::new(result, lhs, rhs))
-    }
+    constructor_for! {
+        fn i32_add(binary) -> Self::I32Add;
+        fn i32_add_imm(binary_imm) -> Self::I32AddImm;
+        fn i32_add_imm16(binary_imm16) -> Self::I32AddImm16;
 
-    /// Creates a new [`Instruction::I32AddImm`].
-    pub fn i32_add_imm(result: Register, lhs: Register) -> Self {
-        Self::I32AddImm(UnaryInstr::new(result, lhs))
-    }
-
-    /// Creates a new [`Instruction::I32AddImm`].
-    pub fn i32_add_imm16(result: Register, lhs: Register, rhs: Const16) -> Self {
-        Self::I32AddImm16(BinInstrImm16::new(result, lhs, rhs))
-    }
-
-    /// Creates a new [`Instruction::I32Mul`].
-    pub fn i32_mul(result: Register, lhs: Register, rhs: Register) -> Self {
-        Self::I32Mul(BinInstr::new(result, lhs, rhs))
-    }
-
-    /// Creates a new [`Instruction::I32MulImm`].
-    pub fn i32_mul_imm(result: Register, lhs: Register) -> Self {
-        Self::I32MulImm(UnaryInstr::new(result, lhs))
-    }
-
-    /// Creates a new [`Instruction::I32MulImm`].
-    pub fn i32_mul_imm16(result: Register, lhs: Register, rhs: Const16) -> Self {
-        Self::I32MulImm16(BinInstrImm16::new(result, lhs, rhs))
+        fn i32_mul(binary) -> Self::I32Mul;
+        fn i32_mul_imm(binary_imm) -> Self::I32MulImm;
+        fn i32_mul_imm16(binary_imm16) -> Self::I32MulImm16;
     }
 }
