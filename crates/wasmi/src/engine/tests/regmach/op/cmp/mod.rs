8355360ed7ae065f58754f581ea8170ffd5caf9a
Robin Freyler <robin.freyler@gmail.com>
2023-07-01 15:24:50 +0200

    move cmp tests into their own submodule

---
+++ crates/wasmi/src/engine/tests/regmach/op/cmp/mod.rs
@@ -0,0 +1,37 @@
+use super::*;
+
+mod f32_eq;
+mod f32_ne;
+mod f64_eq;
+mod f64_ne;
+mod i32_eq;
+mod i32_ne;
+mod i64_eq;
+mod i64_ne;
+
+mod f32_ge;
+mod f32_gt;
+mod f32_le;
+mod f32_lt;
+mod f64_ge;
+mod f64_gt;
+mod f64_le;
+mod f64_lt;
+
+mod i32_ge_s;
+mod i32_ge_u;
+mod i32_gt_s;
+mod i32_gt_u;
+mod i32_le_s;
+mod i32_le_u;
+mod i32_lt_s;
+mod i32_lt_u;
+
+mod i64_ge_s;
+mod i64_ge_u;
+mod i64_gt_s;
+mod i64_gt_u;
+mod i64_le_s;
+mod i64_le_u;
+mod i64_lt_s;
+mod i64_lt_u;
