21aafe1baf362a709bbed5d648dbbe820b255d97
Robin Freyler <robin.freyler@gmail.com>
2023-07-03 10:29:04 +0200

    add more translation tests for block

--- crates/wasmi/src/engine/tests/regmach/op/block.rs
+++ crates/wasmi/src/engine/tests/regmach/op/block.rs
@@ -1,7 +1,7 @@
 use super::*;
 
 #[test]
-fn simple_block() {
+fn empty_block() {
     let wasm = wat2wasm(
         r"
         (module
@@ -14,7 +14,7 @@ fn simple_block() {
 }
 
 #[test]
-fn nested_block() {
+fn nested_empty_block() {
     let wasm = wat2wasm(
         r"
         (module
@@ -23,5 +23,39 @@ fn nested_block() {
     );
     TranslationTest::new(wasm)
         .expect_func([Instruction::Return])
+        .run()
+}
+
+#[test]
+fn identity_block_1() {
+    let wasm = wat2wasm(
+        r"
+        (module
+            (func (param i32) (result i32)
+                (local.get 0)
+                (block (param i32) (result i32))
+            )
+        )",
+    );
+    TranslationTest::new(wasm)
+        .expect_func([Instruction::return_reg(Register::from_u16(0))])
+        .run()
+}
+
+#[test]
+fn nested_identity_block_1() {
+    let wasm = wat2wasm(
+        r"
+        (module
+            (func (param i32) (result i32)
+                (local.get 0)
+                (block (param i32) (result i32)
+                    (block (param i32) (result i32))
+                )
+            )
+        )",
+    );
+    TranslationTest::new(wasm)
+        .expect_func([Instruction::return_reg(Register::from_u16(0))])
         .run()
 }
