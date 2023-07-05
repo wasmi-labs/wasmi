feac7e578346c0b7352938a80588a28de41a242c
Robin Freyler <robin.freyler@gmail.com>
2023-07-05 18:02:01 +0200

    refactor Provider abstraction from ProviderSlice

--- crates/wasmi/src/engine/bytecode2/provider.rs
+++ crates/wasmi/src/engine/bytecode2/provider.rs
@@ -1,24 +1,54 @@
-use super::{Const32, ConstRef, Register};
+use super::Register;
+use wasmi_core::UntypedValue;
 
-/// A reference to a [`ProviderSlice`].
+/// A light-weight reference to a [`ProviderSlice`].
+///
+/// # Dev. Note
+///
+/// We use `[u8; 4]` instead of a simple `u32` here to
+/// reduce the alignment requirement of this type so that
+/// it can be used by variants of [`Instruction`] without
+/// bloating up the [`Instruction`] type due to alignment
+/// constraints.
 #[derive(Debug, Copy, Clone, PartialEq, Eq)]
-pub struct ProviderSliceRef(u32);
+pub struct ProviderSliceRef([u8; 4]);
 
 /// A [`Provider`] slice.
+///
+/// # Note
+///
+/// Usually used for instructions with arbitrary many inputs.
+/// Examples of this are [`Instruction::ReturnProviders`] and
+/// certain call instructions for handling their parameters.
 pub struct ProviderSlice<'a> {
     /// The [`Provider`] values of the slice.
     values: &'a [Provider],
 }
 
-/// A provider value.
+/// A provider for an input to an [`Instruction`].
 #[derive(Debug, Copy, Clone, PartialEq, Eq)]
 pub enum Provider {
     /// A [`Register`] value.
     Register(Register),
-    /// A reference to a constant value.
-    ConstRef(ConstRef),
-    /// A 32-bit constant value.
-    Const32(Const32),
-    /// A 32-bit encoded constant `i64` value.
-    Const32AsI64(Const32),
+    /// An immediate [`UntypedValue`].
+    Immediate(UntypedValue),
+}
+
+impl From<Register> for Provider {
+    fn from(register: Register) -> Self {
+        Self::Register(register)
+    }
+}
+
+impl From<UntypedValue> for Provider {
+    fn from(register: UntypedValue) -> Self {
+        Self::Immediate(register)
+    }
+}
+
+impl Provider {
+    /// Creates a new immediate value [`Provider`].
+    pub fn immediate(value: impl Into<UntypedValue>) -> Self {
+        Self::from(value.into())
+    }
 }
