55b70b191f8861e29381b7b0027c57a5e7ab8304
Robin Freyler <robin.freyler@gmail.com>
2023-07-08 23:50:04 +0200

    add some tests for br, br_if and block Wasm translation

--- crates/wasmi/src/engine/tests/regmach/op/block.rs
+++ crates/wasmi/src/engine/tests/regmach/op/block.rs
@@ -101,6 +101,26 @@ fn nested_identity_block_2() {
 }
 
 #[test]
+fn branched_block_0() {
+    let wasm = wat2wasm(
+        r"
+        (module
+            (func
+                (block
+                    (br 0)
+                )
+            )
+        )",
+    );
+    TranslationTest::new(wasm)
+        .expect_func([
+            Instruction::branch(BranchOffset::from(1)),
+            Instruction::Return,
+        ])
+        .run()
+}
+
+#[test]
 fn branched_block_1() {
     let wasm = wat2wasm(
         r"
@@ -213,6 +233,91 @@ fn branch_if_block_2() {
             Instruction::i32_ctz(Register::from_u16(3), Register::from_u16(1)),
             Instruction::branch_nez(Register::from_u16(3), BranchOffset::from(1)),
             Instruction::return_reg(Register::from_u16(2)),
+        ])
+        .run()
+}
+
+#[test]
+fn branch_to_func_block_0() {
+    let wasm = wat2wasm(
+        r"
+        (module
+            (func
+                (br 0)
+            )
+        )",
+    );
+    TranslationTest::new(wasm)
+        .expect_func([Instruction::Return])
+        .run()
+}
+
+#[test]
+fn branch_to_func_block_1() {
+    let wasm = wat2wasm(
+        r"
+        (module
+            (func (param i32) (result i32)
+                (local.get 0)
+                (br 0)
+            )
+        )",
+    );
+    TranslationTest::new(wasm)
+        .expect_func([Instruction::return_reg(Register::from_u16(0))])
+        .run()
+}
+
+#[test]
+fn branch_to_func_block_nested_0() {
+    let wasm = wat2wasm(
+        r"
+        (module
+            (func
+                (block
+                    (br 1)
+                )
+            )
+        )",
+    );
+    TranslationTest::new(wasm)
+        .expect_func([Instruction::Return])
+        .run()
+}
+
+#[test]
+fn branch_to_func_block_nested_1() {
+    let wasm = wat2wasm(
+        r"
+        (module
+            (func (param i32) (result i32)
+                (local.get 0)
+                (block (param i32) (result i32)
+                    (br 1)
+                )
+            )
+        )",
+    );
+    TranslationTest::new(wasm)
+        .expect_func([Instruction::return_reg(Register::from_u16(0))])
+        .run()
+}
+
+#[test]
+fn branch_if_to_func_block_0() {
+    let wasm = wat2wasm(
+        r"
+        (module
+            (func (param i32)
+                (local.get 0)
+                (br_if 0)
+            )
+        )",
+    );
+    TranslationTest::new(wasm)
+        .expect_func([
+            Instruction::return_nez(Register::from_u16(0)),
+            Instruction::Return,
         ])
         .run()
 }
