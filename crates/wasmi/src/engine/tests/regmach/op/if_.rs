615cb769fc4b3e7a0f3afe61ed5d6835876c62e2
Robin Freyler <robin.freyler@gmail.com>
2023-07-13 10:29:51 +0200

    add if translation test with constant condition

--- crates/wasmi/src/engine/tests/regmach/op/if_.rs
+++ crates/wasmi/src/engine/tests/regmach/op/if_.rs
@@ -44,3 +44,33 @@ fn simple_if_then_else() {
         ])
         .run()
 }
+
+#[test]
+fn const_condition() {
+    fn test_for(condition: bool) {
+        let true_value = 10_i32;
+        let false_value = 20_i32;
+        let expected = match condition {
+            true => true_value,
+            false => false_value,
+        };
+        let condition = i32::from(condition);
+        let wasm = wat2wasm(&format!(
+            r"
+            (module
+                (func (result i32)
+                    (i32.const {condition})
+                    (if (result i32)
+                        (then (i32.const {true_value}))
+                        (else (i32.const {false_value}))
+                    )
+                )
+            )",
+        ));
+        TranslationTest::new(wasm)
+            .expect_func([Instruction::return_imm32(Const32::from(expected))])
+            .run()
+    }
+    test_for(true);
+    test_for(false);
+}
