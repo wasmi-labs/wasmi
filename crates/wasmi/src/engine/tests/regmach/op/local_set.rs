95246847ef9fa3dbdc99f4afd5ce5f264d3cf7b3
Robin Freyler <robin.freyler@gmail.com>
2023-08-11 12:32:34 +0200

    add more local.set and local.tee translation tests

--- crates/wasmi/src/engine/tests/regmach/op/local_set.rs
+++ crates/wasmi/src/engine/tests/regmach/op/local_set.rs
@@ -206,3 +206,77 @@ fn avoid_overwrite_result_1() {
         ])
         .run()
 }
+
+#[test]
+#[cfg_attr(miri, ignore)]
+fn local_set_chain() {
+    let wasm = wat2wasm(
+        r#"
+        (module
+            (func (param i32) (param i32) (result i32)
+                (local.set 0 (i32.const 10))
+                (local.set 1 (local.get 0))
+                (local.get 1)
+            )
+        )"#,
+    );
+    TranslationTest::new(wasm)
+        .expect_func_instrs([
+            Instruction::copy_imm32(Register::from_i16(0), 10_i32),
+            Instruction::copy(Register::from_i16(1), Register::from_i16(0)),
+            Instruction::return_reg(Register::from_i16(1)),
+        ])
+        .run()
+}
+
+#[test]
+#[cfg_attr(miri, ignore)]
+fn local_tee_chain() {
+    let wasm = wat2wasm(
+        r#"
+        (module
+            (func (param i32) (param i32) (result i32)
+                (local.tee 0 (i32.const 10))
+                (local.tee 1)
+            )
+        )"#,
+    );
+    TranslationTest::new(wasm)
+        .expect_func_instrs([
+            Instruction::copy_imm32(Register::from_i16(0), 10_i32),
+            Instruction::copy(Register::from_i16(1), Register::from_i16(0)),
+            Instruction::return_reg(Register::from_i16(1)),
+        ])
+        .run()
+}
+
+#[test]
+#[cfg_attr(miri, ignore)]
+fn preserve_result_1() {
+    let wasm = wat2wasm(
+        r#"
+        (module
+            (func (param $lhs i32) (param $rhs i32) (result i32)
+                (local.get 0)
+                (local.set 0
+                    (i32.add
+                        (local.get $lhs)
+                        (local.get $rhs)
+                    )
+                )
+            )
+        )"#,
+    );
+    TranslationTest::new(wasm)
+        .expect_func_instrs([
+            Instruction::i32_add(
+                Register::from_i16(2),
+                Register::from_i16(0),
+                Register::from_i16(1),
+            ),
+            Instruction::copy(Register::from_i16(3), Register::from_i16(0)),
+            Instruction::copy(Register::from_i16(0), Register::from_i16(2)),
+            Instruction::return_reg(Register::from_i16(3)),
+        ])
+        .run()
+}
