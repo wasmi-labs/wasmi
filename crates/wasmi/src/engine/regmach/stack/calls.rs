4a2ac4caf699f3972f59ee885b6cf8b6e6878435
Robin Freyler <robin.freyler@gmail.com>
2023-08-13 15:49:24 +0200

    initial implementation of the regmach CallStack

--- crates/wasmi/src/engine/regmach/stack/calls.rs
+++ crates/wasmi/src/engine/regmach/stack/calls.rs
@@ -1,19 +1,83 @@
 #![allow(dead_code)] // TODO: remove
 
-use super::{BaseValueStackOffset, FrameValueStackOffset};
+use super::{err_stack_overflow, BaseValueStackOffset, FrameValueStackOffset};
 use crate::{engine::code_map::InstructionPtr2 as InstructionPtr, Instance};
+use alloc::vec::Vec;
+use wasmi_core::TrapCode;
 
 #[cfg(doc)]
-use crate::{engine::bytecode2::Instruction, engine::bytecode2::Register, Global, Memory, Table};
+use crate::{
+    engine::bytecode2::Instruction,
+    engine::bytecode2::Register,
+    engine::CompiledFunc,
+    Global,
+    Memory,
+    Table,
+};
 
+/// The stack of nested function calls.
 #[derive(Debug, Default)]
-pub struct CallStack {}
+pub struct CallStack {
+    /// The stack of nested function calls.
+    calls: Vec<CallFrame>,
+    /// The maximum allowed recursion depth.
+    ///
+    /// # Note
+    ///
+    /// A [`TrapCode::StackOverflow`] is raised if the recursion limit is exceeded.
+    recursion_limit: usize,
+}
 
 impl CallStack {
     /// Default value for the maximum recursion depth.
     pub const DEFAULT_MAX_RECURSION_DEPTH: usize = 1024;
+
+    /// Clears the [`CallStack`] entirely.
+    ///
+    /// # Note
+    ///
+    /// The [`CallStack`] can sometimes be left in a non-empty state upon
+    /// executing a function, for example when a trap is encountered. We
+    /// reset the [`CallStack`] before executing the next function to
+    /// provide a clean slate for all executions.
+    pub fn reset(&mut self) {
+        self.calls.clear();
+    }
+
+    /// Returns the number of [`CallFrame`] on the [`CallStack`].
+    #[inline]
+    fn len(&self) -> usize {
+        self.calls.len()
+    }
+
+    /// Pushes a [`CallFrame`] onto the [`CallStack`].
+    ///
+    /// # Errors
+    ///
+    /// If the recursion limit has been reached.
+    #[inline]
+    pub fn push(&mut self, call: CallFrame) -> Result<(), TrapCode> {
+        if self.len() == self.recursion_limit {
+            return Err(err_stack_overflow());
+        }
+        self.calls.push(call);
+        Ok(())
+    }
+
+    /// Pops the last [`CallFrame`] from the [`CallStack`] if any.
+    #[inline]
+    pub fn pop(&mut self) -> Option<CallFrame> {
+        self.calls.pop()
+    }
+
+    /// Peeks the last [`CallFrame`] of the [`CallStack`] if any.
+    #[inline]
+    pub fn peek(&self) -> Option<&CallFrame> {
+        self.calls.last()
+    }
 }
 
+/// A single frame of a called [`CompiledFunc`].
 #[derive(Debug, Copy, Clone)]
 pub struct CallFrame {
     /// The pointer to the [`Instruction`] that is executed next.
