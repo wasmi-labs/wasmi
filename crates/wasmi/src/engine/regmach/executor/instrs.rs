bd13f0269cda4d6c1a030a39cee0f0ad61045f67
Robin Freyler <robin.freyler@gmail.com>
2023-08-22 22:15:38 +0200

    move return instruction implementation to submodule

--- crates/wasmi/src/engine/regmach/executor/instrs.rs
+++ crates/wasmi/src/engine/regmach/executor/instrs.rs
@@ -4,15 +4,7 @@ use crate::{
     core::TrapCode,
     engine::{
         bytecode::{BlockFuel, BranchOffset, FuncIdx},
-        bytecode2::{
-            AnyConst32,
-            Const32,
-            Instruction,
-            Register,
-            RegisterSpan,
-            RegisterSpanIter,
-            UnaryInstr,
-        },
+        bytecode2::{AnyConst32, Const32, Instruction, Register, RegisterSpan, UnaryInstr},
         cache::InstanceCache,
         code_map::{CodeMap2 as CodeMap, InstructionPtr2 as InstructionPtr},
         config::FuelCosts,
@@ -33,6 +25,7 @@ mod conversion;
 mod global;
 mod load;
 mod memory;
+mod return_;
 mod select;
 mod store;
 mod table;
@@ -1087,150 +1080,6 @@ impl<'ctx, 'engine> Executor<'ctx, 'engine> {
         // is enabled to begin with.
         self.ctx.fuel_mut().consume_fuel(block_fuel.to_u64())?;
         self.try_next_instr()
-    }
-
-    /// Execute an [`Instruction::Return`].
-    #[inline(always)]
-    fn execute_return(&mut self) -> ReturnOutcome {
-        self.ret()
-    }
-
-    /// Execute a generic return [`Instruction`] returning a single value.
-    #[inline(always)]
-    fn execute_return_value<T>(
-        &mut self,
-        value: T,
-        f: fn(&mut Self, T) -> UntypedValue,
-    ) -> ReturnOutcome {
-        match self.call_stack.peek() {
-            Some(caller) => unsafe {
-                // Case: we need to return the `value` back to the caller frame.
-                // Safety: TODO
-                let mut caller_sp = self.value_stack.stack_ptr_at(caller.base_offset());
-                let result = caller_sp.get_mut(caller.results().head());
-                *result = f(self, value);
-            },
-            None => {
-                // Case: the root call frame is returning.
-                todo!()
-            }
-        }
-        self.ret()
-    }
-
-    /// Execute an [`Instruction::ReturnReg`] returning a single [`Register`] value.
-    #[inline(always)]
-    fn execute_return_reg(&mut self, value: Register) -> ReturnOutcome {
-        self.execute_return_value(value, |this, value| this.get_register(value))
-    }
-
-    /// Execute an [`Instruction::ReturnImm32`] returning a single 32-bit value.
-    #[inline(always)]
-    fn execute_return_imm32(&mut self, value: AnyConst32) -> ReturnOutcome {
-        self.execute_return_value(value, |_, value| value.to_u32().into())
-    }
-
-    /// Execute an [`Instruction::ReturnI64Imm32`] returning a single 32-bit encoded `i64` value.
-    #[inline(always)]
-    fn execute_return_i64imm32(&mut self, value: Const32<i64>) -> ReturnOutcome {
-        self.execute_return_value(value, |_, value| i64::from(value).into())
-    }
-
-    /// Execute an [`Instruction::ReturnF64Imm32`] returning a single 32-bit encoded `f64` value.
-    #[inline(always)]
-    fn execute_return_f64imm32(&mut self, value: Const32<f64>) -> ReturnOutcome {
-        self.execute_return_value(value, |_, value| f64::from(value).into())
-    }
-
-    /// Execute an [`Instruction::ReturnMany`] returning many values.
-    #[inline(always)]
-    fn execute_return_many(&mut self, values: RegisterSpanIter) -> ReturnOutcome {
-        match self.call_stack.peek() {
-            Some(caller) => unsafe {
-                // Case: we need to return the `value` back to the caller frame.
-                // Safety: TODO
-                let mut caller_sp = self.value_stack.stack_ptr_at(caller.base_offset());
-                let results = caller.results().iter(values.len());
-                for (result, value) in results.zip(values) {
-                    *caller_sp.get_mut(result) = self.get_register(value);
-                }
-            },
-            None => {
-                // Case: the root call frame is returning.
-                todo!()
-            }
-        }
-        self.ret()
-    }
-
-    /// Execute a generic conditional return [`Instruction`].
-    #[inline(always)]
-    fn execute_return_nez_impl<T>(
-        &mut self,
-        condition: Register,
-        value: T,
-        f: fn(&mut Self, T) -> ReturnOutcome,
-    ) -> ReturnOutcome {
-        let condition = self.get_register(condition);
-        match bool::from(condition) {
-            true => f(self, value),
-            false => {
-                self.next_instr();
-                ReturnOutcome::Wasm
-            }
-        }
-    }
-
-    /// Execute an [`Instruction::Return`].
-    #[inline(always)]
-    fn execute_return_nez(&mut self, condition: Register) -> ReturnOutcome {
-        self.execute_return_nez_impl(condition, (), |this, _| this.execute_return())
-    }
-
-    /// Execute an [`Instruction::ReturnNezReg`] returning a single [`Register`] value.
-    #[inline(always)]
-    fn execute_return_nez_reg(&mut self, condition: Register, value: Register) -> ReturnOutcome {
-        self.execute_return_nez_impl(condition, value, Self::execute_return_reg)
-    }
-
-    /// Execute an [`Instruction::ReturnNezImm32`] returning a single 32-bit constant value.
-    #[inline(always)]
-    fn execute_return_nez_imm32(
-        &mut self,
-        condition: Register,
-        value: AnyConst32,
-    ) -> ReturnOutcome {
-        self.execute_return_nez_impl(condition, value, Self::execute_return_imm32)
-    }
-
-    /// Execute an [`Instruction::ReturnNezI64Imm32`] returning a single 32-bit encoded constant `i64` value.
-    #[inline(always)]
-    fn execute_return_nez_i64imm32(
-        &mut self,
-        condition: Register,
-        value: Const32<i64>,
-    ) -> ReturnOutcome {
-        self.execute_return_nez_impl(condition, value, Self::execute_return_i64imm32)
-    }
-
-    /// Execute an [`Instruction::ReturnNezF64Imm32`] returning a single 32-bit encoded constant `f64` value.
-    #[inline(always)]
-    fn execute_return_nez_f64imm32(
-        &mut self,
-        condition: Register,
-        value: Const32<f64>,
-    ) -> ReturnOutcome {
-        self.execute_return_nez_impl(condition, value, Self::execute_return_f64imm32)
-    }
-
-    /// Execute an [`Instruction::ReturnNezMany`] returning many values.
-    #[inline(always)]
-    fn execute_return_nez_many(
-        &mut self,
-        condition: Register,
-        values: RegisterSpanIter,
-    ) -> ReturnOutcome {
-        self.execute_return_nez_impl(condition, values, Self::execute_return_many)
     }
 
     #[inline(always)]
