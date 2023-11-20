47cd46a75b29f55279919ad550a8ecffe72baa66
Robin Freyler <robin.freyler@gmail.com>
2023-11-20 23:02:07 +0100

    extend block_forward_no_copy test

--- crates/wasmi/src/engine/regmach/tests/op/cmp_br.rs
+++ crates/wasmi/src/engine/regmach/tests/op/cmp_br.rs
@@ -242,35 +242,78 @@ fn block_forward() {
 #[test]
 #[cfg_attr(miri, ignore)]
 fn block_forward_nop_copy() {
-    let wasm = wat2wasm(
-        r"
-        (module
-            (global $g (mut i32) (i32.const 10))
-            (func (param i32 i32) (result i32)
-                (global.get $g)
-                (block (param i32) (result i32)
-                    (local.get 0)
-                    (local.get 1)
-                    (i32.eq)
-                    (br_if 0)
-                    (drop)
-                    (local.get 0)
+    fn test_for(
+        ty: ValueType,
+        op: &str,
+        expect_instr: fn(Register, Register, BranchOffset16) -> Instruction,
+    ) {
+        let ty = DisplayValueType::from(ty);
+        let wasm = wat2wasm(&format!(
+            r"
+            (module
+                (global $g (mut {ty}) ({ty}.const 10))
+                (func (param {ty} {ty}) (result {ty})
+                    (global.get $g)
+                    (block (param {ty}) (result {ty})
+                        (local.get 0)
+                        (local.get 1)
+                        ({ty}.{op})
+                        (br_if 0)
+                        (drop)
+                        (local.get 0)
+                    )
                 )
-            )
-        )",
-    );
-    TranslationTest::new(wasm)
-        .expect_func_instrs([
-            Instruction::global_get(Register::from_i16(2), GlobalIdx::from(0)),
-            Instruction::branch_i32_eq(
-                Register::from_i16(0),
-                Register::from_i16(1),
-                BranchOffset16::from(2),
-            ),
-            Instruction::copy(Register::from_i16(2), Register::from_i16(0)),
-            Instruction::return_reg(2),
-        ])
-        .run()
+            )",
+        ));
+        TranslationTest::new(wasm)
+            .expect_func_instrs([
+                Instruction::global_get(Register::from_i16(2), GlobalIdx::from(0)),
+                expect_instr(
+                    Register::from_i16(0),
+                    Register::from_i16(1),
+                    BranchOffset16::from(2),
+                ),
+                Instruction::copy(Register::from_i16(2), Register::from_i16(0)),
+                Instruction::return_reg(2),
+            ])
+            .run()
+    }
+
+    test_for(ValueType::I32, "eq", Instruction::branch_i32_eq);
+    test_for(ValueType::I32, "ne", Instruction::branch_i32_ne);
+    test_for(ValueType::I32, "lt_s", Instruction::branch_i32_lt_s);
+    test_for(ValueType::I32, "lt_u", Instruction::branch_i32_lt_u);
+    test_for(ValueType::I32, "le_s", Instruction::branch_i32_le_s);
+    test_for(ValueType::I32, "le_u", Instruction::branch_i32_le_u);
+    test_for(ValueType::I32, "gt_s", Instruction::branch_i32_gt_s);
+    test_for(ValueType::I32, "gt_u", Instruction::branch_i32_gt_u);
+    test_for(ValueType::I32, "ge_s", Instruction::branch_i32_ge_s);
+    test_for(ValueType::I32, "ge_u", Instruction::branch_i32_ge_u);
+
+    test_for(ValueType::I64, "eq", Instruction::branch_i64_eq);
+    test_for(ValueType::I64, "ne", Instruction::branch_i64_ne);
+    test_for(ValueType::I64, "lt_s", Instruction::branch_i64_lt_s);
+    test_for(ValueType::I64, "lt_u", Instruction::branch_i64_lt_u);
+    test_for(ValueType::I64, "le_s", Instruction::branch_i64_le_s);
+    test_for(ValueType::I64, "le_u", Instruction::branch_i64_le_u);
+    test_for(ValueType::I64, "gt_s", Instruction::branch_i64_gt_s);
+    test_for(ValueType::I64, "gt_u", Instruction::branch_i64_gt_u);
+    test_for(ValueType::I64, "ge_s", Instruction::branch_i64_ge_s);
+    test_for(ValueType::I64, "ge_u", Instruction::branch_i64_ge_u);
+
+    test_for(ValueType::F32, "eq", Instruction::branch_f32_eq);
+    test_for(ValueType::F32, "ne", Instruction::branch_f32_ne);
+    test_for(ValueType::F32, "lt", Instruction::branch_f32_lt);
+    test_for(ValueType::F32, "le", Instruction::branch_f32_le);
+    test_for(ValueType::F32, "gt", Instruction::branch_f32_gt);
+    test_for(ValueType::F32, "ge", Instruction::branch_f32_ge);
+
+    test_for(ValueType::F64, "eq", Instruction::branch_f64_eq);
+    test_for(ValueType::F64, "ne", Instruction::branch_f64_ne);
+    test_for(ValueType::F64, "lt", Instruction::branch_f64_lt);
+    test_for(ValueType::F64, "le", Instruction::branch_f64_le);
+    test_for(ValueType::F64, "gt", Instruction::branch_f64_gt);
+    test_for(ValueType::F64, "ge", Instruction::branch_f64_ge);
 }
 
 #[test]
