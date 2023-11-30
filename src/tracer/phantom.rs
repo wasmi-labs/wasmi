9908ffe04cc16c6417ee35b9d440d1ae5fd5c749
sinka <xgao@zoyoe.com>
2023-11-30 21:24:33 +1100

    refactor of filltrace

--- src/tracer/phantom.rs
+++ src/tracer/phantom.rs
@@ -8,8 +8,10 @@ use specs::{
 use super::{etable::ETable, Tracer};
 use crate::{
     isa::{DropKeep, Instruction, Keep},
+    func::FuncRef,
     Signature,
 };
+
 
 pub struct PhantomFunction;
 
@@ -49,11 +51,9 @@ impl Tracer {
         &mut self,
         current_sp: u32,
         allocated_memory_pages: u32,
-        last_jump_eid: u32,
-        fid: u32,
+        callee_func_ref: &FuncRef,
         callee_sig: &Signature,
         // Wasm Image Function Id
-        wasm_input_function_idx: u32,
         keep_value: Option<u64>,
     ) {
         let has_return_value = callee_sig.return_type().is_some();
@@ -69,6 +69,12 @@ impl Tracer {
             self.inc_counter();
             return;
         }
+
+        let last_jump_eid = self.last_jump_eid();
+        let fid = self.lookup_function(callee_func_ref);
+        let wasm_input_function_idx = self.wasm_input_func_idx.unwrap();
+
+
 
         let mut inst = PhantomFunction::build_phantom_function_instructions(
             &callee_sig,
