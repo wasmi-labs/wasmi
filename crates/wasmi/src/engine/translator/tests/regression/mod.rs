fdd9364302ce63d80f8ff27375264ad72a98381c
Robin Freyler <robin.freyler@gmail.com>
2023-12-05 12:10:44 +0100

    Fix `local.set` preservation bug (#834)
    * add testcase
    
    * fix bug by avoiding no-op local.set translation
    
    * apply rustfmt
    
    * apply rustfmt (2)

--- crates/wasmi/src/engine/translator/tests/regression/mod.rs
+++ crates/wasmi/src/engine/translator/tests/regression/mod.rs
@@ -1,4 +1,5 @@
 use super::*;
+use crate::engine::bytecode::{BranchOffset, BranchOffset16};
 
 #[test]
 #[cfg_attr(miri, ignore)]
@@ -24,6 +25,20 @@ fn fuzz_regression_1() {
             Instruction::copy(1, 0),
             Instruction::copy_f64imm32(Register::from_i16(0), 13.0_f32),
             Instruction::return_reg(1),
+        ])
+        .run()
+}
+
+#[test]
+#[cfg_attr(miri, ignore)]
+fn fuzz_regression_2() {
+    let wat = include_str!("fuzz_2.wat");
+    let wasm = wat2wasm(wat);
+    TranslationTest::new(wasm)
+        .expect_func_instrs([
+            Instruction::branch_i32_eq_imm(Register::from_i16(0), 0, BranchOffset16::from(2)),
+            Instruction::branch(BranchOffset::from(1)),
+            Instruction::Return,
         ])
         .run()
 }
