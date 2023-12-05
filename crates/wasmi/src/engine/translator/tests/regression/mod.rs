cec647e16af77fd2926bee74a5a3578abd52b240
Robin Freyler <robin.freyler@gmail.com>
2023-12-05 12:00:35 +0100

    add test case

--- crates/wasmi/src/engine/translator/tests/regression/mod.rs
+++ crates/wasmi/src/engine/translator/tests/regression/mod.rs
@@ -1,4 +1,5 @@
 use super::*;
+use crate::engine::{bytecode::RegisterSpan, CompiledFunc};
 
 #[test]
 #[cfg_attr(miri, ignore)]
@@ -24,6 +25,33 @@ fn fuzz_regression_1() {
             Instruction::copy(1, 0),
             Instruction::copy_f64imm32(Register::from_i16(0), 13.0_f32),
             Instruction::return_reg(1),
+        ])
+        .run()
+}
+
+#[test]
+#[cfg_attr(miri, ignore)]
+fn fuzz_regression_3() {
+    let wat = include_str!("fuzz_3.wat");
+    let wasm = wat2wasm(wat);
+    TranslationTest::new(wasm)
+        .expect_func_instrs([
+            Instruction::call_internal_0(
+                RegisterSpan::new(Register::from_i16(0)),
+                CompiledFunc::from_u32(0),
+            ),
+            Instruction::call_internal_0(
+                RegisterSpan::new(Register::from_i16(3)),
+                CompiledFunc::from_u32(0),
+            ),
+            Instruction::copy_span_non_overlapping(
+                RegisterSpan::new(Register::from_i16(0)),
+                RegisterSpan::new(Register::from_i16(2)),
+                3,
+            ),
+            Instruction::branch_table(Register::from_i16(5), 2),
+            Instruction::return_span(RegisterSpan::new(Register::from_i16(0)).iter_u16(3)),
+            Instruction::return_span(RegisterSpan::new(Register::from_i16(0)).iter_u16(3)),
         ])
         .run()
 }
