05a7f26d4de720d27f9a887e28929f6e242f71a5
Robin Freyler <robin.freyler@gmail.com>
2024-02-04 14:05:51 +0100

    apply clippy suggestion

--- crates/wasmi/src/engine/translator/visit_register.rs
+++ crates/wasmi/src/engine/translator/visit_register.rs
@@ -255,7 +255,7 @@ impl VisitInputRegisters for Instruction {
             Instruction::GlobalSet { global: _, input } => f(input),
             Instruction::GlobalSetI32Imm16 { global: _, input: _ } |
             Instruction::GlobalSetI64Imm16 { global: _, input: _ } => {},
-            Instruction::I32AddImmIntoGlobal0 { lhs, rhs } => f(lhs),
+            Instruction::I32AddImmIntoGlobal0 { lhs, rhs: _ } => f(lhs),
             Instruction::I32AddImmFromGlobal0 { result: _, rhs: _ } |
             Instruction::I32AddImmInoutGlobal0 { result: _, rhs: _ } => {},
             Instruction::I32Load(instr) => instr.visit_input_registers(f),
