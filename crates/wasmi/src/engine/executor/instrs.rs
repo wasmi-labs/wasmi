874dd5fee98b31f9d9c4417ed29ab35b04a25d47
Robin Freyler <robin.freyler@gmail.com>
2024-02-04 14:06:10 +0100

    implement execution of new instructions

--- crates/wasmi/src/engine/executor/instrs.rs
+++ crates/wasmi/src/engine/executor/instrs.rs
@@ -555,9 +555,15 @@ impl<'ctx, 'engine> Executor<'ctx, 'engine> {
                 Instr::GlobalSetI64Imm16 { global, input } => {
                     self.execute_global_set_i64imm16(global, input)
                 }
-                Instr::I32AddImmIntoGlobal0 { lhs, rhs } => todo!(),
-                Instr::I32AddImmFromGlobal0 { result, rhs } => todo!(),
-                Instr::I32AddImmInoutGlobal0 { result, rhs } => todo!(),
+                Instr::I32AddImmIntoGlobal0 { lhs, rhs } => {
+                    self.execute_i32_add_imm_into_global_0(lhs, rhs)
+                }
+                Instr::I32AddImmFromGlobal0 { result, rhs } => {
+                    self.execute_i32_add_imm_from_global_0(result, rhs)
+                }
+                Instr::I32AddImmInoutGlobal0 { result, rhs } => {
+                    self.execute_i32_add_imm_inout_global_0(result, rhs)
+                }
                 Instr::I32Load(instr) => self.execute_i32_load(instr)?,
                 Instr::I32LoadAt(instr) => self.execute_i32_load_at(instr)?,
                 Instr::I32LoadOffset16(instr) => self.execute_i32_load_offset16(instr)?,
--- crates/wasmi/src/engine/executor/instrs/global.rs
+++ crates/wasmi/src/engine/executor/instrs/global.rs
@@ -1,5 +1,5 @@
 use super::Executor;
-use crate::engine::bytecode::{Const16, GlobalIdx, Register};
+use crate::engine::bytecode::{Const16, Const32, GlobalIdx, Register};
 use wasmi_core::UntypedValue;
 
 #[cfg(doc)]
@@ -38,6 +38,38 @@ impl<'ctx, 'engine> Executor<'ctx, 'engine> {
     /// Executes a generic `global.set` instruction.
     fn execute_global_set_impl(&mut self, global: GlobalIdx, new_value: UntypedValue) {
         self.cache.set_global(self.ctx, global, new_value);
+        self.next_instr()
+    }
+
+    /// Executes an [`Instruction::I32AddImmIntoGlobal0`].
+    #[inline(always)]
+    pub fn execute_i32_add_imm_into_global_0(&mut self, lhs: Register, rhs: Const32<i32>) {
+        let lhs: i32 = self.get_register_as(lhs);
+        let rhs: i32 = i32::from(rhs);
+        let result = lhs.wrapping_add(rhs);
+        self.cache
+            .set_global(self.ctx, GlobalIdx::from(0), result.into());
+        self.next_instr()
+    }
+
+    /// Executes an [`Instruction::I32AddImmFromGlobal0`].
+    #[inline(always)]
+    pub fn execute_i32_add_imm_from_global_0(&mut self, result: Register, rhs: Const32<i32>) {
+        let lhs: i32 = self.cache.get_global(self.ctx, GlobalIdx::from(0)).into();
+        let rhs: i32 = rhs.into();
+        self.set_register(result, lhs.wrapping_add(rhs));
+        self.next_instr()
+    }
+
+    /// Executes an [`Instruction::I32AddImmInoutGlobal0`].
+    #[inline(always)]
+    pub fn execute_i32_add_imm_inout_global_0(&mut self, result: Register, rhs: Const32<i32>) {
+        let lhs: i32 = self.cache.get_global(self.ctx, GlobalIdx::from(0)).into();
+        let rhs: i32 = rhs.into();
+        let sum = lhs.wrapping_add(rhs);
+        self.cache
+            .set_global(self.ctx, GlobalIdx::from(0), sum.into());
+        self.set_register(result, sum);
         self.next_instr()
     }
 }
