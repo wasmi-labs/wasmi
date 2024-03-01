5fa388f9a852439ddc1271e72727b3549dbfcc16
Robin Freyler <robin.freyler@gmail.com>
2024-03-01 09:34:44 +0100

    add some regression tests

---
+++ crates/wasmi/src/engine/translator/tests/regression/conditional_preserve.rs
@@ -0,0 +1,101 @@
+use super::*;
+use crate::engine::bytecode::BranchOffset16;
+
+#[test]
+#[cfg_attr(miri, ignore)]
+fn simple() {
+    let wasm = wat2wasm(
+        r#"
+        (module
+            (func (param i32 i32) (result i32)
+                local.get 0
+                block
+                    (br_if 0 (local.get 1))
+                    (local.set 0 (i32.const 10)) ;; overwrites (local 0) conditionally
+                end
+            )
+        )
+    "#,
+    );
+    TranslationTest::new(wasm)
+        .expect_func_instrs([
+            Instruction::branch_i32_ne_imm(Register::from_i16(1), 0, BranchOffset16::from(3)),
+            Instruction::copy(2, 0),
+            Instruction::copy_imm32(Register::from_i16(0), 10_i32),
+            Instruction::return_reg(2),
+        ])
+        .run()
+}
+
+#[test]
+#[cfg_attr(miri, ignore)]
+fn nested() {
+    let wasm = wat2wasm(
+        r#"
+        (module
+            (func (param i32 i32) (param $c0 i32) (param $c1 i32) (result i32 i32)
+                local.get 0 ;; 1st return value
+                local.get 1 ;; 2nd return value
+                block
+                    (br_if 0 (local.get $c0))
+                    (local.set 0 (i32.const 10)) ;; conditionally overwrites (local 0) on stack
+                    block
+                        (br_if 1 (local.get $c1))
+                        (local.set 1 (i32.const 20)) ;; conditionally overwrites (local 1) on stack
+                    end
+                end
+            )
+        )
+    "#,
+    );
+    TranslationTest::new(wasm)
+        .expect_func_instrs([
+            Instruction::branch_i32_ne_imm(Register::from_i16(2), 0, BranchOffset16::from(6)),
+            Instruction::copy(5, 0),
+            Instruction::copy_imm32(Register::from_i16(0), 10_i32),
+            Instruction::branch_i32_ne_imm(Register::from_i16(3), 0, BranchOffset16::from(3)),
+            Instruction::copy(4, 1),
+            Instruction::copy_imm32(Register::from_i16(1), 20_i32),
+            Instruction::return_reg2(5, 4),
+        ])
+        .run()
+}
+
+#[test]
+#[cfg_attr(miri, ignore)]
+fn block_with_result() {
+    let wasm = wat2wasm(
+        r#"
+        (module
+            (func (param i32 i32 i32) (result i32)
+                (select
+                    (local.get 0)       ;; lhs - uses (local 0) that is conditionally overwritten
+                    (block (result i32) ;; rhs - conditionally overwriting (local 0)
+                        (drop (br_if 0
+                            (local.get 1) ;; br_if return value
+                            (local.get 2) ;; br_if condition
+                        ))
+                        (local.set 0 (i32.const 0)) ;; overwrites (local 0) after `br_if` conditionally
+                        (i32.const 1)
+                    )
+                    ;; condition - overwriting (local 0)
+                    (local.tee 0
+                        (i32.lt_s (local.get 1) (local.get 2))
+                    )
+                )
+            )
+        )
+    "#,
+    );
+    TranslationTest::new(wasm)
+        .expect_func_instrs([
+            Instruction::branch_i32_ne_imm(Register::from_i16(2), 0, BranchOffset16::from(6)),
+            Instruction::copy(5, 0),
+            Instruction::copy_imm32(Register::from_i16(0), 10_i32),
+            Instruction::branch_i32_ne_imm(Register::from_i16(3), 0, BranchOffset16::from(3)),
+            Instruction::copy(4, 1),
+            Instruction::copy_imm32(Register::from_i16(1), 20_i32),
+            Instruction::return_reg2(5, 4),
+        ])
+        .run()
+}
--- crates/wasmi/src/engine/translator/tests/regression/mod.rs
+++ crates/wasmi/src/engine/translator/tests/regression/mod.rs
@@ -1,3 +1,5 @@
+mod conditional_preserve;
+
 use super::*;
 use crate::{
     core::TrapCode,
