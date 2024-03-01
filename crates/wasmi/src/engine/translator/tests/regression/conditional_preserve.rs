3b3d8325c31f7a549b8512cfd02697e30a664073
Robin Freyler <robin.freyler@gmail.com>
2024-03-01 14:23:12 +0100

    simplify expression test

--- crates/wasmi/src/engine/translator/tests/regression/conditional_preserve.rs
+++ crates/wasmi/src/engine/translator/tests/regression/conditional_preserve.rs
@@ -63,39 +63,37 @@ fn nested_block() {
 
 #[test]
 #[cfg_attr(miri, ignore)]
-fn block_with_result() {
+fn expr_block() {
     let wasm = wat2wasm(
         r#"
         (module
-            (func (param i32 i32 i32) (result i32)
-                (select
-                    (local.get 0)       ;; lhs - uses (local 0) that is conditionally overwritten
-                    (block (result i32) ;; rhs - conditionally overwriting (local 0)
+            (func (param i32 i32) (result i32)
+                (i32.add
+                    (local.get 1)
+                    (block (result i32)
                         (drop (br_if 0
-                            (local.get 1) ;; br_if return value
-                            (local.get 2) ;; br_if condition
+                            (i32.const 10) ;; br_if return value
+                            (local.get 0)  ;; br_if condition
                         ))
-                        (local.set 0 (i32.const 0)) ;; overwrites (local 0) after `br_if` conditionally
-                        (i32.const 1)
-                    )
-                    ;; condition - overwriting (local 0)
-                    (local.tee 0
-                        (i32.lt_s (local.get 1) (local.get 2))
+                        (local.set 1 (i32.const 20))
+                        (i32.const 30)
                     )
                 )
             )
         )
-    "#,
+        "#,
     );
     TranslationTest::new(wasm)
         .expect_func_instrs([
-            Instruction::branch_i32_ne_imm(Register::from_i16(2), 0, BranchOffset16::from(6)),
-            Instruction::copy(5, 0),
-            Instruction::copy_imm32(Register::from_i16(0), 10_i32),
-            Instruction::branch_i32_ne_imm(Register::from_i16(3), 0, BranchOffset16::from(3)),
-            Instruction::copy(4, 1),
+            // What Wasmi incorrectly produces:
+            Instruction::branch_i32_eq_imm(Register::from_i16(0), 0, BranchOffset16::from(3)),
+            Instruction::copy_imm32(Register::from_i16(2), 10_i32),
+            Instruction::branch(BranchOffset::from(4)),
+            Instruction::copy(3, 1),
             Instruction::copy_imm32(Register::from_i16(1), 20_i32),
-            Instruction::return_reg2(5, 4),
+            Instruction::copy_imm32(Register::from_i16(2), 30_i32),
+            Instruction::i32_add(Register::from_i16(2), Register::from_i16(3), Register::from_i16(2)),
+            Instruction::return_reg(2),
         ])
         .run()
 }
