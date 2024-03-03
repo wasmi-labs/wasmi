1b76150a4ca61c986498748284b30d38ca536baf
Robin Freyler <robin.freyler@gmail.com>
2024-03-03 11:50:58 +0100

    add if based local preservation tests

--- crates/wasmi/src/engine/translator/tests/regression/conditional_preserve.rs
+++ crates/wasmi/src/engine/translator/tests/regression/conditional_preserve.rs
@@ -29,6 +29,33 @@ fn simple_block() {
 
 #[test]
 #[cfg_attr(miri, ignore)]
+fn simple_if() {
+    let wasm = wat2wasm(
+        r#"
+        (module
+            (func (param i32 i32 i32) (result i32)
+                local.get 0
+                (if (local.get 2)
+                    (then
+                        (local.set 0 (i32.const 10)) ;; overwrites (local 0) conditionally
+                    )
+                )
+            )
+        )
+    "#,
+    );
+    TranslationTest::new(wasm)
+        .expect_func_instrs([
+            Instruction::copy(3, 0),
+            Instruction::branch_i32_eq_imm(Register::from_i16(2), 0, BranchOffset16::from(2)),
+            Instruction::copy_imm32(Register::from_i16(0), 10_i32),
+            Instruction::return_reg(3),
+        ])
+        .run()
+}
+
+#[test]
+#[cfg_attr(miri, ignore)]
 fn nested_block() {
     let wasm = wat2wasm(
         r#"
@@ -55,6 +82,42 @@ fn nested_block() {
             Instruction::branch_i32_ne_imm(Register::from_i16(2), 0, BranchOffset16::from(4)),
             Instruction::copy_imm32(Register::from_i16(0), 10_i32),
             Instruction::branch_i32_ne_imm(Register::from_i16(3), 0, BranchOffset16::from(2)),
+            Instruction::copy_imm32(Register::from_i16(1), 20_i32),
+            Instruction::return_reg2(5, 4),
+        ])
+        .run()
+}
+
+#[test]
+#[cfg_attr(miri, ignore)]
+fn nested_if() {
+    let wasm = wat2wasm(
+        r#"
+        (module
+            (func (param i32 i32 i32 i32) (result i32 i32)
+                local.get 0 ;; 1st return value
+                local.get 1 ;; 2nd return value
+                (if (local.get 2)
+                    (then
+                        (local.set 0 (i32.const 10)) ;; overwrites (local 0) conditionally
+                        (if (local.get 3)
+                            (then
+                                (local.set 1 (i32.const 20)) ;; overwrites (local 1) conditionally
+                            )
+                        )
+                    )
+                )
+            )
+        )
+    "#,
+    );
+    TranslationTest::new(wasm)
+        .expect_func_instrs([
+            Instruction::copy(5, 0),
+            Instruction::copy(4, 1),
+            Instruction::branch_i32_eq_imm(Register::from_i16(2), 0, BranchOffset16::from(4)),
+            Instruction::copy_imm32(Register::from_i16(0), 10_i32),
+            Instruction::branch_i32_eq_imm(Register::from_i16(3), 0, BranchOffset16::from(2)),
             Instruction::copy_imm32(Register::from_i16(1), 20_i32),
             Instruction::return_reg2(5, 4),
         ])
@@ -97,6 +160,47 @@ fn expr_block() {
                 Register::from_i16(2),
             ),
             Instruction::return_reg(2),
+        ])
+        .run()
+}
+
+#[test]
+#[cfg_attr(miri, ignore)]
+fn expr_if() {
+    let wasm = wat2wasm(
+        r#"
+        (module
+            (func (param i32 i32 i32) (result i32)
+                (i32.add
+                    (local.get 0)
+                    (if (result i32) (local.get 1)
+                        (then
+                            (local.set 0 (i32.const 10))
+                            (i32.const 20)
+                        )
+                        (else
+                            (i32.const 30)
+                        )
+                    )
+                )
+            )
+        )
+        "#,
+    );
+    TranslationTest::new(wasm)
+        .expect_func_instrs([
+            Instruction::copy(4, 0),
+            Instruction::branch_i32_eq_imm(Register::from_i16(1), 0, BranchOffset16::from(4)),
+            Instruction::copy_imm32(Register::from_i16(0), 10_i32),
+            Instruction::copy_imm32(Register::from_i16(3), 20_i32),
+            Instruction::branch(BranchOffset::from(2)),
+            Instruction::copy_imm32(Register::from_i16(3), 30_i32),
+            Instruction::i32_add(
+                Register::from_i16(3),
+                Register::from_i16(4),
+                Register::from_i16(3),
+            ),
+            Instruction::return_reg(3),
         ])
         .run()
 }
