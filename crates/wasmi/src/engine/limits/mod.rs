d030bf874afbe496e64b3800fc9f044ec1a75b06
Robin Freyler <robin.freyler@gmail.com>
2024-04-20 18:44:16 +0200

    remove commented out code

--- crates/wasmi/src/engine/limits/mod.rs
+++ crates/wasmi/src/engine/limits/mod.rs
@@ -4,10 +4,6 @@ mod stack;
 #[cfg(test)]
 mod tests;
 
-// use self::{
-//     engine::{EngineLimits, EngineLimitsError, AvgBytesPerFunctionLimit},
-//     stack::{StackLimits, LimitsError},
-// };
 pub use self::{
     engine::{EngineLimits, EngineLimitsError},
     stack::{LimitsError, StackLimits},
