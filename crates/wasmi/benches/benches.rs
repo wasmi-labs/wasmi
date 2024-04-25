6d067a95074405e64c9493efa276625cf154cccb
Robin Freyler <robin.freyler@gmail.com>
2024-04-25 14:49:00 +0200

    add some malicous translation benchmarks

--- crates/wasmi/benches/benches.rs
+++ crates/wasmi/benches/benches.rs
@@ -10,6 +10,7 @@ use self::bench::{
 use bench::bench_config;
 use core::{slice, time::Duration};
 use criterion::{criterion_group, criterion_main, Bencher, Criterion};
+use std::fmt::{self, Display};
 use wasmi::{
     core::TrapCode,
     CompilationMode,
@@ -39,6 +40,10 @@ criterion_group!(
         bench_translate_erc20,
         bench_translate_erc721,
         bench_translate_erc1155,
+        bench_translate_case_memcpy_memset,
+        bench_translate_case_best,
+        bench_translate_case_worst_stackbomb_16,
+        bench_translate_case_worst_stackbomb_1000,
 );
 criterion_group!(
     name = bench_instantiate;
@@ -247,6 +252,139 @@ fn bench_translate_erc721(c: &mut Criterion) {
 
 fn bench_translate_erc1155(c: &mut Criterion) {
     bench_translate_for_all(c, "erc1155", "benches/wasm/erc1155.wasm");
+}
+
+fn bench_translate_case_memcpy_memset(c: &mut Criterion) {
+    c.bench_function("translate/case/memcpy_memset", |b| {
+        let len = 8_000_000;
+        let src = vec![0xFF; len];
+        let mut dst = vec![0x00; len];
+        b.iter(|| {
+            dst.copy_from_slice(&src);
+            dst.fill(0x00);
+        })
+    });
+}
+
+fn bench_translate_case_best(c: &mut Criterion) {
+    pub struct Generator(usize);
+    impl Display for Generator {
+        fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+            for _ in 0..self.0 {
+                // a = b + (c * d)
+                writeln!(
+                    f,
+                    "(local.set $a \
+                        (i64.add \
+                            (local.get $b) \
+                            (i64.mul \
+                                (local.get $c) (local.get $d)\
+                            )\
+                        )\
+                    )",
+                )?;
+            }
+            Ok(())
+        }
+    }
+    c.bench_function("translate/case/best", |b| {
+        let gen = Generator(1_000_000);
+        let wat = format!(
+            "\
+            (module
+                (func (export \"test\") (result i64)
+                    (local $a i64)
+                    (local $b i64)
+                    (local $c i64)
+                    (local $d i64)
+                    (local.set $a (i64.const 1))
+                    (local.set $b (i64.const 2))
+                    (local.set $c (i64.const 3))
+                    (local.set $d (i64.const 4))
+                    {gen}
+                    (local.get $a)
+                )
+            )
+        "
+        );
+        let wasm = wat2wasm(wat.as_bytes());
+        b.iter(|| {
+            let engine = Engine::default();
+            let _ = Module::new(&engine, &wasm[..]).unwrap();
+        })
+    });
+}
+
+pub struct WasmCompileStackBomb {
+    locals: usize,
+    repetitions: usize,
+}
+impl Display for WasmCompileStackBomb {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "(local")?;
+        for _ in 0..self.locals {
+            write!(f, " i64")?;
+        }
+        writeln!(f, ")")?;
+        for i in 0..self.locals {
+            writeln!(f, "(local.get {})", i)?;
+        }
+        for i in 0..self.repetitions {
+            let src = i % self.locals;
+            let dst = (i + 1) % self.locals;
+            writeln!(f, "(local.set {dst} (local.get {src}))")?;
+        }
+        for _ in 0..self.locals {
+            writeln!(f, "(drop)")?;
+        }
+        Ok(())
+    }
+}
+
+fn bench_translate_case_worst_stackbomb_16(c: &mut Criterion) {
+    c.bench_function("translate/case/worst/stackbomb/16", |b| {
+        let gen = WasmCompileStackBomb {
+            locals: 16,
+            repetitions: 1_000_000,
+        };
+        let wat = format!(
+            "\
+            (module
+                (func (export \"test\")
+                    {gen}
+                )
+            )
+        "
+        );
+        let wasm = wat2wasm(wat.as_bytes());
+        b.iter(|| {
+            let engine = Engine::default();
+            let _ = Module::new(&engine, &wasm[..]).unwrap();
+        })
+    });
+}
+
+fn bench_translate_case_worst_stackbomb_1000(c: &mut Criterion) {
+    c.bench_function("translate/case/worst/stackbomb/1000", |b| {
+        let gen = WasmCompileStackBomb {
+            locals: 10_000,
+            repetitions: 1_000_000,
+        };
+        let wat = format!(
+            "\
+            (module
+                (func (export \"test\")
+                    {gen}
+                )
+            )
+        "
+        );
+        let wasm = wat2wasm(wat.as_bytes());
+        b.iter(|| {
+            let engine = Engine::default();
+            let _ = Module::new(&engine, &wasm[..]).unwrap();
+        })
+    });
 }
 
 fn bench_instantiate_wasm_kernel(c: &mut Criterion) {
