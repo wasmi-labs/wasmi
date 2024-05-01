fea754e2786b448e7fd297e8899d033badda5715
Robin Freyler <robin.freyler@gmail.com>
2024-05-01 15:32:13 +0200

    c-api: add lib docs

--- crates/capi/src/lib.rs
+++ crates/capi/src/lib.rs
@@ -1,3 +1,7 @@
+//! Implements C-API support for the Wasmi WebAssembly interpreter.
+//! 
+//! Namely implements the Wasm C-API proposal found here: <https://github.com/WebAssembly/wasm-c-api/>
+
 #![no_std]
 #![allow(non_snake_case, non_camel_case_types, non_upper_case_globals)]
 
