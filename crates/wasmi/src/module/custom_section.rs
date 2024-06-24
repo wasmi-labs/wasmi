382465193f1e71ee0dd5c3ecd9ca2c4934405bd6
Robin Freyler <robin.freyler@gmail.com>
2024-06-24 15:24:37 +0200

    optimize memory allocations of custom sections
    Now all names and data of all custom sections are stored in a single vector instead of having many small allocations for many custom sections. This is a guard against malicious Wasm inputs with tons of tiny custom sections putting heavy strain on the memory allocator. Also with this implementation Drop can be simpler, only having to deallocate two vectors.

--- crates/wasmi/src/module/custom_section.rs
+++ crates/wasmi/src/module/custom_section.rs
@@ -1,84 +1,128 @@
 use core::slice;
-use std::{boxed::Box, vec::Vec};
+use std::{str, vec::Vec};
 
 /// Wasm custom sections.
 #[derive(Default, Debug)]
 pub struct CustomSections {
-    items: Vec<CustomSection>,
+    inner: CustomSectionsInner,
 }
 
 impl CustomSections {
     /// Returns an iterator over the [`CustomSection`]s stored in `self`.
     #[inline]
     pub fn iter(&self) -> CustomSectionsIter {
-        CustomSectionsIter {
-            iter: self.items.iter(),
-        }
+        self.inner.iter()
     }
 }
 
 /// A builder for [`CustomSections`].
 #[derive(Default, Debug)]
 pub struct CustomSectionsBuilder {
-    items: Vec<CustomSection>,
+    inner: CustomSectionsInner,
 }
 
 impl CustomSectionsBuilder {
     /// Pushes a new custom section segment to the [`CustomSectionsBuilder`].
     #[inline]
     pub fn push(&mut self, name: &str, data: &[u8]) {
-        self.items.push(CustomSection {
-            name: name.into(),
-            data: data.into(),
-        })
+        self.inner.push(name, data);
     }
 
     /// Finalize construction of the [`CustomSections`].
     #[inline]
     pub fn finish(self) -> CustomSections {
-        CustomSections { items: self.items }
+        CustomSections { inner: self.inner }
+    }
+}
+
+/// Internal representation of [`CustomSections`].
+#[derive(Debug, Default)]
+pub struct CustomSectionsInner {
+    /// The name and data lengths of each Wasm custom section.
+    items: Vec<CustomSectionInner>,
+    /// The combined name and data of all Wasm custom sections.
+    names_and_data: Vec<u8>,
+}
+
+/// Internal representation of a Wasm [`CustomSection`].
+#[derive(Debug, Copy, Clone)]
+pub struct CustomSectionInner {
+    /// The length in bytes of the Wasm custom section name.
+    len_name: usize,
+    /// The length in bytes of the Wasm custom section data.
+    len_data: usize,
+}
+
+impl CustomSectionsInner {
+    /// Pushes a new custom section segment to the [`CustomSectionsBuilder`].
+    #[inline]
+    pub fn push(&mut self, name: &str, data: &[u8]) {
+        let name_bytes = name.as_bytes();
+        self.names_and_data.extend_from_slice(name_bytes);
+        self.names_and_data.extend_from_slice(data);
+        self.items.push(CustomSectionInner {
+            len_name: name_bytes.len(),
+            len_data: data.len(),
+        })
+    }
+
+    /// Returns an iterator over the [`CustomSection`]s stored in `self`.
+    #[inline]
+    pub fn iter(&self) -> CustomSectionsIter {
+        CustomSectionsIter {
+            items: self.items.iter(),
+            names_and_data: &self.names_and_data[..],
+        }
     }
 }
 
 /// The data of a Wasm custom section.
 #[derive(Debug)]
-pub struct CustomSection {
+pub struct CustomSection<'a> {
     /// The name of the custom section.
-    name: Box<str>,
+    name: &'a str,
     /// The undecoded data of the custom section.
-    data: Box<[u8]>,
+    data: &'a [u8],
 }
 
-impl CustomSection {
+impl<'a> CustomSection<'a> {
     /// Returns the name or identifier of the [`CustomSection`].
     #[inline]
-    pub fn name(&self) -> &str {
-        &self.name
+    pub fn name(&self) -> &'a str {
+        self.name
     }
 
     /// Returns a shared reference to the data of the [`CustomSection`].
     #[inline]
-    pub fn data(&self) -> &[u8] {
-        &self.data
+    pub fn data(&self) -> &'a [u8] {
+        self.data
     }
 }
 
 /// An iterator over the custom sections of a Wasm module.
 #[derive(Debug)]
 pub struct CustomSectionsIter<'a> {
-    iter: slice::Iter<'a, CustomSection>,
+    items: slice::Iter<'a, CustomSectionInner>,
+    names_and_data: &'a [u8],
 }
 
 impl<'a> Iterator for CustomSectionsIter<'a> {
-    type Item = &'a CustomSection;
+    type Item = CustomSection<'a>;
 
     #[inline]
     fn size_hint(&self) -> (usize, Option<usize>) {
-        self.iter.size_hint()
+        self.items.size_hint()
     }
 
     #[inline]
     fn next(&mut self) -> Option<Self::Item> {
-        self.iter.next()
+        let item = self.items.next()?;
+        let names_and_data = self.names_and_data;
+        let (name, names_and_data) = names_and_data.split_at(item.len_name);
+        let (data, names_and_data) = names_and_data.split_at(item.len_data);
+        self.names_and_data = names_and_data;
+        // Safety: We encoded this part of the data buffer from the bytes of a string previously.
+        let name = unsafe { str::from_utf8_unchecked(name) };
+        Some(CustomSection { name, data })
     }
 }
