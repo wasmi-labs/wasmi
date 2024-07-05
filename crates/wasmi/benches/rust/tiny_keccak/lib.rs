bf55b78b2d9db471cdf1b5de30f04aa0370ac68a
Robin Freyler <robin.freyler@gmail.com>
2024-07-05 17:08:44 +0200

    rename and add docs

--- crates/wasmi/benches/rust/tiny_keccak/lib.rs
+++ crates/wasmi/benches/rust/tiny_keccak/lib.rs
@@ -1,14 +1,17 @@
 use tiny_keccak::{Hasher, Keccak};
 
+/// The data required to run the `tiny_keccak` benchmark.
 pub struct TinyKeccakData {
-    data: Box<[u8; 4096]>,
+    /// The input data to the tiny keccak hasher.
+    input: Box<[u8; 4096]>,
+    /// The buffer to store the hash result.
     result: [u8; 32],
 }
 
 #[no_mangle]
 pub extern "C" fn setup() -> Box<TinyKeccakData> {
     Box::new(TinyKeccakData {
-        data: Box::new([254_u8; 4096]),
+        input: Box::new([254_u8; 4096]),
         result: [0x0_u8; 32],
     })
 }
@@ -19,6 +22,6 @@ pub extern "C" fn teardown(_: Box<TinyKeccakData>) {}
 #[no_mangle]
 pub extern "C" fn run(data: &mut TinyKeccakData) {
     let mut keccak = Keccak::v256();
-    keccak.update(&data.data[..]);
+    keccak.update(&data.input[..]);
     keccak.finalize(&mut data.result[..]);
 }
