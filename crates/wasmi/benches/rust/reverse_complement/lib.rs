c75a1566ac7ea4a061f201c680ffdc0a1b00d758
Robin Freyler <robin.freyler@gmail.com>
2024-07-05 18:43:16 +0200

    clean-up reserve_complement lib.rs externals

--- crates/wasmi/benches/rust/reverse_complement/lib.rs
+++ crates/wasmi/benches/rust/reverse_complement/lib.rs
@@ -1,51 +1,35 @@
-use std::mem::ManuallyDrop;
-
 mod impl_;
 use impl_ as rev_complement;
 
+#[repr(C)]
 pub struct RevComplementData {
-    input: ManuallyDrop<Box<[u8]>>,
-    output: ManuallyDrop<Box<[u8]>>,
+    input: Box<[u8]>,
+    output: Box<[u8]>,
 }
 
 #[no_mangle]
-pub extern "C" fn setup(size: usize) -> *mut RevComplementData {
-    let input = vec![0; size];
-    let output = vec![0; size];
-
-    let test_data = Box::new(RevComplementData {
-        input: ManuallyDrop::new(input.into_boxed_slice()),
-        output: ManuallyDrop::new(output.into_boxed_slice()),
-    });
-
-    // Basically leak the pointer to the test data. This shouldn't be harmful since `prepare` is called
-    // only once per bench run (not for the iteration), and afterwards whole memory instance is discarded.
-    Box::into_raw(test_data)
+pub extern "C" fn setup(size: usize) -> Box<RevComplementData> {
+    Box::new(RevComplementData {
+        input: vec![0; size].into(),
+        output: vec![0; size].into(),
+    })
 }
 
-/// # Safety
-///
-/// It is the caller's responsibility to provide non `null` `test_data`.
 #[no_mangle]
-pub unsafe extern "C" fn input_ptr(test_data: *mut RevComplementData) -> *mut u8 {
-    (*test_data).input.as_mut_ptr()
 pub extern "C" fn teardown(_: Box<RevComplementData>) {}
 
+#[no_mangle]
+pub extern "C" fn input_ptr(data: &mut RevComplementData) -> *mut u8 {
+    data.input.as_mut_ptr()
 }
 
-/// # Safety
-///
-/// It is the caller's responsibility to provide non `null` `test_data`.
 #[no_mangle]
-pub unsafe extern "C" fn output_ptr(test_data: *mut RevComplementData) -> *const u8 {
-    (*test_data).output.as_ptr()
+pub extern "C" fn output_ptr(data: &RevComplementData) -> *const u8 {
+    data.output.as_ptr()
 }
 
-/// # Safety
-///
-/// It is the caller's responsibility to provide non `null` `test_data`.
 #[no_mangle]
-pub unsafe extern "C" fn run(test_data: *mut RevComplementData) {
-    let result = rev_complement::run(&(*test_data).input);
-    (*test_data).output.copy_from_slice(&result);
+pub extern "C" fn run(data: &mut RevComplementData) {
+    let result = rev_complement::run(&data.input[..]);
+    data.output.copy_from_slice(&result[..]);
 }
