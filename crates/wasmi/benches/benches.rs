49cd5a38a575a65faca0ec0928bc0e23e4c4ca0c
Robin Freyler <robin.freyler@gmail.com>
2024-07-06 19:42:15 +0200

    add benchmark for nested calls with various arities

--- crates/wasmi/benches/benches.rs
+++ crates/wasmi/benches/benches.rs
@@ -117,6 +117,7 @@ criterion_group! {
         bench_execute_recursive_scan,
         bench_execute_recursive_trap,
         bench_execute_flat_calls,
+        bench_execute_nested_calls,
         bench_execute_host_calls,
         bench_execute_fuse,
         bench_execute_divrem,
@@ -1143,6 +1144,31 @@ fn bench_execute_flat_calls(c: &mut Criterion) {
     let wasm = include_bytes!("wat/flat_calls.wat");
     let mut g = c.benchmark_group("execute/call/flat");
     for n in [0, 1, 8, 16] {
+        bench_with(&mut g, wasm, n);
+    }
+}
+
+fn bench_execute_nested_calls(c: &mut Criterion) {
+    fn bench_with(g: &mut BenchmarkGroup<WallTime>, wasm: &[u8], n: usize) {
+        /// How often the host functions are called per benchmark run.
+        const ITERATIONS: i64 = 1000;
+
+        let id = format!("{n}");
+        g.bench_function(&id, |b| {
+            let (mut store, instance) = load_instance_from_wat(wasm);
+            let func_name = format!("run/{n}");
+            let run = instance
+                .get_typed_func::<i64, i64>(&store, &func_name)
+                .unwrap();
+            b.iter(|| {
+                run.call(&mut store, ITERATIONS).unwrap();
+            });
+        });
+    }
+
+    let wasm = include_bytes!("wat/nested_calls.wat");
+    let mut g = c.benchmark_group("execute/call/nested");
+    for n in [1, 8, 16] {
         bench_with(&mut g, wasm, n);
     }
 }
