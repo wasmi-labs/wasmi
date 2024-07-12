c180d09ab40fa7d5ee7d583e386837ac4fbd8bf7
Robin Freyler <robin.freyler@gmail.com>
2024-07-12 16:53:15 +0200

    reorganize capi folder

--- crates/capi/src/lib.rs
+++ crates/capi/src/lib.rs
@@ -1,1 +1,15 @@
-pub use wasmi_c_api::*;
+//! Implements C-API support for the Wasmi WebAssembly interpreter.
+//!
+//! Namely implements the Wasm C-API proposal found here: <https://github.com/WebAssembly/wasm-c-api/>
+
+#![no_std]
+#![allow(non_snake_case, non_camel_case_types, non_upper_case_globals)]
+
+extern crate alloc;
+
+pub use wasmi;
+
+mod config;
+mod engine;
+
+pub use self::{config::*, engine::*};
