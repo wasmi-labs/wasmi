a36c651f2f55e8d9ec0bfce01925e253fbfbee27
Robin Freyler <robin.freyler@gmail.com>
2024-07-22 01:36:39 +0200

    silence dead_code warnings for Wasmi C-API for now

--- crates/c_api/src/lib.rs
+++ crates/c_api/src/lib.rs
@@ -4,6 +4,7 @@
 
 #![no_std]
 #![allow(non_snake_case, non_camel_case_types, non_upper_case_globals)]
+#![allow(dead_code)] // TODO: remove when done implementing Wasmi C-API
 
 extern crate alloc;
 
