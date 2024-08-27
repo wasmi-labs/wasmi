966e3724912e623bf700efde8ebc3ef13229862f
Robin Freyler <robin.freyler@gmail.com>
2024-08-27 20:31:12 +0200

    add proper no_std crate setup

--- crates/ir/src/lib.rs
+++ crates/ir/src/lib.rs
@@ -1,5 +1,11 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
+#[cfg(not(feature = "std"))]
+extern crate alloc as std;
+
+#[cfg(feature = "std")]
+extern crate std;
+
 mod r#enum;
 mod error;
 mod for_each_op;
