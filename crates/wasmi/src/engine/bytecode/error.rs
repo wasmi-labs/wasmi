b2a577944adcd084ae1e6dcadcdbbd1c9b70aef6
Robin Freyler <robin.freyler@gmail.com>
2024-09-17 11:43:26 +0200

    remove invalid intra doc links

--- crates/wasmi/src/engine/bytecode/error.rs
+++ crates/wasmi/src/engine/bytecode/error.rs
@@ -3,13 +3,13 @@ use core::fmt;
 /// An error that may be occurred when operating with some Wasmi IR primitives.
 #[derive(Debug)]
 pub enum Error {
-    /// Encountered when trying to create a [`Reg`](crate::Reg) from an out of bounds integer.
+    /// Encountered when trying to create a register from an out of bounds integer.
     RegisterOutOfBounds,
-    /// Encountered when trying to create a [`BranchOffset`](crate::BranchOffset) from an out of bounds integer.
+    /// Encountered when trying to create a branch offset from an out of bounds integer.
     BranchOffsetOutOfBounds,
-    /// Encountered when trying to create a [`Comparator`](crate::Comparator) from an out of bounds integer.
+    /// Encountered when trying to create a comparator from an out of bounds integer.
     ComparatorOutOfBounds,
-    /// Encountered when trying to create a [`BlockFuel`](crate::BlockFuel) from an out of bounds integer.
+    /// Encountered when trying to create block fuel from an out of bounds integer.
     BlockFuelOutOfBounds,
 }
 
