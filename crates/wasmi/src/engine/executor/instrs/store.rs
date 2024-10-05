b9241653d160934ed5ba3619fa7ac497b53d1f90
Robin Freyler <robin.freyler@gmail.com>
2024-10-05 12:36:57 +0200

    use get_memory utility method

--- crates/wasmi/src/engine/executor/instrs/store.rs
+++ crates/wasmi/src/engine/executor/instrs/store.rs
@@ -90,12 +90,7 @@ impl Executor<'_> {
         'exec: 'bytes,
         'store: 'bytes,
     {
-        // Safety: the underlying instance of `self.cache` is always kept up-to-date conservatively.
-        let memory = unsafe {
-            self.cache
-                .get_memory(memory)
-                .unwrap_or_else(|| panic!("missing linear memory for {memory:?}"))
-        };
+        let memory = self.get_memory(memory);
         store.resolve_memory_mut(&memory).data_mut()
     }
 
