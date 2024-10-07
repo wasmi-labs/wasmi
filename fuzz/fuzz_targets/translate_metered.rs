2dbfdab406cb9a0a0fcf90a638771bcaf8541bc5
gtrepta <50716988+gtrepta@users.noreply.github.com>
2024-10-07 14:49:36 -0500

    Update wasm-smith to 0.218.0 (#1231)
    * Update wasm-smith to 0.218.0
    
    * Fix clippy warnings
    
    * Make function calls for exec/translate modules more consistent.
    
    ---------
    
    Co-authored-by: Scott Guest <scott.guest@runtimeverification.com>

--- fuzz/fuzz_targets/translate_metered.rs
+++ fuzz/fuzz_targets/translate_metered.rs
@@ -1,9 +1,16 @@
 #![no_main]
+
+mod utils;
+
 use libfuzzer_sys::fuzz_target;
+use utils::arbitrary_translate_module;
 use wasmi::{Config, Engine, Module};
 
-fuzz_target!(|data: wasm_smith::Module| {
-    let wasm = data.to_bytes();
+fuzz_target!(|seed: &[u8]| {
+    let Ok(smith_module) = arbitrary_translate_module(seed) else {
+        return;
+    };
+    let wasm = smith_module.to_bytes();
     let mut config = Config::default();
     config.consume_fuel(true);
     let engine = Engine::new(&config);
