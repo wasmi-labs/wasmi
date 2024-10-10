a46a972aaea926fd8d02e14db49a24e1a4601243
Scott Guest <scott.guest@runtimeverification.com>
2024-10-10 16:40:10 -0400

    Use existing Arbitrary instances for primitive types in ty_to_arbitrary_val

--- fuzz/fuzz_targets/utils.rs
+++ fuzz/fuzz_targets/utils.rs
@@ -6,7 +6,6 @@ use wasmi::{core::ValType, ExternRef, FuncRef, Val};
 pub fn disable_unsupported_config(config: &mut wasm_smith::Config) {
     config.gc_enabled = false;
     config.exceptions_enabled = false;
-    config.memory64_enabled = false;
     config.relaxed_simd_enabled = false;
     config.simd_enabled = false;
     config.threads_enabled = false;
@@ -28,20 +27,10 @@ pub fn arbitrary_swarm_config_module(
 /// Converts a [`ValType`] into an arbitrary [`Val`]
 pub fn ty_to_arbitrary_val(ty: &ValType, u: &mut Unstructured) -> Val {
     match ty {
-        ValType::I32 => Val::I32(u.int_in_range::<i32>(i32::MIN..=i32::MAX).unwrap_or(1)),
-        ValType::I64 => Val::I64(u.int_in_range::<i64>(i64::MIN..=i64::MAX).unwrap_or(1)),
-        ValType::F32 => Val::F32(
-            u.int_in_range::<u32>(u32::MIN..=u32::MAX)
-                .map(f32::from_bits)
-                .unwrap_or(1.0)
-                .into(),
-        ),
-        ValType::F64 => Val::F64(
-            u.int_in_range::<u64>(u64::MIN..=u64::MAX)
-                .map(f64::from_bits)
-                .unwrap_or(1.0)
-                .into(),
-        ),
+        ValType::I32 => Val::I32(i32::arbitrary(u).unwrap_or(1)),
+        ValType::I64 => Val::I64(i64::arbitrary(u).unwrap_or(1)),
+        ValType::F32 => Val::F32(f32::arbitrary(u).unwrap_or(1.0).into()),
+        ValType::F64 => Val::F64(f64::arbitrary(u).unwrap_or(1.0).into()),
         ValType::FuncRef => Val::FuncRef(FuncRef::null()),
         ValType::ExternRef => Val::ExternRef(ExternRef::null()),
     }
