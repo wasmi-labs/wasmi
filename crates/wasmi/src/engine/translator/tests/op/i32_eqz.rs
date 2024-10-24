74f5276474d8db871e3b3c40c2ccad4961632b6e
Robin Freyler <robin.freyler@gmail.com>
2024-10-24 13:47:53 +0200

    add mission cases for i32_eqz fusion tests

--- crates/wasmi/src/engine/translator/tests/op/i32_eqz.rs
+++ crates/wasmi/src/engine/translator/tests/op/i32_eqz.rs
@@ -36,6 +36,8 @@ fn binop_i32_eqz_i64() {
             .run()
     }
     test_for!(
+        ("i32", "eq", Instruction::i32_ne),
+        ("i32", "ne", Instruction::i32_eq),
         ("i32", "and", Instruction::i32_and_eqz),
         ("i32", "or", Instruction::i32_or_eqz),
         ("i32", "xor", Instruction::i32_xor_eqz),
@@ -47,6 +49,9 @@ fn binop_i32_eqz_i64() {
         ("i32", "gt_u", Instruction::i32_le_u),
         ("i32", "ge_s", Instruction::i32_lt_s),
         ("i32", "ge_u", Instruction::i32_lt_u),
+
+        ("i64", "eq", Instruction::i64_ne),
+        ("i64", "ne", Instruction::i64_eq),
         ("i64", "lt_s", swap_ops!(Instruction::i64_le_s)),
         ("i64", "lt_u", swap_ops!(Instruction::i64_le_u)),
         ("i64", "le_s", swap_ops!(Instruction::i64_lt_s)),
@@ -55,6 +60,7 @@ fn binop_i32_eqz_i64() {
         ("i64", "gt_u", Instruction::i64_le_u),
         ("i64", "ge_s", Instruction::i64_lt_s),
         ("i64", "ge_u", Instruction::i64_lt_u),
+
         ("f32", "eq", Instruction::f32_ne),
         ("f32", "ne", Instruction::f32_eq),
         ("f64", "eq", Instruction::f64_ne),
@@ -119,6 +125,8 @@ fn binop_imm_i32_eqz_rhs() {
         (u32, "gt_u", Instruction::i32_le_u_imm16_rhs),
         (i32, "ge_s", Instruction::i32_lt_s_imm16_rhs),
         (u32, "ge_u", Instruction::i32_lt_u_imm16_rhs),
+        (i64, "eq", Instruction::i64_ne_imm16),
+        (i64, "ne", Instruction::i64_eq_imm16),
         (i64, "lt_s", swap_ops!(Instruction::i64_le_s_imm16_lhs)),
         (u64, "lt_u", swap_ops!(Instruction::i64_le_u_imm16_lhs)),
         (i64, "le_s", swap_ops!(Instruction::i64_lt_s_imm16_lhs)),
@@ -180,6 +188,8 @@ fn binop_imm_i32_eqz_lhs() {
         (u32, "gt_u", swap_ops!(Instruction::i32_le_u_imm16_lhs)),
         (i32, "ge_s", swap_ops!(Instruction::i32_lt_s_imm16_lhs)),
         (u32, "ge_u", swap_ops!(Instruction::i32_lt_u_imm16_lhs)),
+        (i64, "eq", Instruction::i64_ne_imm16),
+        (i64, "ne", Instruction::i64_eq_imm16),
         (i64, "lt_s", Instruction::i64_le_s_imm16_rhs),
         (u64, "lt_u", Instruction::i64_le_u_imm16_rhs),
         (i64, "le_s", Instruction::i64_lt_s_imm16_rhs),
