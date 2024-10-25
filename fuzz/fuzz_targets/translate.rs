0dcfaf355ef302a0167d388116aa8a3270be884f
Robin Freyler <robin.freyler@gmail.com>
2024-10-25 14:20:55 +0200

    add TranslateFuzzConfig abstraction

--- fuzz/fuzz_targets/translate.rs
+++ fuzz/fuzz_targets/translate.rs
@@ -4,12 +4,29 @@ use arbitrary::{Arbitrary, Unstructured};
 use libfuzzer_sys::fuzz_target;
 use wasmi::{Config, Engine, Module};
 
+/// Configuration for translation fuzzing.
+struct TranslateFuzzConfig {
+    /// Is `true` if Wasmi shall enable fuel metering for its translation.
+    consume_fuel: bool,
+    /// Is `true` if Wasmi shall use streaming translation instead of buffered translation.
+    streaming: bool,
+}
+
+impl Arbitrary<'_> for TranslateFuzzConfig {
+    fn arbitrary(u: &mut Unstructured) -> arbitrary::Result<Self> {
+        let bits = u8::arbitrary(u)?;
+        let consume_fuel = (bits & 0x1) != 0;
+        let streaming = (bits & (0x1 << 1)) != 0;
+        Ok(Self {
+            consume_fuel,
+            streaming,
+        })
+    }
+}
+
 fuzz_target!(|seed: &[u8]| {
     let mut u = Unstructured::new(seed);
-    let Ok(consume_fuel) = bool::arbitrary(&mut u) else {
-        return;
-    };
-    let Ok(streaming) = bool::arbitrary(&mut u) else {
+    let Ok(translate_config) = TranslateFuzzConfig::arbitrary(&mut u) else {
         return;
     };
     let Ok(fuzz_config) = wasmi_fuzz::FuzzConfig::arbitrary(&mut u) else {
@@ -20,9 +37,9 @@ fuzz_target!(|seed: &[u8]| {
     };
     let wasm = smith_module.to_bytes();
     let mut config = Config::default();
-    config.consume_fuel(consume_fuel);
+    config.consume_fuel(translate_config.consume_fuel);
     let engine = Engine::new(&config);
-    let make_module = match streaming {
+    let make_module = match translate_config.streaming {
         true => Module::new_streaming,
         false => Module::new,
     };
