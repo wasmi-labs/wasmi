4094193d30100d6f5dcb8caf8f0ccf34d0a1b6b3
Robin Freyler <robin.freyler@gmail.com>
2024-10-25 14:55:22 +0200

    cover unchecked Wasmi translation in fuzzer

--- fuzz/fuzz_targets/translate.rs
+++ fuzz/fuzz_targets/translate.rs
@@ -10,6 +10,8 @@ struct TranslateFuzzConfig {
     consume_fuel: bool,
     /// Is `true` if Wasmi shall use streaming translation instead of buffered translation.
     streaming: bool,
+    /// Is `true` if Wasmi shall validate the Wasm input during translation.
+    checked: bool,
 }
 
 impl Arbitrary<'_> for TranslateFuzzConfig {
@@ -17,9 +19,11 @@ impl Arbitrary<'_> for TranslateFuzzConfig {
         let bits = u8::arbitrary(u)?;
         let consume_fuel = (bits & 0x1) != 0;
         let streaming = (bits & (0x1 << 1)) != 0;
+        let checked = (bits & (0x1 << 1)) != 0;
         Ok(Self {
             consume_fuel,
             streaming,
+            checked,
         })
     }
 
@@ -40,13 +44,24 @@ fuzz_target!(|seed: &[u8]| {
     let Ok(smith_module) = wasm_smith::Module::new(fuzz_config.into(), &mut u) else {
         return;
     };
-    let wasm = smith_module.to_bytes();
+    let wasm_bytes = smith_module.to_bytes();
+    let wasm = wasm_bytes.as_slice();
     let mut config = Config::default();
     config.consume_fuel(translate_config.consume_fuel);
     let engine = Engine::new(&config);
-    let make_module = match translate_config.streaming {
-        true => Module::new_streaming,
-        false => Module::new,
+    if !translate_config.checked {
+        // We validate the Wasm module before handing it over to Wasmi
+        // despite `wasm_smith` stating to only produce valid Wasm.
+        // Translating an invalid Wasm module is undefined behavior.
+        if Module::validate(&engine, wasm).is_err() {
+            return;
+        }
+    }
+    let status = match (translate_config.streaming, translate_config.checked) {
+        (true, true) => Module::new_streaming(&engine, wasm),
+        (false, true) => Module::new(&engine, wasm),
+        (true, false) => unsafe { Module::new_streaming_unchecked(&engine, wasm) },
+        (false, false) => unsafe { Module::new_unchecked(&engine, wasm) },
     };
-    make_module(&engine, &wasm[..]).unwrap();
+    status.unwrap();
 });
