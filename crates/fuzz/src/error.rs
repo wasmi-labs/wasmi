d6b79464f87987e60d2c84a0f2546e3e3136ab6a
Robin Freyler <robin.freyler@gmail.com>
2024-10-26 13:07:48 +0200

    improve FuzzError and add is_non_deterministic

--- crates/fuzz/src/error.rs
+++ crates/fuzz/src/error.rs
@@ -1,10 +1,17 @@
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq)]
 pub enum FuzzError {
     Trap(TrapCode),
     Other,
 }
 
-#[derive(Debug, Copy, Clone)]
+impl FuzzError {
+    /// Returns `true` if `self` may be of non-deterministic origin.
+    pub fn is_non_deterministic(&self) -> bool {
+        matches!(self, Self::Trap(TrapCode::StackOverflow) | Self::Other)
+    }
+}
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
 pub enum TrapCode {
     UnreachableCodeReached,
     MemoryOutOfBounds,
