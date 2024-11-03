591ef3070e188a37661b3b0c9c3a48a0ac4c4b15
Robin Freyler <robin.freyler@gmail.com>
2024-11-03 21:37:46 +0100

    fix wasmi_collections docs for all features toggles

--- crates/collections/src/lib.rs
+++ crates/collections/src/lib.rs
@@ -12,8 +12,8 @@
 //! - [`Set`]: generic key-value mapping, based on either [`HashSet`] or [`BTreeSet`]
 //! - [`StringInterner`]: stores and deduplicates strings efficiently, based on either [`HashSet`] or [`BTreeSet`]
 //!
-//! [`HashSet`]: hashbrown::HashSet
-//! [`HashMap`]: hashbrown::HashMap
+//! [`HashSet`]: https://docs.rs/hashbrown/0.15.0/hashbrown/struct.HashSet.html
+//! [`HashMap`]: https://docs.rs/hashbrown/0.15.0/hashbrown/struct.HashMap.html
 //! [`BTreeSet`]: std::collections::BTreeSet
 //! [`BTreeMap`]: std::collections::BTreeMap
 
