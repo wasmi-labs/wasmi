b4daacaef19defc2564aac25380a006f231c6954
Robin Freyler <robin.freyler@gmail.com>
2024-11-12 00:11:37 +0100

    add NonZeroBranchOffsets invariants checker

--- crates/wasmi/src/engine/translator/conditions.rs
+++ crates/wasmi/src/engine/translator/conditions.rs
@@ -14,7 +14,10 @@
 //! translation and therefore also act as "documentation" about some of them.
 
 use super::{FuncTranslator, Instr};
-use crate::ir::{self, BranchOffset, Instruction, Reg};
+use crate::{
+    core::UntypedVal,
+    ir::{self, BranchOffset, ComparatorAndOffset, Instruction, Reg},
+};
 use core::{
     cmp,
     fmt::{self, Display},
@@ -78,6 +81,13 @@ pub struct Error {
     reason: Reason,
 }
 
+impl Error {
+    /// Creates a new [`Error`] with the `instr` and `reason`.
+    pub fn new(instr: Instr, reason: Reason) -> Self {
+        Self { instr, reason }
+    }
+}
+
 /// The reason behind a broken Wasmi translation invariant.
 #[derive(Debug, Clone)]
 pub enum Reason {
@@ -120,6 +130,14 @@ pub enum Reason {
     InvalidReturnValues {
         /// The invalid number of returned values.
         invalid_results: u32,
+    },
+    MissingFunctionLocalConst {
+        /// The register for which the function local constant is missing.
+        reg: Reg,
+    },
+    InvalidComparatorAndOffset {
+        /// The invalid encoded comparator and offset.
+        value: UntypedVal,
     },
 }
 
@@ -252,6 +270,24 @@ impl Display for ErrorWithContext<'_> {
                     "
                 )
             }
+            Reason::MissingFunctionLocalConst { reg } => {
+                write!(
+                    f,
+                    "missing function local constant value for instruction at {pos}:\n\
+                        \t- instruction: {instr:?}\n\
+                        \t- register to constant: {reg:?}\
+                    "
+                )
+            }
+            Reason::InvalidComparatorAndOffset { value } => {
+                write!(
+                    f,
+                    "invalid encoded comparator and offset for instruction at {pos}:\n\
+                        \t- instruction: {instr:?}\n\
+                        \t- encoded value: {value:?}\
+                    "
+                )
+            }
         }
     }
 }
@@ -340,6 +376,20 @@ pub fn verify_translation_invariants(translator: &FuncTranslator) -> Result<(), 
     TranslationInvariantsChecker { translator }.verify_translation_invariants()
 }
 
+/// Extension convenience trait to push to errors accumulator upon `Err`.
+trait OkOrPush {
+    /// Pushes to `errors` if `self` is an `Err`.
+    fn ok_or_push(self, errors: &mut Vec<Error>);
+}
+
+impl OkOrPush for Result<(), Error> {
+    fn ok_or_push(self, errors: &mut Vec<Error>) {
+        if let Err(e) = self {
+            errors.push(e);
+        }
+    }
+}
+
 /// Encapsulates state required for translation invariants checking.
 struct TranslationInvariantsChecker<'translator> {
     /// The underlying Wasmi function translator which has already finished its job.
@@ -355,8 +405,16 @@ impl<'translator> TranslationInvariantsChecker<'translator> {
     ///
     /// If a Wasmi translation invariant is broken.
     fn verify_translation_invariants(&self) -> Result<(), ErrorList<'translator>> {
-        let errors = Vec::new();
-        // TODO: perform invariant checking
+        let mut errors = Vec::new();
+        let mut checker = NonZeroBranchOffsets::new(self.translator);
+        let instrs = self.translator.alloc.instr_encoder.instrs.as_slice();
+        checker.start().ok_or_push(&mut errors);
+        for (current, instruction) in instrs.iter().enumerate() {
+            let instr = Instr::from_usize(current);
+            checker.instr(instr, instruction).ok_or_push(&mut errors);
+        }
+        checker.finish().ok_or_push(&mut errors);
+        // At this point all invariants have been processed.
         if errors.is_empty() {
             return Ok(());
         }
@@ -365,6 +423,130 @@ impl<'translator> TranslationInvariantsChecker<'translator> {
                 translator: self.translator,
             },
             errors,
+        ))
+    }
+}
+
+/// Implemented by all post conditions checker.
+trait InvariantsChecker<'a> {
+    /// Create a new [`InvariantsChecker`].
+    fn new(translator: &'a FuncTranslator) -> Self;
+    /// Setup the [`InvariantsChecker`].
+    ///
+    /// This method is called once before inspecting all instructions.
+    fn start(&mut self) -> Result<(), Error> {
+        Ok(())
+    }
+    /// Checks the `instruction` at `instr` with this [`InvariantsChecker`].
+    fn instr(&mut self, instr: Instr, instruction: &Instruction) -> Result<(), Error>;
+    /// Finishes the [`InvariantsChecker`].
+    ///
+    /// This method is called once after inspecting all instructions.
+    fn finish(&mut self) -> Result<(), Error> {
+        Ok(())
+    }
+}
+
+/// Checks that all branch offsets are non-zero when fuel metering is enabled.
+pub struct NonZeroBranchOffsets<'a> {
+    translator: &'a FuncTranslator,
+    /// True if fuel metering is enabled for fast access.
+    consume_fuel: bool,
+}
+
+impl<'a> InvariantsChecker<'a> for NonZeroBranchOffsets<'a> {
+    fn new(translator: &'a FuncTranslator) -> Self {
+        let consume_fuel = translator.engine.config().get_consume_fuel();
+        Self {
+            translator,
+            consume_fuel,
+        }
+    }
+
+    fn instr(&mut self, instr: Instr, instruction: &Instruction) -> Result<(), Error> {
+        use Instruction as I;
+        if !self.consume_fuel {
+            return Ok(());
+        }
+        let offset = match *instruction {
+            | I::Branch { offset } => offset,
+            | I::BranchCmpFallback { params, .. } => {
+                let Some(value) = self.translator.alloc.stack.resolve_const(params) else {
+                    return Err(Error::new(
+                        instr,
+                        Reason::MissingFunctionLocalConst { reg: params },
+                    ));
+                };
+                let Some(decoded) = ComparatorAndOffset::from_untyped(value) else {
+                    return Err(Error::new(
+                        instr,
+                        Reason::InvalidComparatorAndOffset { value },
+                    ));
+                };
+                decoded.offset
+            }
+            | I::BranchI32And { offset, .. }
+            | I::BranchI32AndImm16 { offset, .. }
+            | I::BranchI32Or { offset, .. }
+            | I::BranchI32OrImm16 { offset, .. }
+            | I::BranchI32Xor { offset, .. }
+            | I::BranchI32XorImm16 { offset, .. }
+            | I::BranchI32AndEqz { offset, .. }
+            | I::BranchI32AndEqzImm16 { offset, .. }
+            | I::BranchI32OrEqz { offset, .. }
+            | I::BranchI32OrEqzImm16 { offset, .. }
+            | I::BranchI32XorEqz { offset, .. }
+            | I::BranchI32XorEqzImm16 { offset, .. }
+            | I::BranchI32Eq { offset, .. }
+            | I::BranchI32EqImm16 { offset, .. }
+            | I::BranchI32Ne { offset, .. }
+            | I::BranchI32NeImm16 { offset, .. }
+            | I::BranchI32LtS { offset, .. }
+            | I::BranchI32LtSImm16Lhs { offset, .. }
+            | I::BranchI32LtSImm16Rhs { offset, .. }
+            | I::BranchI32LtU { offset, .. }
+            | I::BranchI32LtUImm16Lhs { offset, .. }
+            | I::BranchI32LtUImm16Rhs { offset, .. }
+            | I::BranchI32LeS { offset, .. }
+            | I::BranchI32LeSImm16Lhs { offset, .. }
+            | I::BranchI32LeSImm16Rhs { offset, .. }
+            | I::BranchI32LeU { offset, .. }
+            | I::BranchI32LeUImm16Lhs { offset, .. }
+            | I::BranchI32LeUImm16Rhs { offset, .. }
+            | I::BranchI64Eq { offset, .. }
+            | I::BranchI64EqImm16 { offset, .. }
+            | I::BranchI64Ne { offset, .. }
+            | I::BranchI64NeImm16 { offset, .. }
+            | I::BranchI64LtS { offset, .. }
+            | I::BranchI64LtSImm16Lhs { offset, .. }
+            | I::BranchI64LtSImm16Rhs { offset, .. }
+            | I::BranchI64LtU { offset, .. }
+            | I::BranchI64LtUImm16Lhs { offset, .. }
+            | I::BranchI64LtUImm16Rhs { offset, .. }
+            | I::BranchI64LeS { offset, .. }
+            | I::BranchI64LeSImm16Lhs { offset, .. }
+            | I::BranchI64LeSImm16Rhs { offset, .. }
+            | I::BranchI64LeU { offset, .. }
+            | I::BranchI64LeUImm16Lhs { offset, .. }
+            | I::BranchI64LeUImm16Rhs { offset, .. }
+            | I::BranchF32Eq { offset, .. }
+            | I::BranchF32Ne { offset, .. }
+            | I::BranchF32Lt { offset, .. }
+            | I::BranchF32Le { offset, .. }
+            | I::BranchF64Eq { offset, .. }
+            | I::BranchF64Ne { offset, .. }
+            | I::BranchF64Lt { offset, .. }
+            | I::BranchF64Le { offset, .. } => BranchOffset::from(offset),
+            _ => return Ok(()),
+        };
+        if offset.to_i32() != 0 {
+            return Ok(());
+        }
+        Err(Error::new(
+            instr,
+            Reason::InvalidBranchOffset {
+                invalid_offset: offset,
+            },
         ))
     }
 }
