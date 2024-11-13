a092a5ce5a5e7b8c5b8dbdda62e48d825cb902f5
Robin Freyler <robin.freyler@gmail.com>
2024-11-13 20:05:00 +0100

    adjust no_std usage for wasmi_ir

--- crates/ir/src/lib.rs
+++ crates/ir/src/lib.rs
@@ -1,8 +1,6 @@
-#![cfg_attr(not(feature = "std"), no_std)]
+#![no_std]
 
-#[cfg(not(feature = "std"))]
-extern crate alloc as std;
-
+extern crate alloc;
 #[cfg(feature = "std")]
 extern crate std;
 
