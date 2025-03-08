563ebe41ccd9a04fc810e2062e9f20187a737734
Robin Freyler <robin.freyler@gmail.com>
2025-03-08 18:21:32 +0100

    reformat some i64_store translation tests

--- crates/wasmi/src/engine/translator/tests/op/store/i64_store.rs
+++ crates/wasmi/src/engine/translator/tests/op/store/i64_store.rs
@@ -51,32 +51,38 @@ fn offset16() {
 #[test]
 #[cfg_attr(miri, ignore)]
 fn offset16_imm() {
-    test_store_offset16_imm::<i64>(
-        WASM_OP,
+    [
         i64::from(i16::MIN) - 1,
-        Instruction::store64_offset16,
-    );
-    test_store_offset16_imm::<i64>(
-        WASM_OP,
         i64::from(i16::MAX) + 1,
-        Instruction::store64_offset16,
-    );
-    test_store_offset16_imm::<i64>(WASM_OP, i64::MAX - 1, Instruction::store64_offset16);
-    test_store_offset16_imm::<i64>(WASM_OP, i64::MIN + 1, Instruction::store64_offset16);
-    test_store_offset16_imm::<i64>(WASM_OP, i64::MIN, Instruction::store64_offset16);
-    test_store_offset16_imm::<i64>(WASM_OP, i64::MAX, Instruction::store64_offset16);
+        i64::MIN,
+        i64::MIN + 1,
+        i64::MAX - 1,
+        i64::MAX,
+    ]
+    .into_iter()
+    .for_each(|value| {
+        test_store_offset16_imm::<i64>(WASM_OP, value, Instruction::store64_offset16);
+    })
 }
 
 #[test]
 #[cfg_attr(miri, ignore)]
 fn offset16_imm16() {
-    test_store_offset16_imm16::<i16>(WASM_OP, Instruction::i64_store_offset16_imm16, 0);
-    test_store_offset16_imm16::<i16>(WASM_OP, Instruction::i64_store_offset16_imm16, 1);
-    test_store_offset16_imm16::<i16>(WASM_OP, Instruction::i64_store_offset16_imm16, -1);
-    test_store_offset16_imm16::<i16>(WASM_OP, Instruction::i64_store_offset16_imm16, i16::MIN + 1);
-    test_store_offset16_imm16::<i16>(WASM_OP, Instruction::i64_store_offset16_imm16, i16::MAX - 1);
-    test_store_offset16_imm16::<i16>(WASM_OP, Instruction::i64_store_offset16_imm16, i16::MIN);
-    test_store_offset16_imm16::<i16>(WASM_OP, Instruction::i64_store_offset16_imm16, i16::MAX);
+    [
+        0,
+        -1,
+        1,
+        -42,
+        42,
+        i16::MIN + 1,
+        i16::MIN,
+        i16::MAX - 1,
+        i16::MAX,
+    ]
+    .into_iter()
+    .for_each(|value| {
+        test_store_offset16_imm16::<i16>(WASM_OP, Instruction::i64_store_offset16_imm16, value);
+    })
 }
 
 #[test]
