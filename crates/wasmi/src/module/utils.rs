fa07a6eb0668d29f981aac7a21af4cc441e53aba
Robin Freyler <robin.freyler@gmail.com>
2025-03-09 18:55:24 +0100

    apply rustfmt

--- crates/wasmi/src/module/utils.rs
+++ crates/wasmi/src/module/utils.rs
@@ -1,5 +1,5 @@
+use crate::{core::ValType, FuncType, GlobalType, IndexType, MemoryType, Mutability, TableType};
 use wasmparser::AbstractHeapType;
-use crate::{IndexType, core::ValType, FuncType, GlobalType, MemoryType, Mutability, TableType};
 
 impl TableType {
     /// Creates a new [`TableType`] from the given `wasmparser` primitive.
