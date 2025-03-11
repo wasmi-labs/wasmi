53806958138044e360bd393611a1219f19bb5d20
Robin Freyler <robin.freyler@gmail.com>
2025-03-11 16:00:16 +0100

    extend memory.grow to trigger failure

--- crates/wasmi/src/engine/translator/tests/op/memory/memory_grow.rs
+++ crates/wasmi/src/engine/translator/tests/op/memory/memory_grow.rs
@@ -21,22 +21,24 @@ fn reg() {
         .run();
 }
 
-fn test_imm16(delta: u32) {
+fn test_imm32(index_ty: IndexType, memory_index: MemIdx, delta: u32) {
     assert!(delta != 0);
+    let index_ty = index_ty.wat();
     let wasm = &format!(
         r"
         (module
-            (memory $m 10)
-            (func (result i32)
-                (i32.const {delta})
-                (memory.grow $m)
+            (memory $mem0 {index_ty} 1)
+            (memory $mem1 {index_ty} 1)
+            (func (result {index_ty})
+                {index_ty}.const {delta}
+                memory.grow {memory_index}
             )
         )",
     );
     TranslationTest::new(wasm)
-        .expect_func_instrs([
+        .expect_func_instrs(iter_filter_opts![
             Instruction::memory_grow_by(Reg::from(0), delta),
-            Instruction::memory_index(0),
+            Instruction::memory_index(memory_index.0),
             Instruction::return_reg(Reg::from(0)),
         ])
         .run();
@@ -44,14 +46,22 @@ fn test_imm16(delta: u32) {
 
 #[test]
 #[cfg_attr(miri, ignore)]
-fn imm16() {
-    test_imm16(1);
-    test_imm16(42);
-    test_imm16(u32::from(u16::MAX) - 1);
-    test_imm16(u32::from(u16::MAX));
-    test_imm16(u32::from(u16::MAX) + 1);
-    test_imm16(u32::MAX - 1);
-    test_imm16(u32::MAX);
+fn imm32() {
+    for delta in [
+        1,
+        42,
+        u32::from(u16::MAX) - 1,
+        u32::from(u16::MAX),
+        u32::from(u16::MAX) + 1,
+        u32::MAX - 1,
+        u32::MAX,
+    ] {
+        for mem_idx in [0, 1].map(MemIdx) {
+            for index_ty in [IndexType::Memory32, IndexType::Memory64] {
+                test_imm32(index_ty, mem_idx, delta)
+            }
+        }
+    }
 }
 
 #[test]
