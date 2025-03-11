6395a9cac11f8a2f3f937bd31437fb15bef61f84
Robin Freyler <robin.freyler@gmail.com>
2025-03-11 11:19:11 +0100

    deduplicate wide-arithmetic executor impls

--- crates/wasmi/src/engine/executor/instrs/wide_arithmetic.rs
+++ crates/wasmi/src/engine/executor/instrs/wide_arithmetic.rs
@@ -15,6 +15,12 @@ struct Params128 {
     rhs_hi: Reg,
 }
 
+/// Function signature for `i64.binop128` handlers.
+type BinOp128Fn = fn(UntypedVal, UntypedVal, UntypedVal, UntypedVal) -> (UntypedVal, UntypedVal);
+
+/// Function signature for `i64.mul_wide_sx` handlers.
+type I64MulWideFn = fn(UntypedVal, UntypedVal) -> (UntypedVal, UntypedVal);
+
 impl Executor<'_> {
     /// Fetches the parameters required by the `i64.add128` and `i64.sub128` instructions.
     fn fetch_params128(&self) -> Params128 {
@@ -37,8 +43,8 @@ impl Executor<'_> {
         }
     }
 
-    /// Executes an [`Instruction::I64Add128`].
-    pub fn execute_i64_add128(&mut self, results: [Reg; 2], lhs_lo: Reg) {
+    /// Executes a generic Wasm `i64.binop128` instruction.
+    fn execute_i64_binop128(&mut self, results: [Reg; 2], lhs_lo: Reg, binop: BinOp128Fn) {
         let Params128 {
             lhs_hi,
             rhs_lo,
@@ -48,44 +54,44 @@ impl Executor<'_> {
         let lhs_hi = self.get_register(lhs_hi);
         let rhs_lo = self.get_register(rhs_lo);
         let rhs_hi = self.get_register(rhs_hi);
-        let (result_lo, result_hi) = UntypedVal::i64_add128(lhs_lo, lhs_hi, rhs_lo, rhs_hi);
+        let (result_lo, result_hi) = binop(lhs_lo, lhs_hi, rhs_lo, rhs_hi);
         self.set_register(results[0], result_lo);
         self.set_register(results[1], result_hi);
     }
 
+    /// Executes an [`Instruction::I64Add128`].
+    pub fn execute_i64_add128(&mut self, results: [Reg; 2], lhs_lo: Reg) {
+        self.execute_i64_binop128(results, lhs_lo, UntypedVal::i64_add128)
+    }
+
     /// Executes an [`Instruction::I64Sub128`].
     pub fn execute_i64_sub128(&mut self, results: [Reg; 2], lhs_lo: Reg) {
-        let Params128 {
-            lhs_hi,
-            rhs_lo,
-            rhs_hi,
-        } = self.fetch_params128();
-        let lhs_lo = self.get_register(lhs_lo);
-        let lhs_hi = self.get_register(lhs_hi);
-        let rhs_lo = self.get_register(rhs_lo);
-        let rhs_hi = self.get_register(rhs_hi);
-        let (result_lo, result_hi) = UntypedVal::i64_sub128(lhs_lo, lhs_hi, rhs_lo, rhs_hi);
+        self.execute_i64_binop128(results, lhs_lo, UntypedVal::i64_sub128)
+    }
+
+    /// Executes a generic Wasm `i64.mul_wide_sx` instruction.
+    fn execute_i64_mul_wide_sx(
+        &mut self,
+        results: FixedRegSpan<2>,
+        lhs: Reg,
+        rhs: Reg,
+        mul_wide: I64MulWideFn,
+    ) {
+        let lhs = self.get_register(lhs);
+        let rhs = self.get_register(rhs);
+        let (result_lo, result_hi) = mul_wide(lhs, rhs);
+        let results = results.to_array();
         self.set_register(results[0], result_lo);
         self.set_register(results[1], result_hi);
     }
 
     /// Executes an [`Instruction::I64MulWideS`].
     pub fn execute_i64_mul_wide_s(&mut self, results: FixedRegSpan<2>, lhs: Reg, rhs: Reg) {
-        let lhs = self.get_register(lhs);
-        let rhs = self.get_register(rhs);
-        let (result_lo, result_hi) = UntypedVal::i64_mul_wide_s(lhs, rhs);
-        let results = results.to_array();
-        self.set_register(results[0], result_lo);
-        self.set_register(results[1], result_hi);
+        self.execute_i64_mul_wide_sx(results, lhs, rhs, UntypedVal::i64_mul_wide_s)
     }
 
     /// Executes an [`Instruction::I64MulWideU`].
     pub fn execute_i64_mul_wide_u(&mut self, results: FixedRegSpan<2>, lhs: Reg, rhs: Reg) {
-        let lhs = self.get_register(lhs);
-        let rhs = self.get_register(rhs);
-        let (result_lo, result_hi) = UntypedVal::i64_mul_wide_u(lhs, rhs);
-        let results = results.to_array();
-        self.set_register(results[0], result_lo);
-        self.set_register(results[1], result_hi);
+        self.execute_i64_mul_wide_sx(results, lhs, rhs, UntypedVal::i64_mul_wide_u)
     }
 }
