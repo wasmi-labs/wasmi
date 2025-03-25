eaa097591a854a75880af332d0d676515e8a6be9
Robin Freyler <robin.freyler@gmail.com>
2025-03-25 20:06:20 +0100

    un-ignore passing SIMD test

--- crates/wast/tests/mod.rs
+++ crates/wast/tests/mod.rs
@@ -248,7 +248,7 @@ macro_rules! expand_tests {
             #[ignore] fn wasm_simd_load_extend("simd_load_extend");
             #[ignore] fn wasm_simd_load_splat("simd_load_splat");
             #[ignore] fn wasm_simd_load_zero("simd_load_zero");
-            #[ignore] fn wasm_simd_splat("simd_splat");
+            fn wasm_simd_splat("simd_splat");
             fn wasm_simd_store("simd_store");
             #[ignore] fn wasm_simd_store16_lane("simd_store16_lane");
             #[ignore] fn wasm_simd_store32_lane("simd_store32_lane");
