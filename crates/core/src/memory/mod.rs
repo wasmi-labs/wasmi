cf5a35b1a419cc844a4309dc6979cc64e4070bd9
Robin Freyler <robin.freyler@gmail.com>
2025-04-07 14:16:36 +0200

    move memory.rs into memory/access.rs submodule

---
+++ crates/core/src/memory/mod.rs
@@ -0,0 +1,13 @@
+mod access;
+
+pub use self::access::{
+    load,
+    load_at,
+    load_extend,
+    load_extend_at,
+    store,
+    store_at,
+    store_wrap,
+    store_wrap_at,
+    ExtendInto,
+};
