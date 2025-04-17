78fcbd6e9cfa4d87a0d0ed5bd551b5fe81683df8
Robin Freyler <robin.freyler@gmail.com>
2025-04-17 15:04:05 +0200

    add docs to OperatorCode trait

--- crates/ir2/src/instr/utils.rs
+++ crates/ir2/src/instr/utils.rs
@@ -1,3 +1,8 @@
+use crate::{CopyEncoder, OpCode, EncoderError};
+
+/// Trait to query the [`OpCode`] of operators.
+///
+/// Implemented by [`Op`] and all operators found in the [`crate::Op`] module.
 pub trait OperatorCode {
     fn op_code(&self) -> crate::OpCode;
 }
