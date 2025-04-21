2b727eed805e63f5742f45d891204ff3d627c8eb
Robin Freyler <robin.freyler@gmail.com>
2025-04-21 16:24:36 +0200

    add BinaryCommutativeOperator trait

--- crates/ir2/src/utils.rs
+++ crates/ir2/src/utils.rs
@@ -61,3 +61,46 @@ pub trait UnaryOperator {
     /// Creates the operator variant that takes a [`Stack`] and returns a [`Stack`].
     fn make_ss(result: Stack, input: Stack) -> Self::OpSs;
 }
+
+/// Class of commutative binary operators.
+pub trait BinaryCommutativeOperator {
+    /// The name of the operator class.
+    const NAME: &'static str;
+
+    /// The type of immediate input operands.
+    type Imm;
+
+    /// The operator variant with signature: `fn(Reg, Imm) -> Reg`
+    type OpRri: Operator;
+    /// The operator variant with signature: `fn(Reg, Stack) -> Reg`
+    type OpRrs: Operator;
+    /// The operator variant with signature: `fn(Stack, Imm) -> Reg`
+    type OpRsi: Operator;
+    /// The operator variant with signature: `fn(Stack, Stack) -> Reg`
+    type OpRss: Operator;
+    /// The operator variant with signature: `fn(Reg, Imm) -> Stack`
+    type OpSri: Operator;
+    /// The operator variant with signature: `fn(Reg, Stack) -> Stack`
+    type OpSrs: Operator;
+    /// The operator variant with signature: `fn(Stack, Imm) -> Stack`
+    type OpSsi: Operator;
+    /// The operator variant with signature: `fn(Stack, Stack) -> Stack`
+    type OpSss: Operator;
+
+    /// Creates the operator variant with signature: `fn(Reg, Imm) -> Reg`
+    fn make_rri(result: Reg, lhs: Reg, rhs: Self::Imm) -> Self::OpRri;
+    /// Creates the operator variant with signature: `fn(Reg, Stack) -> Reg`
+    fn make_rrs(result: Reg, lhs: Reg, rhs: Stack) -> Self::OpRrs;
+    /// Creates the operator variant with signature: `fn(Stack, Imm) -> Reg`
+    fn make_rsi(result: Reg, lhs: Stack, rhs: Self::Imm) -> Self::OpRsi;
+    /// Creates the operator variant with signature: `fn(Stack, Stack) -> Reg`
+    fn make_rss(result: Reg, lhs: Stack, rhs: Stack) -> Self::OpRss;
+    /// Creates the operator variant with signature: `fn(Reg, Imm) -> Stack`
+    fn make_sri(result: Stack, lhs: Reg, rhs: Self::Imm) -> Self::OpSri;
+    /// Creates the operator variant with signature: `fn(Reg, Stack) -> Stack`
+    fn make_srs(result: Stack, lhs: Reg, rhs: Stack) -> Self::OpSrs;
+    /// Creates the operator variant with signature: `fn(Stack, Imm) -> Stack`
+    fn make_ssi(result: Stack, lhs: Stack, rhs: Self::Imm) -> Self::OpSsi;
+    /// Creates the operator variant with signature: `fn(Stack, Stack) -> Stack`
+    fn make_sss(result: Stack, lhs: Stack, rhs: Stack) -> Self::OpSss;
+}
