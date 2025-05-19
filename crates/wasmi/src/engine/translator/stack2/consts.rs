b02d6d52cffbfd786a84ba39d962d71311ad16dc
Robin Freyler <robin.freyler@gmail.com>
2025-05-19 12:05:27 +0200

    implement all stubs of new Stack

--- crates/wasmi/src/engine/translator/stack2/consts.rs
+++ crates/wasmi/src/engine/translator/stack2/consts.rs
@@ -1,48 +1,59 @@
-use crate::{core::UntypedVal, engine::TranslationError, Error};
+use crate::{core::UntypedVal, engine::TranslationError, ir::Reg, Error};
 use alloc::{
     collections::{btree_map, BTreeMap},
     vec::Vec,
 };
-use core::slice::Iter as SliceIter;
+use core::{iter::Rev, slice::Iter as SliceIter};
 
 /// A pool of deduplicated function local constant values.
 ///
-/// - Those constant values are identified by their associated [`ConstIdx`].
+/// - Those constant values are identified by their associated [`Reg`].
 /// - All constant values are also deduplicated so that no duplicates
 ///   are stored in a [`ConstRegistry`]. This also means that deciding if two
-///   [`ConstIdx`] values refer to the equal constant values can be efficiently
-///   done by comparing the [`ConstIdx`]s without resolving to their
+///   [`Reg`] values refer to the equal constant values can be efficiently
+///   done by comparing the [`Reg`] indices without resolving to their
 ///   underlying constant values.
-#[derive(Debug, Default, Clone)]
+#[derive(Debug, Default)]
 pub struct ConstRegistry {
-    /// Mapping from constant [`UntypedVal`] values to [`ConstIdx`] indices.
-    const2idx: BTreeMap<UntypedVal, ConstIdx>,
-    /// Mapping from [`ConstIdx`] indices to constant [`UntypedVal`] values.
+    /// Mapping from constant [`UntypedVal`] values to [`Reg`] indices.
+    const2idx: BTreeMap<UntypedVal, Reg>,
+    /// Mapping from [`Reg`] indices to constant [`UntypedVal`] values.
     idx2const: Vec<UntypedVal>,
+    /// The [`Reg`] index for the next allocated function local constant value.
+    next_idx: i16,
 }
 
-/// An index referring to a function local constant.
-#[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord)]
-pub struct ConstIdx(usize);
-
 impl ConstRegistry {
-    /// The maximum number of function local constants per function.
-    const MAX_LEN: usize = (1 << 16) - 1;
-
     /// Resets the [`ConstRegistry`] data structure.
     pub fn reset(&mut self) {
         self.const2idx.clear();
         self.idx2const.clear();
+        self.next_idx = Self::first_index();
     }
 
-    /// Returns `true` if no function local constants have been allocated.
-    pub fn is_empty(&self) -> bool {
-        self.len() == 0
+    /// The maximum index for [`Reg`] referring to function local constant values.
+    ///
+    /// # Note
+    ///
+    /// The maximum index is also the one to be assigned to the first allocated
+    /// function local constant value as indices are counting downwards.
+    fn first_index() -> i16 {
+        -1
+    }
+
+    /// The mininmum index for [`Reg`] referring to function local constant values.
+    ///
+    /// # Note
+    ///
+    /// This index is not assignable to a function local constant value and acts
+    /// as a bound to guard against overflowing the range of indices.
+    fn last_index() -> i16 {
+        i16::MIN
     }
 
     /// Returns the number of allocated function local constant values.
-    pub fn len(&self) -> usize {
-        self.idx2const.len()
+    pub fn len_consts(&self) -> u16 {
+        self.next_idx.abs_diff(Self::first_index())
     }
 
     /// Allocates a new constant `value` on the [`ConstRegistry`] and returns its identifier.
@@ -55,25 +66,29 @@ impl ConstRegistry {
     /// # Errors
     ///
     /// If too many constant values have been allocated for this [`ConstRegistry`].
-    pub fn alloc(&mut self, value: UntypedVal) -> Result<ConstIdx, Error> {
-        let len = self.len();
-        if len >= Self::MAX_LEN {
+    pub fn alloc(&mut self, value: UntypedVal) -> Result<Reg, Error> {
+        if self.next_idx == Self::last_index() {
             return Err(Error::from(TranslationError::TooManyFuncLocalConstValues));
         }
         match self.const2idx.entry(value) {
             btree_map::Entry::Occupied(entry) => Ok(*entry.get()),
             btree_map::Entry::Vacant(entry) => {
-                let idx = ConstIdx(len);
-                entry.insert(idx);
+                let register = Reg::from(self.next_idx);
+                self.next_idx -= 1;
+                entry.insert(register);
                 self.idx2const.push(value);
-                Ok(idx)
+                Ok(register)
             }
         }
     }
 
-    /// Returns the function local constant [`UntypedVal`] of the [`ConstIdx`] if any.
-    pub fn get(&self, index: ConstIdx) -> Option<UntypedVal> {
-        self.idx2const.get(index.0).copied()
+    /// Returns the function local constant [`UntypedVal`] of the [`Reg`] if any.
+    pub fn get(&self, register: Reg) -> Option<UntypedVal> {
+        if !register.is_const() {
+            return None;
+        }
+        let index = i16::from(register).wrapping_add(1).unsigned_abs() as usize;
+        self.idx2const.get(index).copied()
     }
 
     /// Returns an iterator yielding all function local constant values of the [`ConstRegistry`].
@@ -89,14 +104,17 @@ impl ConstRegistry {
 /// Iterator yielding all allocated function local constant values.
 pub struct ConstRegistryIter<'a> {
     /// The underlying iterator.
-    iter: SliceIter<'a, UntypedVal>,
+    iter: Rev<SliceIter<'a, UntypedVal>>,
 }
 
 impl<'a> ConstRegistryIter<'a> {
     /// Creates a new [`ConstRegistryIter`] from the given slice of [`UntypedVal`].
     pub fn new(consts: &'a ConstRegistry) -> Self {
+        // Note: we need to revert the iteration since we allocate new
+        //       function local constants in reverse order of their absolute
+        //       vector indices in the function call frame during execution.
         Self {
-            iter: consts.idx2const.as_slice().iter(),
+            iter: consts.idx2const.as_slice().iter().rev(),
         }
     }
 }
