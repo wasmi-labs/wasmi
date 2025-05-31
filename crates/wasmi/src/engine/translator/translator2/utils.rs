10dc015542b6490bbb56a88246cae4177fd5f651
Robin Freyler <robin.freyler@gmail.com>
2025-05-31 20:30:57 +0200

    add bail_unreachable utility macro

--- crates/wasmi/src/engine/translator/translator2/utils.rs
+++ crates/wasmi/src/engine/translator/translator2/utils.rs
@@ -1,3 +1,20 @@
+/// Bail out early in case the current code is unreachable.
+///
+/// # Note
+///
+/// - This should be prepended to most Wasm operator translation procedures.
+/// - If we are in unreachable code most Wasm translation is skipped. Only
+///   certain control flow operators such as `End` are going through the
+///   translation process. In particular the `End` operator may end unreachable
+///   code blocks.
+macro_rules! bail_unreachable {
+    ($this:ident) => {{
+        if !$this.reachable {
+            return Ok(());
+        }
+    }};
+}
+
 /// Implemented by types that can be reset for reuse.
 pub trait Reset {
     /// Resets `self` for reuse.
