ad814a5ed98af8f96c2365a44e4e4b9032027b0e
Robin Freyler <robin.freyler@gmail.com>
2025-07-19 14:18:31 +0200

    add Wasm simd extract_lane translation

--- crates/wasmi/src/engine/translator/func2/simd/mod.rs
+++ crates/wasmi/src/engine/translator/func2/simd/mod.rs
@@ -38,4 +38,33 @@ impl FuncTranslator {
         )?;
         Ok(())
     }
+
+    /// Generically translate any of the Wasm `simd` extract lane instructions.
+    fn translate_extract_lane<T: IntoLaneIdx, R>(
+        &mut self,
+        lane: u8,
+        make_instr: fn(result: Reg, input: Reg, lane: T::LaneIdx) -> Instruction,
+        const_eval: fn(input: V128, lane: T::LaneIdx) -> R,
+    ) -> Result<(), Error>
+    where
+        R: Into<TypedVal> + Typed,
+    {
+        bail_unreachable!(self);
+        let Ok(lane) = <T::LaneIdx>::try_from(lane) else {
+            panic!("encountered out of bounds lane index: {lane}")
+        };
+        let input = self.stack.pop();
+        if let Operand::Immediate(input) = input {
+            let result = const_eval(input.val().into(), lane);
+            self.stack.push_immediate(result)?;
+            return Ok(());
+        };
+        let input = self.layout.operand_to_reg(input)?;
+        self.push_instr_with_result(
+            <R as Typed>::TY,
+            |result| make_instr(result, input, lane),
+            FuelCostsProvider::simd,
+        )?;
+        Ok(())
+    }
 }
