df0d23b42478a83019137fcd2d48f0749bb75a38
Robin Freyler <robin.freyler@gmail.com>
2025-08-12 11:51:12 +0200

    apply rustfmt

--- crates/wasmi/src/engine/limits/stack.rs
+++ crates/wasmi/src/engine/limits/stack.rs
@@ -76,7 +76,7 @@ impl StackConfig {
     }
 
     /// Sets the maximum number of stacks that the [`Engine`] keeps for reuse.
-    /// 
+    ///
     /// [`Engine`]: crate::Engine
     pub fn set_max_cached_stacks(&mut self, value: usize) {
         self.max_recursion_depth = value;
@@ -98,7 +98,7 @@ impl StackConfig {
     }
 
     /// Returns the maximum number of stacks that the [`Engine`] keeps for reuse.
-    /// 
+    ///
     /// [`Engine`]: crate::Engine
     pub fn max_cached_stacks(&mut self) -> usize {
         self.max_cached_stacks
