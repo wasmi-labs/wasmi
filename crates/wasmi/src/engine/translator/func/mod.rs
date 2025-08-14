2a9b95f211aa941c71a4c5edb81c88e1421b3469
Robin Freyler <robin.freyler@gmail.com>
2025-08-14 15:28:49 +0200

    add `FuncTranslator::immediate_to_reg` helper method

--- crates/wasmi/src/engine/translator/func/mod.rs
+++ crates/wasmi/src/engine/translator/func/mod.rs
@@ -988,6 +988,47 @@ impl FuncTranslator {
         Ok(())
     }
 
+    /// Efficiently converts the `operand` to a [`Reg`] if it is an immediate.
+    ///
+    /// # Note
+    ///
+    /// - Preferrably, this encodes the immediate `operand` into a `copy` instruction
+    ///   with the immediate encoded inline.
+    /// - If the immediate `operand` cannot be encoded as `copy` with inline immediate
+    ///   a function local constant [`Reg`] will be allocated and returned.
+    /// - Returns the associated [`Reg`] if `operand` is an [`Operand::Temp`] or [`Operand::Local`].
+    fn immediate_to_reg(
+        &mut self,
+        operand: Operand,
+        consume_fuel: Option<Instr>,
+    ) -> Result<Reg, Error> {
+        match operand {
+            Operand::Local(operand) => self.layout.local_to_reg(operand.local_index()),
+            Operand::Temp(operand) => self.layout.temp_to_reg(operand.operand_index()),
+            Operand::Immediate(operand) => {
+                let value = operand.val();
+                let result = self.layout.temp_to_reg(operand.operand_index())?;
+                match Self::make_copy_imm_instr(result, value, &mut self.layout)? {
+                    Instruction::Copy { value, .. } => {
+                        // Case: not possible to craft a `copy` instruction
+                        //       with an inline immediate, so we can return
+                        //       the allocated function local constant [`Reg`]
+                        //       instead.
+                        Ok(value)
+                    }
+                    copy_instr => {
+                        self.instrs.push_instr(
+                            copy_instr,
+                            consume_fuel,
+                            FuelCostsProvider::base,
+                        )?;
+                        Ok(result)
+                    }
+                }
+            }
+        }
+    }
+
     /// Preserves all local operands on the stack.
     ///
     /// # Note
