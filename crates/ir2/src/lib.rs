56aa7083c15fcc6c3165e2b55fe0b98fed6b7543
Robin Freyler <robin.freyler@gmail.com>
2025-08-20 18:34:49 +0200

    add wasmi_ir2 crate contents

--- crates/ir2/src/lib.rs
+++ crates/ir2/src/lib.rs
@@ -1,5 +1,22 @@
 #![allow(warnings)]
+#![no_std]
 
+extern crate alloc;
+#[cfg(feature = "std")]
+extern crate std;
+
+mod error;
+mod index;
 mod ops;
+mod primitive;
+mod span;
 
-pub use self::ops::Instruction;
+use wasmi_core as core;
+
+pub use self::{
+    error::Error,
+    index::Stack,
+    ops::Instruction,
+    primitive::{Address, BlockFuel, BranchOffset, Offset16, Sign},
+    span::{BoundedStackSpan, FixedStackSpan, StackSpan},
+};
