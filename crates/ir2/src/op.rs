23641d949f42e8d1df3edc7f22b19108f6cda0b3
Robin Freyler <robin.freyler@gmail.com>
2025-08-20 20:10:47 +0200

    add Copy and Clone impls for Op

--- crates/ir2/src/op.rs
+++ crates/ir2/src/op.rs
@@ -2,3 +2,10 @@ use crate::{BranchOffset, Sign, Stack};
 use core::num::NonZero;
 
 include!(concat!(env!("OUT_DIR"), "/op.rs"));
+
+impl Copy for Op {}
+impl Clone for Op {
+    fn clone(&self) -> Self {
+        *self
+    }
+}
