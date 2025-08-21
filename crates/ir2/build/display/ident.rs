d31d3fabb8f6bc9f97df80d32b2b6251c893a74d
Robin Freyler <robin.freyler@gmail.com>
2025-08-21 17:07:12 +0200

    move DisplayIdent into ident submodule

---
+++ crates/ir2/build/display/ident.rs
@@ -0,0 +1,92 @@
+use crate::build::{
+    display::utils::IntoDisplayMaybe as _,
+    op::{Input, LoadOp, StoreOp},
+    token::{Case, Ident, SnakeCase},
+};
+use core::fmt::{self, Display};
+
+pub struct DisplayIdent<T> {
+    value: T,
+    case: Case,
+}
+
+impl<T> DisplayIdent<T> {
+    pub fn camel(value: T) -> Self {
+        Self {
+            value,
+            case: Case::Camel,
+        }
+    }
+
+    #[expect(dead_code)]
+    pub fn snake(value: T) -> Self {
+        Self {
+            value,
+            case: Case::Snake,
+        }
+    }
+}
+
+impl Display for DisplayIdent<&'_ LoadOp> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let case = self.case;
+        let kind = self.value.kind;
+        let ident = case.wrap(kind.ident());
+        let result_suffix = case.wrap(Input::Stack);
+        let ptr_suffix = SnakeCase(self.value.ptr);
+        let ident_prefix = self
+            .value
+            .kind
+            .ident_prefix()
+            .map(|v| case.wrap(v))
+            .display_maybe();
+        let mem0_ident = self
+            .value
+            .mem0
+            .then_some(Ident::Mem0)
+            .map(|v| case.wrap(v))
+            .display_maybe();
+        let offset16_ident = self
+            .value
+            .offset16
+            .then_some(Ident::Offset16)
+            .map(|v| case.wrap(v))
+            .display_maybe();
+        write!(
+            f,
+            "{ident_prefix}{ident}{mem0_ident}{offset16_ident}_{result_suffix}{ptr_suffix}",
+        )
+    }
+}
+
+impl Display for DisplayIdent<&'_ StoreOp> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let case = self.case;
+        let kind = self.value.kind;
+        let ident = case.wrap(kind.ident());
+        let ptr_suffix = case.wrap(self.value.ptr);
+        let value_suffix = SnakeCase(self.value.value);
+        let ident_prefix = self
+            .value
+            .kind
+            .ident_prefix()
+            .map(|v| case.wrap(v))
+            .display_maybe();
+        let mem0_ident = self
+            .value
+            .mem0
+            .then_some(Ident::Mem0)
+            .map(|v| case.wrap(v))
+            .display_maybe();
+        let offset16_ident = self
+            .value
+            .offset16
+            .then_some(Ident::Offset16)
+            .map(|v| case.wrap(v))
+            .display_maybe();
+        write!(
+            f,
+            "{ident_prefix}{ident}{mem0_ident}{offset16_ident}_{ptr_suffix}{value_suffix}",
+        )
+    }
+}
