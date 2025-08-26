4ca1e27cc8473d957217618305afb2896d9d287d
Robin Freyler <robin.freyler@gmail.com>
2025-08-26 09:52:27 +0200

    add Op constructor codegen

---
+++ crates/ir2/build/display/constructors.rs
@@ -0,0 +1,178 @@
+use crate::build::{
+    display::{
+        ident::DisplayIdent,
+        utils::{DisplayConcat, DisplaySequence},
+        Indent,
+    },
+    isa::Isa,
+    op::{
+        BinaryOp,
+        CmpBranchOp,
+        CmpSelectOp,
+        Field,
+        GenericOp,
+        LoadOp,
+        Op,
+        StoreOp,
+        TableGetOp,
+        TableSetOp,
+        UnaryOp,
+    },
+    token::SnakeCase,
+};
+use core::fmt::{self, Display};
+
+pub struct DisplayConstructor<T> {
+    pub value: T,
+    pub indent: Indent,
+}
+
+impl<T> DisplayConstructor<T> {
+    pub fn new(value: T, indent: Indent) -> Self {
+        Self { value, indent }
+    }
+
+    pub fn map<V>(&self, value: V) -> DisplayConstructor<V> {
+        DisplayConstructor {
+            value,
+            indent: self.indent,
+        }
+    }
+}
+
+impl<'a, T> DisplayConstructor<&'a T> {
+    fn display_constructor(&self, f: &mut fmt::Formatter, fields: &[Option<Field>]) -> fmt::Result
+    where
+        DisplayIdent<&'a T>: Display,
+    {
+        let indent = self.indent;
+        let snake_ident = DisplayIdent::snake(self.value);
+        let camel_ident = DisplayIdent::camel(self.value);
+        let fn_params = DisplaySequence(
+            fields
+                .iter()
+                .filter_map(Option::as_ref)
+                .map(|param| (param, ", "))
+                .map(DisplayConcat),
+        );
+        let struct_params = DisplaySequence(
+            fields
+                .iter()
+                .filter_map(Option::as_ref)
+                .map(|param| (SnakeCase(param.ident), ", "))
+                .map(DisplayConcat),
+        );
+        write!(
+            f,
+            "\
+            {indent}pub fn {snake_ident}({fn_params}) -> Self {{\n\
+            {indent}    Self::{camel_ident} {{ {struct_params} }}\n\
+            {indent}}}\n\
+            "
+        )
+    }
+}
+
+impl Display for DisplayConstructor<&'_ Isa> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let indent = self.indent;
+        let variants = DisplaySequence(
+            self.value
+                .ops
+                .iter()
+                .map(|op| DisplayConstructor::new(op, indent.inc_by(3))),
+        );
+        write!(
+            f,
+            "\
+            {indent}impl Op {{\n\
+                        {variants}\
+            {indent}}}\n\
+        "
+        )
+    }
+}
+
+impl Display for DisplayConstructor<&'_ Op> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self.value {
+            Op::Unary(op) => self.map(op).fmt(f),
+            Op::Binary(op) => self.map(op).fmt(f),
+            Op::CmpBranch(op) => self.map(op).fmt(f),
+            Op::CmpSelect(op) => self.map(op).fmt(f),
+            Op::Load(op) => self.map(op).fmt(f),
+            Op::Store(op) => self.map(op).fmt(f),
+            Op::TableGet(op) => self.map(op).fmt(f),
+            Op::TableSet(op) => self.map(op).fmt(f),
+            Op::Generic0(op) => self.map(op).fmt(f),
+            Op::Generic1(op) => self.map(op).fmt(f),
+            Op::Generic2(op) => self.map(op).fmt(f),
+            Op::Generic3(op) => self.map(op).fmt(f),
+            Op::Generic4(op) => self.map(op).fmt(f),
+            Op::Generic5(op) => self.map(op).fmt(f),
+        }
+    }
+}
+
+impl Display for DisplayConstructor<&'_ UnaryOp> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let fields = self.value.fields().map(Option::from);
+        self.display_constructor(f, &fields)
+    }
+}
+
+impl Display for DisplayConstructor<&'_ BinaryOp> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let fields = self.value.fields().map(Option::from);
+        self.display_constructor(f, &fields)
+    }
+}
+
+impl Display for DisplayConstructor<&'_ CmpBranchOp> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let fields = self.value.fields().map(Option::from);
+        self.display_constructor(f, &fields)
+    }
+}
+
+impl Display for DisplayConstructor<&'_ CmpSelectOp> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let fields = self.value.fields().map(Option::from);
+        self.display_constructor(f, &fields)
+    }
+}
+
+impl Display for DisplayConstructor<&'_ LoadOp> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let fields = self.value.fields();
+        self.display_constructor(f, &fields)
+    }
+}
+
+impl Display for DisplayConstructor<&'_ StoreOp> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let fields = self.value.fields();
+        self.display_constructor(f, &fields)
+    }
+}
+
+impl Display for DisplayConstructor<&'_ TableGetOp> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let fields = self.value.fields().map(Option::from);
+        self.display_constructor(f, &fields)
+    }
+}
+
+impl Display for DisplayConstructor<&'_ TableSetOp> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let fields = self.value.fields().map(Option::from);
+        self.display_constructor(f, &fields)
+    }
+}
+
+impl<const N: usize> Display for DisplayConstructor<&'_ GenericOp<N>> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let fields = self.value.fields.map(Option::from);
+        self.display_constructor(f, &fields)
+    }
+}
--- crates/ir2/build/display/mod.rs
+++ crates/ir2/build/display/mod.rs
@@ -1,6 +1,12 @@
+mod constructors;
 mod ident;
 mod op;
 mod result_mut;
 mod utils;
 
-pub use self::{op::DisplayOp, result_mut::DisplayResultMut, utils::Indent};
+pub use self::{
+    constructors::DisplayConstructor,
+    op::DisplayOp,
+    result_mut::DisplayResultMut,
+    utils::Indent,
+};
