f11440839adf2af737e409e26c3c51856a8cee3c
Robin Freyler <robin.freyler@gmail.com>
2025-09-03 12:43:17 +0200

    re-export wasmi_ir2 as ir2 within wasmi

--- crates/wasmi/src/lib.rs
+++ crates/wasmi/src/lib.rs
@@ -140,9 +140,13 @@ mod core {
 #[doc(inline)]
 use wasmi_collections as collections;
 
-/// Definitions from the `wasmi_collections` crate.
+/// Definitions from the `wasmi_ir` crate.
 #[doc(inline)]
 use wasmi_ir as ir;
+
+/// Definitions from the `wasmi_ir2` crate.
+#[doc(inline)]
+use wasmi_ir2 as ir2;
 
 /// Defines some errors that may occur upon interaction with Wasmi.
 pub mod errors {
