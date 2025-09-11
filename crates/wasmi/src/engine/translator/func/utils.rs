e4ec86a26dfe2b9a16415a8b0ebfd7c765f14442
Robin Freyler <robin.freyler@gmail.com>
2025-09-11 23:31:50 +0200

    move IntoShiftAmount utility trait

--- crates/wasmi/src/engine/translator/func/utils.rs
+++ crates/wasmi/src/engine/translator/func/utils.rs
@@ -56,31 +56,3 @@ pub enum Input<T> {
     /// A 16-bit encoded immediate value operand.
     Immediate(T),
 }
-
-pub trait IntoShiftAmount {
-    /// The type denoting the shift amount.
-    ///
-    /// This is an unsigned integer ranging from `1..N` where `N` is the number of bits in `Self`.
-    type Value: Copy;
-
-    /// Returns `self` wrapped into a proper shift amount for `Self`.
-    ///
-    /// Returns `None` if the resulting shift amount is 0, a.k.a. a no-op.
-    fn into_shift_amount(self) -> Option<Self::Value>;
-}
-
-macro_rules! impl_into_shift_amount {
-    ( $($ty:ty),* $(,)? ) => {
-        $(
-            impl IntoShiftAmount for $ty {
-                type Value = u8;
-
-                fn into_shift_amount(self) -> Option<Self::Value> {
-                    let len_bits = (::core::mem::size_of::<Self>() * 8) as Self;
-                    self.checked_rem_euclid(len_bits)
-                }
-            }
-        )*
-    };
-}
-impl_into_shift_amount!(i32, u32, i64, u64, i128, u128);
