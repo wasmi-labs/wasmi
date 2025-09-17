fa2a78c94b762b85ae1a7407360ad4eae148557b
Robin Freyler <robin.freyler@gmail.com>
2025-09-17 12:38:05 +0200

    simplify {local,temp}_to_slot usage where possible

--- crates/wasmi/src/engine/translator/func/simd/mod.rs
+++ crates/wasmi/src/engine/translator/func/simd/mod.rs
@@ -304,8 +304,8 @@ impl FuncTranslator {
                 //       lane value and translate as a more efficient non-SIMD operation.
                 return translate_imm(self, memarg, ptr, lane, V128::from(v128.val()));
             }
-            Operand::Local(v128) => self.layout.local_to_slot(v128.local_index())?,
-            Operand::Temp(v128) => self.layout.temp_to_slot(v128.operand_index())?,
+            Operand::Local(v128) => self.layout.local_to_slot(v128)?,
+            Operand::Temp(v128) => self.layout.temp_to_slot(v128)?,
         };
         let (memory, offset) = Self::decode_memarg(memarg)?;
         let ptr = self.copy_if_immediate(ptr)?;
