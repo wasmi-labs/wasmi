977e3a736c1e3851f94648e2e37ae45d0c86adb2
Robin Freyler <robin.freyler@gmail.com>
2025-09-17 14:33:44 +0200

    fix compile error in panic for make_copy_imm_instr

--- crates/wasmi/src/engine/translator/func/mod.rs
+++ crates/wasmi/src/engine/translator/func/mod.rs
@@ -58,8 +58,9 @@ use crate::{
     FuncType,
     TrapCode,
     ValType,
-    V128,
 };
+#[cfg(feature = "simd")]
+use crate::V128;
 use alloc::vec::Vec;
 use wasmparser::{MemArg, WasmFeatures};
 
@@ -481,7 +482,7 @@ impl FuncTranslator {
                 Op::copy128(result, value_lo, value_hi)
             }
             #[cfg(not(feature = "simd"))]
-            ValType::V128 => panic!("unexpected `v128` operand: {value}"),
+            ValType::V128 => panic!("unexpected `v128` operand: {value:?}"),
         };
         Ok(instr)
     }
