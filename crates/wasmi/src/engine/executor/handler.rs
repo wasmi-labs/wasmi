2bc92edee033bf3397f2d57667beff84ed0c11cf
Robin Freyler <robin.freyler@gmail.com>
2025-10-08 10:02:38 +0200

    add binary i32 operators

--- crates/wasmi/src/engine/executor/handler.rs
+++ crates/wasmi/src/engine/executor/handler.rs
@@ -7,13 +7,16 @@ use crate::{
     errors::HostError,
     instance::InstanceEntity,
     ir,
-    ir::OpCode,
-    ir::{decode, Slot},
+    ir::{decode, OpCode, Slot},
     store::PrunedStore,
     TrapCode,
 };
 use alloc::boxed::Box;
-use core::ptr::{self, NonNull};
+use core::{
+    num::NonZero,
+    ops::{Div, Rem},
+    ptr::{self, NonNull},
+};
 
 #[derive(Debug, Default, Copy, Clone)]
 pub struct Done {
@@ -131,18 +134,41 @@ trait GetValue<T> {
     fn get_value(src: Self, sp: Sp) -> T;
 }
 
+macro_rules! impl_get_value {
+    ( $($ty:ty),* $(,)? ) => {
+        $(
+            impl GetValue<$ty> for $ty {
+                #[inline(always)]
+                fn get_value(src: Self, _sp: Sp) -> $ty {
+                    src
+                }
+            }
+        )*
+    };
+}
+impl_get_value!(
+    u8,
+    u16,
+    u32,
+    u64,
+    i8,
+    i16,
+    i32,
+    i64,
+    f32,
+    f64,
+    NonZero<i32>,
+    NonZero<i64>,
+    NonZero<u32>,
+    NonZero<u64>,
+);
+
 impl<T> GetValue<T> for Slot
 where
     T: From<UntypedVal>,
 {
     fn get_value(src: Self, sp: Sp) -> T {
         sp.get::<T>(src)
-    }
-}
-
-impl GetValue<i32> for i32 {
-    fn get_value(src: Self, _sp: Sp) -> i32 {
-        src
     }
 }
 
@@ -207,7 +233,7 @@ macro_rules! impl_unwrap_result {
         )*
     };
 }
-impl_unwrap_result!(i32, i64, u32, u64, f32, f64);
+impl_unwrap_result!(bool, i32, i64, u32, u64, f32, f64);
 
 macro_rules! unwrap_result {
     ($value:expr, $state:expr) => {{
@@ -220,6 +246,7 @@ macro_rules! unwrap_result {
 
 fn op_code_to_handler(code: OpCode) -> Handler {
     match code {
+        // unary
         OpCode::I32Popcnt_Ss => i32_popcnt_ss,
         OpCode::I32Ctz_Ss => i32_ctz_ss,
         OpCode::I32Clz_Ss => i32_clz_ss,
@@ -272,6 +299,64 @@ fn op_code_to_handler(code: OpCode) -> Handler {
         OpCode::U64TruncSatF32_Ss => u64_trunc_sat_f32,
         OpCode::I64TruncSatF64_Ss => i64_trunc_sat_f64,
         OpCode::U64TruncSatF64_Ss => u64_trunc_sat_f64,
+        // binary
+        // i32
+        OpCode::I32Eq_Sss => i32_eq_sss,
+        OpCode::I32Eq_Ssi => i32_eq_ssi,
+        OpCode::I32NotEq_Sss => i32_not_eq_sss,
+        OpCode::I32NotEq_Ssi => i32_not_eq_ssi,
+        OpCode::I32Add_Sss => i32_add_sss,
+        OpCode::I32Add_Ssi => i32_add_ssi,
+        OpCode::I32Mul_Sss => i32_mul_sss,
+        OpCode::I32Mul_Ssi => i32_mul_ssi,
+        OpCode::I32BitAnd_Sss => i32_bitand_sss,
+        OpCode::I32BitAnd_Ssi => i32_bitand_ssi,
+        OpCode::I32BitOr_Sss => i32_bitor_sss,
+        OpCode::I32BitOr_Ssi => i32_bitor_ssi,
+        OpCode::I32BitXor_Sss => i32_bitxor_sss,
+        OpCode::I32BitXor_Ssi => i32_bitxor_ssi,
+        OpCode::I32Sub_Sss => i32_sub_sss,
+        OpCode::I32Sub_Ssi => i32_sub_ssi,
+        OpCode::I32Sub_Sis => i32_sub_sis,
+        OpCode::I32Div_Sss => i32_div_sss,
+        OpCode::I32Div_Ssi => i32_div_ssi,
+        OpCode::I32Div_Sis => i32_div_sis,
+        OpCode::U32Div_Sss => u32_div_sss,
+        OpCode::U32Div_Ssi => u32_div_ssi,
+        OpCode::U32Div_Sis => u32_div_sis,
+        OpCode::I32Rem_Sss => i32_rem_sss,
+        OpCode::I32Rem_Ssi => i32_rem_ssi,
+        OpCode::I32Rem_Sis => i32_rem_sis,
+        OpCode::U32Rem_Sss => u32_rem_sss,
+        OpCode::U32Rem_Ssi => u32_rem_ssi,
+        OpCode::U32Rem_Sis => u32_rem_sis,
+        OpCode::I32Le_Sss => i32_le_sss,
+        OpCode::I32Le_Ssi => i32_le_ssi,
+        OpCode::I32Le_Sis => i32_le_sis,
+        OpCode::I32Lt_Sss => i32_lt_sss,
+        OpCode::I32Lt_Ssi => i32_lt_ssi,
+        OpCode::I32Lt_Sis => i32_lt_sis,
+        OpCode::U32Le_Sss => u32_le_sss,
+        OpCode::U32Le_Ssi => u32_le_ssi,
+        OpCode::U32Le_Sis => u32_le_sis,
+        OpCode::U32Lt_Sss => u32_lt_sss,
+        OpCode::U32Lt_Ssi => u32_lt_ssi,
+        OpCode::U32Lt_Sis => u32_lt_sis,
+        OpCode::I32Shl_Sss => i32_shl_sss,
+        OpCode::I32Shl_Ssi => i32_shl_ssi,
+        OpCode::I32Shl_Sis => i32_shl_sis,
+        OpCode::I32Shr_Sss => i32_shr_sss,
+        OpCode::I32Shr_Ssi => i32_shr_ssi,
+        OpCode::I32Shr_Sis => i32_shr_sis,
+        OpCode::U32Shr_Sss => u32_shr_sss,
+        OpCode::U32Shr_Ssi => u32_shr_ssi,
+        OpCode::U32Shr_Sis => u32_shr_sis,
+        OpCode::I32Rotl_Sss => i32_rotl_sss,
+        OpCode::I32Rotl_Ssi => i32_rotl_ssi,
+        OpCode::I32Rotl_Sis => i32_rotl_sis,
+        OpCode::I32Rotr_Sss => i32_rotr_sss,
+        OpCode::I32Rotr_Ssi => i32_rotr_ssi,
+        OpCode::I32Rotr_Sis => i32_rotr_sis,
         _ => todo!(),
     }
 }
@@ -355,34 +440,139 @@ handler_unary! {
     fn u64_trunc_sat_f64(U64TruncSatF64_Ss) = wasm::i64_trunc_sat_f64_u;
 }
 
-fn i32_add_sss(
-    state: &mut VmState,
-    ip: Ip,
-    sp: Sp,
-    mem0: *mut u8,
-    mem0_len: usize,
-    instance: NonNull<InstanceEntity>,
-) -> Done {
-    let (ip, decode::I32Add_Sss { result, lhs, rhs }) = unsafe { ip.decode() };
-    let lhs = get_value(lhs, sp);
-    let rhs = get_value(rhs, sp);
-    set_value(sp, result, wasm::i32_add(lhs, rhs));
-    dispatch!(state, ip, sp, mem0, mem0_len, instance)
+macro_rules! handler_binary {
+    ( $( fn $handler:ident($decode:ident) = $eval:expr );* $(;)? ) => {
+        $(
+            fn $handler(
+                state: &mut VmState,
+                ip: Ip,
+                sp: Sp,
+                mem0: *mut u8,
+                mem0_len: usize,
+                instance: NonNull<InstanceEntity>,
+            ) -> Done {
+                let (ip, $crate::ir::decode::$decode { result, lhs, rhs }) = unsafe { ip.decode() };
+                let lhs = get_value(lhs, sp);
+                let rhs = get_value(rhs, sp);
+                set_value(sp, result, unwrap_result!($eval(lhs, rhs), state));
+                dispatch!(state, ip, sp, mem0, mem0_len, instance)
+            }
+        )*
+    };
+}
+handler_binary! {
+    // i32: commutative
+    fn i32_eq_sss(I32Eq_Sss) = wasm::i32_eq;
+    fn i32_eq_ssi(I32Eq_Ssi) = wasm::i32_eq;
+    fn i32_not_eq_sss(I32NotEq_Sss) = wasm::i32_ne;
+    fn i32_not_eq_ssi(I32NotEq_Ssi) = wasm::i32_ne;
+    fn i32_add_sss(I32Add_Sss) = wasm::i32_add;
+    fn i32_add_ssi(I32Add_Ssi) = wasm::i32_add;
+    fn i32_mul_sss(I32Mul_Sss) = wasm::i32_mul;
+    fn i32_mul_ssi(I32Mul_Ssi) = wasm::i32_mul;
+    fn i32_bitand_sss(I32BitAnd_Sss) = wasm::i32_bitand;
+    fn i32_bitand_ssi(I32BitAnd_Ssi) = wasm::i32_bitand;
+    fn i32_bitor_sss(I32BitOr_Sss) = wasm::i32_bitor;
+    fn i32_bitor_ssi(I32BitOr_Ssi) = wasm::i32_bitor;
+    fn i32_bitxor_sss(I32BitXor_Sss) = wasm::i32_bitxor;
+    fn i32_bitxor_ssi(I32BitXor_Ssi) = wasm::i32_bitxor;
+    // i32: non-commutative
+    fn i32_sub_sss(I32Sub_Sss) = wasm::i32_sub;
+    fn i32_sub_ssi(I32Sub_Ssi) = wasm::i32_sub;
+    fn i32_sub_sis(I32Sub_Sis) = wasm::i32_sub;
+    fn i32_div_sss(I32Div_Sss) = wasm::i32_div_s;
+    fn i32_div_ssi(I32Div_Ssi) = wasmi_i32_div_ssi;
+    fn i32_div_sis(I32Div_Sis) = wasm::i32_div_s;
+    fn u32_div_sss(U32Div_Sss) = wasm::i32_div_u;
+    fn u32_div_ssi(U32Div_Ssi) = wasmi_u32_div_ssi;
+    fn u32_div_sis(U32Div_Sis) = wasm::i32_div_u;
+    fn i32_rem_sss(I32Rem_Sss) = wasm::i32_rem_s;
+    fn i32_rem_ssi(I32Rem_Ssi) = wasmi_i32_rem_ssi;
+    fn i32_rem_sis(I32Rem_Sis) = wasm::i32_rem_s;
+    fn u32_rem_sss(U32Rem_Sss) = wasm::i32_rem_u;
+    fn u32_rem_ssi(U32Rem_Ssi) = wasmi_u32_rem_ssi;
+    fn u32_rem_sis(U32Rem_Sis) = wasm::i32_rem_u;
+    // i32: comparisons
+    fn i32_le_sss(I32Le_Sss) = wasm::i32_le_s;
+    fn i32_le_ssi(I32Le_Ssi) = wasm::i32_le_s;
+    fn i32_le_sis(I32Le_Sis) = wasm::i32_le_s;
+    fn i32_lt_sss(I32Lt_Sss) = wasm::i32_lt_s;
+    fn i32_lt_ssi(I32Lt_Ssi) = wasm::i32_lt_s;
+    fn i32_lt_sis(I32Lt_Sis) = wasm::i32_lt_s;
+    fn u32_le_sss(U32Le_Sss) = wasm::i32_le_u;
+    fn u32_le_ssi(U32Le_Ssi) = wasm::i32_le_u;
+    fn u32_le_sis(U32Le_Sis) = wasm::i32_le_u;
+    fn u32_lt_sss(U32Lt_Sss) = wasm::i32_lt_u;
+    fn u32_lt_ssi(U32Lt_Ssi) = wasm::i32_lt_u;
+    fn u32_lt_sis(U32Lt_Sis) = wasm::i32_lt_u;
+    // i32: shift + rotate
+    fn i32_shl_sss(I32Shl_Sss) = wasm::i32_shl;
+    fn i32_shl_ssi(I32Shl_Ssi) = wasmi_i32_shl_ssi;
+    fn i32_shl_sis(I32Shl_Sis) = wasm::i32_shl;
+    fn i32_shr_sss(I32Shr_Sss) = wasm::i32_shr_s;
+    fn i32_shr_ssi(I32Shr_Ssi) = wasmi_i32_shr_ssi;
+    fn i32_shr_sis(I32Shr_Sis) = wasm::i32_shr_s;
+    fn u32_shr_sss(U32Shr_Sss) = wasm::i32_shr_u;
+    fn u32_shr_ssi(U32Shr_Ssi) = wasmi_u32_shr_ssi;
+    fn u32_shr_sis(U32Shr_Sis) = wasm::i32_shr_u;
+    fn i32_rotl_sss(I32Rotl_Sss) = wasm::i32_rotl;
+    fn i32_rotl_ssi(I32Rotl_Ssi) = wasmi_i32_rotl_ssi;
+    fn i32_rotl_sis(I32Rotl_Sis) = wasm::i32_rotl;
+    fn i32_rotr_sss(I32Rotr_Sss) = wasm::i32_rotr;
+    fn i32_rotr_ssi(I32Rotr_Ssi) = wasmi_i32_rotr_ssi;
+    fn i32_rotr_sis(I32Rotr_Sis) = wasm::i32_rotr;
 }
 
-fn i32_add_ssi(
-    state: &mut VmState,
-    ip: Ip,
-    sp: Sp,
-    mem0: *mut u8,
-    mem0_len: usize,
-    instance: NonNull<InstanceEntity>,
-) -> Done {
-    let (ip, decode::I32Add_Ssi { result, lhs, rhs }) = unsafe { ip.decode() };
-    let lhs = get_value(lhs, sp);
-    let rhs = get_value(rhs, sp);
-    set_value(sp, result, wasm::i32_add(lhs, rhs));
-    dispatch!(state, ip, sp, mem0, mem0_len, instance)
+fn wasmi_i32_div_ssi(lhs: i32, rhs: NonZero<i32>) -> Result<i32, TrapCode> {
+    wasm::i32_div_s(lhs, rhs.get())
+}
+
+fn wasmi_i64_div_ssi(lhs: i64, rhs: NonZero<i64>) -> Result<i64, TrapCode> {
+    wasm::i64_div_s(lhs, rhs.get())
+}
+
+fn wasmi_u32_div_ssi(lhs: u32, rhs: NonZero<u32>) -> u32 {
+    <u32 as Div<NonZero<u32>>>::div(lhs, rhs)
+}
+
+fn wasmi_u64_div_ssi(lhs: u64, rhs: NonZero<u64>) -> u64 {
+    <u64 as Div<NonZero<u64>>>::div(lhs, rhs)
+}
+
+fn wasmi_i32_rem_ssi(lhs: i32, rhs: NonZero<i32>) -> Result<i32, TrapCode> {
+    wasm::i32_rem_s(lhs, rhs.get())
+}
+
+fn wasmi_i64_rem_ssi(lhs: i64, rhs: NonZero<i64>) -> Result<i64, TrapCode> {
+    wasm::i64_rem_s(lhs, rhs.get())
+}
+
+fn wasmi_u32_rem_ssi(lhs: u32, rhs: NonZero<u32>) -> u32 {
+    <u32 as Rem<NonZero<u32>>>::rem(lhs, rhs)
+}
+
+fn wasmi_u64_rem_ssi(lhs: u64, rhs: NonZero<u64>) -> u64 {
+    <u64 as Rem<NonZero<u64>>>::rem(lhs, rhs)
+}
+
+fn wasmi_i32_shl_ssi(lhs: i32, rhs: u8) -> i32 {
+    wasm::i32_shl(lhs, i32::from(rhs))
+}
+
+fn wasmi_i32_shr_ssi(lhs: i32, rhs: u8) -> i32 {
+    wasm::i32_shr_s(lhs, i32::from(rhs))
+}
+
+fn wasmi_u32_shr_ssi(lhs: u32, rhs: u8) -> u32 {
+    wasm::i32_shr_u(lhs, u32::from(rhs))
+}
+
+fn wasmi_i32_rotl_ssi(lhs: i32, rhs: u8) -> i32 {
+    wasm::i32_rotl(lhs, i32::from(rhs))
+}
+
+fn wasmi_i32_rotr_ssi(lhs: i32, rhs: u8) -> i32 {
+    wasm::i32_rotr(lhs, i32::from(rhs))
 }
 
 fn branch_i32_eq_ss(
