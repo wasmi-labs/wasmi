10c40910b30eac434b0212a1f7545dcf85d400b9
Robin Freyler <robin.freyler@gmail.com>
2025-10-15 12:39:57 +0200

    implement Stack, ValueStack and CallStack

--- crates/wasmi/src/engine/executor/handler/state.rs
+++ crates/wasmi/src/engine/executor/handler/state.rs
@@ -1,17 +1,16 @@
 use crate::{
+    collections::HeadVec,
     core::UntypedVal,
-    engine::executor::{
-        stack::{CallStack, ValueStack},
-        CodeMap,
-    },
+    engine::executor::CodeMap,
     errors::HostError,
     instance::InstanceEntity,
     ir,
-    ir::Slot,
+    ir::{Slot, SlotSpan},
     store::PrunedStore,
+    Instance,
     TrapCode,
 };
-use alloc::boxed::Box;
+use alloc::{boxed::Box, vec::Vec};
 use core::ptr::{self, NonNull};
 
 #[derive(Debug, Default, Copy, Clone)]
@@ -77,6 +76,14 @@ pub struct Sp {
     value: *mut UntypedVal,
 }
 
+impl<'a> From<&'a mut [UntypedVal]> for Sp {
+    fn from(value: &'a mut [UntypedVal]) -> Self {
+        Self {
+            value: value.as_mut_ptr(),
+        }
+    }
+}
+
 impl Sp {
     pub fn get<T>(self, slot: Slot) -> T
     where
@@ -95,4 +102,123 @@ impl Sp {
         let cell = unsafe { &mut *self.value.add(index) };
         *cell = value.into();
     }
+}
+
+pub struct Stack {
+    values: ValueStack,
+    frames: CallStack,
+}
+
+impl Stack {
+    pub fn push_frame(
+        &mut self,
+        ip: Ip,
+        params: SlotSpan,
+        size: usize,
+        state: &VmState,
+        instance: Option<Instance>,
+    ) -> Result<Sp, TrapCode> {
+        let delta = usize::from(u16::from(params.head()));
+        let Some(start) = self.frames.top_start().checked_add(delta) else {
+            return Err(TrapCode::StackOverflow);
+        };
+        let sp = self.values.push(start, size)?;
+        self.frames.push(ip, start, state, instance)?;
+        Ok(sp)
+    }
+
+    pub fn pop_frame(
+        &mut self,
+        instance: NonNull<InstanceEntity>,
+    ) -> (Sp, NonNull<InstanceEntity>) {
+        let (start, changed_instance) = self.frames.pop();
+        let sp = self.values.sp(start);
+        if let Some(instance) = changed_instance {
+            return (sp, instance);
+        }
+        (sp, instance)
+    }
+}
+
+pub struct ValueStack {
+    cells: Vec<UntypedVal>,
+    max_height: usize,
+}
+
+impl ValueStack {
+    fn sp(&mut self, start: usize) -> Sp {
+        Sp::from(&mut self.cells[start..]) // TODO: maybe avoid bounds check if necessary for performance
+    }
+
+    fn push(&mut self, start: usize, size: usize) -> Result<Sp, TrapCode> {
+        let Some(end) = start.checked_add(size) else {
+            return Err(TrapCode::StackOverflow);
+        };
+        if end > self.max_height {
+            return Err(TrapCode::StackOverflow);
+        }
+        self.cells.resize(end, UntypedVal::default());
+        let sp = self.sp(start);
+        Ok(sp)
+    }
+}
+
+pub struct CallStack {
+    frames: Vec<Frame>,
+    instances: HeadVec<NonNull<InstanceEntity>>,
+    max_height: usize,
+}
+
+impl CallStack {
+    fn top_start(&self) -> usize {
+        let Some(top) = self.top() else { return 0 };
+        top.start
+    }
+
+    fn top(&self) -> Option<&Frame> {
+        self.frames.last()
+    }
+
+    fn push(
+        &mut self,
+        ip: Ip,
+        start: usize,
+        state: &VmState,
+        instance: Option<Instance>,
+    ) -> Result<(), TrapCode> {
+        if self.frames.len() == self.max_height {
+            return Err(TrapCode::StackOverflow);
+        }
+        let changes_instance = instance.is_some();
+        if let Some(instance) = instance {
+            let entity = state.store.inner().resolve_instance(&instance);
+            self.instances.push(entity.into());
+        }
+        self.frames.push(Frame {
+            ip,
+            start,
+            changes_instance,
+        });
+        Ok(())
+    }
+
+    fn pop(&mut self) -> (usize, Option<NonNull<InstanceEntity>>) {
+        let Some(popped) = self.frames.pop() else {
+            panic!("unexpected empty frame stack") // TODO: return `Result` instead of panicking
+        };
+        let start = self.top_start();
+        if popped.changes_instance {
+            let Some(instance) = self.instances.pop() else {
+                panic!("unexpected empty instance stack") // TODO: return `Result` instead of panicking
+            };
+            return (start, Some(instance));
+        }
+        (start, None)
+    }
+}
+
+pub struct Frame {
+    pub ip: Ip,
+    start: usize,
+    changes_instance: bool,
 }
