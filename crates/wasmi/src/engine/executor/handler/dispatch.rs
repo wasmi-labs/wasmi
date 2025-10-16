9b9d56b2bae524e90df11826bd3cba3fbd952ac6
Robin Freyler <robin.freyler@gmail.com>
2025-10-16 14:26:11 +0200

    add return execution handler

--- crates/wasmi/src/engine/executor/handler/dispatch.rs
+++ crates/wasmi/src/engine/executor/handler/dispatch.rs
@@ -82,6 +82,8 @@ fn op_code_to_handler(code: OpCode) -> Handler {
         OpCode::GlobalSet => exec::global_set,
         OpCode::GlobalSet32 => exec::global_set_32,
         OpCode::GlobalSet64 => exec::global_set_64,
+        // return
+        OpCode::Return => exec::r#return,
         // call
         OpCode::CallInternal => exec::call_internal,
         // unconditional branch
