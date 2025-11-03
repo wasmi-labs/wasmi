f755625f7e0d3b7fed1e975be4b6d43b85f299d3
Robin Freyler <robin.freyler@gmail.com>
2025-11-03 22:15:30 +0100

    apply rustfmt

--- crates/wasmi/src/engine/executor/handler/mod.rs
+++ crates/wasmi/src/engine/executor/handler/mod.rs
@@ -8,11 +8,6 @@ mod state;
 
 use self::{dispatch::Done, state::DoneReason};
 pub use self::{
-    dispatch::{
-        init_wasm_func_call,
-        op_code_to_handler,
-        ControlFlowBreak,
-        ControlFlowContinue,
-    },
+    dispatch::{init_wasm_func_call, op_code_to_handler, ControlFlowBreak, ControlFlowContinue},
     state::{Inst, Stack},
 };
