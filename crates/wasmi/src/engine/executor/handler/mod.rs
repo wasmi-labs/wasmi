a021fe7c5d80151e0b9d4a2dd4811ba8cbbf8bec
Robin Freyler <robin.freyler@gmail.com>
2025-11-03 22:14:14 +0100

    implement Done in terms of ControlFlow
    This allows for better modularity of execution helpers by allowing for the use of the ? operator in certain circumstances.

--- crates/wasmi/src/engine/executor/handler/mod.rs
+++ crates/wasmi/src/engine/executor/handler/mod.rs
@@ -8,6 +8,11 @@ mod state;
 
 use self::{dispatch::Done, state::DoneReason};
 pub use self::{
-    dispatch::{init_wasm_func_call, op_code_to_handler},
+    dispatch::{
+        init_wasm_func_call,
+        op_code_to_handler,
+        ControlFlowBreak,
+        ControlFlowContinue,
+    },
     state::{Inst, Stack},
 };
