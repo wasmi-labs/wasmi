ae1dff1398842a66cc05c2a6041fb9648eeaf6c8
Robin Freyler <robin.freyler@gmail.com>
2025-11-04 10:19:56 +0100

    remove unused ControlContinue with trampolines

--- crates/wasmi/src/engine/executor/handler/mod.rs
+++ crates/wasmi/src/engine/executor/handler/mod.rs
@@ -6,11 +6,13 @@ mod eval;
 mod exec;
 mod state;
 
+#[cfg(feature = "trampolines")]
+use self::dispatch::ControlContinue;
 pub use self::{
     dispatch::{init_wasm_func_call, op_code_to_handler},
     state::{Inst, Stack},
 };
 use self::{
-    dispatch::{Control, ControlBreak, ControlContinue, Done},
+    dispatch::{Control, ControlBreak, Done},
     state::DoneReason,
 };
