28e2a1e0c9a3d7f93178ea3c864640e970d6a4df
Robin Freyler <robin.freyler@gmail.com>
2025-11-14 13:37:24 +0100

    move WasmFuncCall and HostFuncCall into func submodule

--- crates/wasmi/src/engine/executor/handler/mod.rs
+++ crates/wasmi/src/engine/executor/handler/mod.rs
@@ -4,18 +4,14 @@ mod dispatch;
 mod utils;
 mod eval;
 mod exec;
+mod func;
 mod state;
 
 #[cfg(feature = "portable-dispatch")]
 use self::dispatch::ControlContinue;
 pub use self::{
-    dispatch::{
-        init_host_func_call,
-        init_wasm_func_call,
-        op_code_to_handler,
-        resume_wasm_func_call,
-        ExecutionOutcome,
-    },
+    dispatch::{op_code_to_handler, ExecutionOutcome},
+    func::{init_host_func_call, init_wasm_func_call, resume_wasm_func_call},
     state::{Inst, Stack},
 };
 use self::{
