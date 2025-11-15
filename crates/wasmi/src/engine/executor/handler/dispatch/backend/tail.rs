0f58dee288da4de322f78746765e3c6513188d8d
Robin Freyler <robin.freyler@gmail.com>
2025-11-15 12:16:58 +0100

    improve portable-dispatch codegen

--- crates/wasmi/src/engine/executor/handler/dispatch/backend/tail.rs
+++ crates/wasmi/src/engine/executor/handler/dispatch/backend/tail.rs
@@ -1,10 +1,30 @@
-use crate::engine::executor::handler::{
-    dispatch::{fetch_handler, Break, Control, ExecutionOutcome},
-    state::{Inst, Ip, Mem0Len, Mem0Ptr, Sp, VmState},
+use crate::{
+    engine::executor::handler::{
+        dispatch::{fetch_handler, Break, Control, ExecutionOutcome},
+        exec,
+        state::{Inst, Ip, Mem0Len, Mem0Ptr, Sp, VmState},
+    },
+    ir,
+    ir::OpCode,
 };
 
 pub enum Never {}
 pub type Done = Control<Never, Break>;
+
+pub type Handler =
+    fn(&mut VmState, ip: Ip, sp: Sp, mem0: Mem0Ptr, mem0_len: Mem0Len, instance: Inst) -> Done;
+
+macro_rules! expand_op_code_to_handler {
+    ( $( $snake_case:ident => $camel_case:ident ),* $(,)? ) => {
+        #[inline(always)]
+        pub fn op_code_to_handler(code: OpCode) -> Handler {
+            match code {
+                $( ir::OpCode::$camel_case => exec::$snake_case, )*
+            }
+        }
+    };
+}
+ir::for_each_op!(expand_op_code_to_handler);
 
 macro_rules! dispatch {
     ($state:expr, $ip:expr, $sp:expr, $mem0:expr, $mem0_len:expr, $instance:expr) => {{
