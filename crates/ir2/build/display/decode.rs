a355168893c69ec6801c1c5f44735f7d3965f6c7
Robin Freyler <robin.freyler@gmail.com>
2025-11-25 13:06:13 +0100

    fix simd splat execution handlers

--- crates/ir2/build/display/decode.rs
+++ crates/ir2/build/display/decode.rs
@@ -62,8 +62,8 @@ impl Display for DisplayDecode<&'_ Isa> {
 impl Display for DisplayDecode<&'_ UnaryOp> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         let camel_ident = DisplayIdent::camel(self.value);
-        let slot_ty = FieldTy::Slot;
-        writeln!(f, "pub type {camel_ident} = UnaryOp<{slot_ty}>;")
+        let value_ty = self.value.value_field().ty;
+        writeln!(f, "pub type {camel_ident} = UnaryOp<{value_ty}>;")
     }
 }
 
