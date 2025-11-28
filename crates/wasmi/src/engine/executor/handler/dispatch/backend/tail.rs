363000406e64f14bd173a1a70330f670e0bdb49c
Robin Freyler <robin.freyler@gmail.com>
2025-11-28 22:19:15 +0100

    apply some clippy suggestions

--- crates/wasmi/src/engine/executor/handler/dispatch/backend/tail.rs
+++ crates/wasmi/src/engine/executor/handler/dispatch/backend/tail.rs
@@ -52,9 +52,8 @@ pub fn execute_until_done(
     mem0_len: Mem0Len,
     instance: Inst,
 ) -> Result<Sp, ExecutionOutcome> {
-    let mut state = state;
     let handler = fetch_handler(ip);
-    let Control::Break(reason) = handler(&mut state, ip, sp, mem0, mem0_len, instance);
+    let Control::Break(reason) = handler(state, ip, sp, mem0, mem0_len, instance);
     if let Some(trap_code) = reason.trap_code() {
         return Err(ExecutionOutcome::from(trap_code));
     }
