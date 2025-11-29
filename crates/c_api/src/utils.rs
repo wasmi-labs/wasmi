de4e1d1ea68bebfba5a04ab1a513197fa9ac4c9e
Robin Freyler <robin.freyler@gmail.com>
2025-11-29 11:51:56 +0100

    remove dead_code in Wasmi's c-api

--- crates/c_api/src/utils.rs
+++ crates/c_api/src/utils.rs
@@ -1,5 +1,3 @@
-#![allow(dead_code)] // TODO: remove when all warnings are gone
-
 use core::{ffi, mem::MaybeUninit, ptr, slice};
 
 /// Wrapper for running a C-defined finalizer over foreign data upon [`Drop`].
@@ -29,24 +27,6 @@ pub unsafe fn slice_from_raw_parts<'a, T>(ptr: *const T, len: usize) -> &'a [T] 
         0 => &[],
         _ => slice::from_raw_parts(ptr, len),
     }
-}
-
-/// Convenience method for creating a mutable Rust slice from C inputs.
-///
-/// # Note
-///
-/// Returns an empty Rust slice if `len` is 0 disregarding `ptr`.
-pub unsafe fn slice_from_raw_parts_mut<'a, T>(ptr: *mut T, len: usize) -> &'a mut [T] {
-    match len {
-        0 => &mut [],
-        _ => slice::from_raw_parts_mut(ptr, len),
-    }
-}
-
-/// Aborts the execution with a message.
-#[allow(clippy::empty_loop)] // TODO: implement this properly for both, no_std and std modes
-pub fn abort(_message: &str) -> ! {
-    loop {}
 }
 
 /// Initialize a `MaybeUninit<T>` with `value`.
