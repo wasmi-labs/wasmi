cf877d73b6c84f9fd1ba07378583718ac79b7447
Robin Freyler <robin.freyler@gmail.com>
2025-12-18 13:46:40 +0100

    move Cell definitions into their own submodule

--- crates/wasmi/src/engine/executor/handler/mod.rs
+++ crates/wasmi/src/engine/executor/handler/mod.rs
@@ -2,15 +2,17 @@
 mod dispatch;
 #[macro_use]
 mod utils;
+mod cell;
 mod eval;
 mod exec;
 mod func;
 mod state;
 
 pub use self::{
+    cell::{Cell, LoadAs, StoreAs},
     dispatch::{op_code_to_handler, ExecutionOutcome},
     func::{init_host_func_call, init_wasm_func_call, resume_wasm_func_call},
-    state::{Cell, Inst, LoadAs, Stack, StoreAs},
+    state::{Inst, Stack},
 };
 use self::{
     dispatch::{Break, Control, Done},
