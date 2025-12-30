c14cf9f8de49a2a915ba4682cd6aad60df6b1fc2
Robin Freyler <robin.freyler@gmail.com>
2025-12-30 12:12:27 +0100

    re-export cell definitions

--- crates/wasmi/src/engine/executor/handler/mod.rs
+++ crates/wasmi/src/engine/executor/handler/mod.rs
@@ -8,6 +8,22 @@ mod exec;
 mod func;
 mod state;
 
+pub use self::{
+    cell::{
+        load_from_cells,
+        load_from_cells_into,
+        store_to_cells,
+        Cell,
+        CellError,
+        CellsReader,
+        CellsWriter,
+        LoadFromCells,
+        StoreToCells,
+    },
+    dispatch::{op_code_to_handler, ExecutionOutcome},
+    func::{init_host_func_call, init_wasm_func_call, resume_wasm_func_call},
+    state::{Inst, Stack},
+};
 use self::{
     dispatch::{Break, Control, Done},
     state::DoneReason,
