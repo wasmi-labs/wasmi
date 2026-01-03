ce67a4124a5bed8cbca1630c670286ad073d28ec
Robin Freyler <robin.freyler@gmail.com>
2026-01-03 01:10:15 +0100

    fix compile errors in typed_func.rs

--- crates/wasmi/src/func/typed_func.rs
+++ crates/wasmi/src/func/typed_func.rs
@@ -1,7 +1,7 @@
 use super::{Func, into_func::WasmTyList};
 use crate::{
     core::UntypedVal,
-    engine::{CallParams, CallResults, LoadFromCells, StoreToCells},
+    engine::{CallParams, CallResults, LoadByVal, LoadFromCellsByValue, StoreToCells},
     AsContext,
     AsContextMut,
     Error,
@@ -100,8 +100,8 @@ where
         ctx.as_context().store.engine().clone().execute_func(
             ctx.as_context_mut(),
             &self.func,
-            params,
-            <CallResultsTuple<Results>>::default(),
+            &params,
+            <LoadByVal<Results>>::default(),
         )
     }
 
@@ -133,8 +133,8 @@ where
             .execute_func_resumable(
                 ctx.as_context_mut(),
                 &self.func,
-                params,
-                <CallResultsTuple<Results>>::default(),
+                &params,
+                <LoadByVal<Results>>::default(),
             )
             .map(TypedResumableCall::new)
     }
@@ -200,5 +200,5 @@ pub trait WasmParams: WasmTyList + StoreToCells {}
 impl<T> WasmParams for T where T: WasmTyList + StoreToCells {}
 
 /// The typed results of a [`TypedFunc`].
-pub trait WasmResults: WasmTyList + LoadFromCells {}
-impl<T> WasmResults for T where T: WasmTyList + LoadFromCells {}
+pub trait WasmResults: WasmTyList + LoadFromCellsByValue {}
+impl<T> WasmResults for T where T: WasmTyList + LoadFromCellsByValue {}
