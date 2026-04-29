1788f81452fcb5da37971aba03c01d01585b2007
Robin Freyler <robin.freyler@gmail.com>
2026-04-29 13:02:13 +0200

    remove Ireg, Freg32 and Freg64 type aliases

--- crates/ir/src/lib.rs
+++ crates/ir/src/lib.rs
@@ -23,17 +23,6 @@ pub use self::{
     index::Slot,
     op::Op,
     opcode::{InvalidOpCode, OpCode},
-    primitive::{
-        Address,
-        BlockFuel,
-        BranchOffset,
-        BranchTableTarget,
-        Freg32,
-        Freg64,
-        Ireg,
-        Offset16,
-        Reg,
-        Sign,
-    },
+    primitive::{Address, BlockFuel, BranchOffset, BranchTableTarget, Offset16, Reg, Sign},
     span::{BoundedSlotSpan, FixedSlotSpan, SlotSpan, SlotSpanIter},
 };
