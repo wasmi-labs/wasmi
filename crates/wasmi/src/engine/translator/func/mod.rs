6cb0018c0f3c26ec57e3a7abb059e51acbfdafad
Robin Freyler <robin.freyler@gmail.com>
2026-05-01 15:31:18 +0200

    resolve todo in copy_many_strip_noop_start

--- crates/wasmi/src/engine/translator/func/mod.rs
+++ crates/wasmi/src/engine/translator/func/mod.rs
@@ -683,7 +683,10 @@ impl FuncTranslator {
         while let Some((value, rest)) = values.split_first() {
             let ty = value.ty();
             let value = match value {
-                Operand::Reg(_value) => todo!(),
+                Operand::Reg(_value) => {
+                    // Register values will never yield no-op copies.
+                    break;
+                }
                 Operand::Local(value) => layout.local_to_slot(value)?,
                 Operand::Temp(value) => value.temp_slots().head(),
                 Operand::Immediate(_) => {
