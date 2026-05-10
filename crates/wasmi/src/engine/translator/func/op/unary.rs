6dd22a1e1db5094e20603fc5c35b4b98f852264c
Robin Freyler <robin.freyler@gmail.com>
2026-05-10 02:38:15 +0200

    put IntoResult trait into utils submodule

--- crates/wasmi/src/engine/translator/func/op/unary.rs
+++ crates/wasmi/src/engine/translator/func/op/unary.rs
@@ -3,6 +3,7 @@ use crate::{
     core::{Typed, wasm},
     ir::{Op, Reg, Slot},
 };
+use super::IntoResult as _;
 
 pub trait UnaryOp {
     type Result;
@@ -46,49 +47,6 @@ macro_rules! impl_unary_op_for {
             }
         )*
     };
-}
-
-/// Helper trait to convert values to `Result` values.
-pub trait IntoResult<E>: Sized {
-    /// The value part of the resulting `Result` value.
-    type Val;
-
-    /// Converts `self` into a `Result` value.
-    ///
-    /// # Note
-    ///
-    /// - Non-`Result` values are converted to a `Result::Ok` value.
-    /// - `Result` values are forwarded as identity.
-    fn into_result(self) -> Result<Self::Val, E>;
-}
-
-macro_rules! impl_into_result_for {
-    ( $($ty:ty),* $(,)? ) => {
-        $(
-            impl<E> IntoResult<E> for $ty {
-                type Val = Self;
-
-                #[inline]
-                fn into_result(self) -> Result<Self, E> {
-                    Ok(self)
-                }
-            }
-        )*
-    };
-}
-impl_into_result_for! {
-    i32, i64,
-    u32, u64,
-    f32, f64,
-}
-
-impl<T, E> IntoResult<E> for Result<T, E> {
-    type Val = T;
-
-    #[inline]
-    fn into_result(self) -> Result<Self::Val, E> {
-        self
-    }
 }
 
 impl_unary_op_for! {
