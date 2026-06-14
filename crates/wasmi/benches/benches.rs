6cb7e87f04491d31770c351972e3b9dafbc4817e
Robin Freyler <robin.freyler@gmail.com>
2026-06-14 22:36:14 +0200

    Revert "make sure input and output is FASTA encoded"
    This reverts commit a18ae67b49c4a2668b6d5c09bacbbe736abd7fb2.

--- crates/wasmi/benches/benches.rs
+++ crates/wasmi/benches/benches.rs
@@ -1020,22 +1020,10 @@ fn bench_execute_reverse_complement(c: &mut Criterion) {
             .call(&mut store, data_ptr)
             .unwrap();
 
-        let [input, output]: [Box<[u8]>; 2] = {
-            // Make sure the input and output `.txt` files are properly FASTA
-            // encoded which requires line endings with LF and not CRLF.
-            //
-            // On Windows these files might be checked-out with CRLF new-line
-            // encoding which invalidates the benchmark computation.
-            let mut input = REVCOMP_INPUT.to_vec();
-            let mut output = REVCOMP_OUTPUT.to_vec();
-            input.retain(|b| *b != b'\r');
-            output.retain(|b| *b != b'\r');
-            [input.into(), output.into()]
-        };
         // Copy test data inside the wasm memory.
         let memory = instance.get_memory(&store, "memory").unwrap();
         memory
-            .write(&mut store, input_offset as usize, &input[..])
+            .write(&mut store, input_offset as usize, REVCOMP_INPUT)
             .unwrap();
 
         // Run the rev complement benchmark.
@@ -1051,11 +1039,11 @@ fn bench_execute_reverse_complement(c: &mut Criterion) {
             .call(&mut store, data_ptr)
             .unwrap();
 
-        let mut result: Box<[u8]> = vec![0x00_u8; output.len()].into();
+        let mut result = [0x00_u8; REVCOMP_OUTPUT.len()];
         memory
-            .read(&store, output_offset as usize, &mut result[..])
+            .read(&store, output_offset as usize, &mut result)
             .unwrap();
-        assert_eq!(result, output);
+        assert_eq!(&result[..], REVCOMP_OUTPUT);
 
         // Teardown benchmark data.
         instance
