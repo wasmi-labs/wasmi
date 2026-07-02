a765bd13cb310af9a71a17dbc9b6fb7f4b209f2e
Robin Freyler <robin.freyler@gmail.com>
2026-07-02 02:06:39 +0200

    remove now unused set_global helper

--- crates/wasmi/src/engine/executor/handler/utils.rs
+++ crates/wasmi/src/engine/executor/handler/utils.rs
@@ -12,7 +12,7 @@ use crate::{
     Table,
     TrapCode,
     V128,
-    core::{CoreElementSegment, CoreGlobal, CoreMemory, CoreTable, RawVal, ShiftAmount, WriteAs},
+    core::{CoreElementSegment, CoreGlobal, CoreMemory, CoreTable, RawVal, ShiftAmount},
     engine::{
         DedupFuncType,
         EngineFunc,
@@ -721,17 +721,6 @@ where
         return Err(TrapCode::BadSignature);
     }
     Ok(*func)
-}
-
-pub fn set_global<V>(global: index::Global, value: V, state: &mut VmState, instance: Inst)
-where
-    RawVal: WriteAs<V>,
-{
-    let global = fetch_global(instance, global);
-    let global = resolve_global_mut(state.store, &global);
-    let mut value_ptr = global.get_raw_ptr();
-    let global_ref = unsafe { value_ptr.as_mut() };
-    global_ref.write_as(value);
 }
 
 pub fn update_instance(
