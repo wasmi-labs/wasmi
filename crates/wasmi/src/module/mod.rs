af227edad7a52c668f6ea015695aa3c2afbf9322
Robin Freyler <robin.freyler@gmail.com>
2026-07-03 14:50:20 +0200

    use Display impl instead of Debug of ImportName

--- crates/wasmi/src/module/mod.rs
+++ crates/wasmi/src/module/mod.rs
@@ -474,26 +474,27 @@ impl<'a> Iterator for ModuleImportsIter<'a> {
             match imported {
                 Imported::Func(name) => {
                     let func_type = self.funcs.next().unwrap_or_else(|| {
-                        panic!("unexpected missing imported function for {name:?}")
+                        panic!("unexpected missing imported function for {name}")
                     });
                     let func_type = self.engine.resolve_func_type(func_type, FuncType::clone);
                     ImportType::new(name, func_type)
                 }
                 Imported::Table(name) => {
-                    let table_type = self.tables.next().unwrap_or_else(|| {
-                        panic!("unexpected missing imported table for {name:?}")
-                    });
+                    let table_type = self
+                        .tables
+                        .next()
+                        .unwrap_or_else(|| panic!("unexpected missing imported table for {name}"));
                     ImportType::new(name, *table_type)
                 }
                 Imported::Memory(name) => {
                     let memory_type = self.memories.next().unwrap_or_else(|| {
-                        panic!("unexpected missing imported linear memory for {name:?}")
+                        panic!("unexpected missing imported linear memory for {name}")
                     });
                     ImportType::new(name, *memory_type)
                 }
                 Imported::Global(name) => {
                     let global_type = self.globals.next().unwrap_or_else(|| {
-                        panic!("unexpected missing imported global variable for {name:?}")
+                        panic!("unexpected missing imported global variable for {name}")
                     });
                     ImportType::new(name, *global_type)
                 }
