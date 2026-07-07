2d4e4a7085652eb94a6e7bff11203b195516cdfe
Robin Freyler <robin.freyler@gmail.com>
2026-07-07 15:20:09 +0200

    fix yet another similar bug

--- crates/wasmi/src/engine/translator/func/simd/mod.rs
+++ crates/wasmi/src/engine/translator/func/simd/mod.rs
@@ -393,11 +393,9 @@ impl FuncTranslator {
                 return Ok(());
             }
             op => {
-                self.push_op_with_result_reg(
-                    <L as LoadOp>::Result::TY,
-                    op,
-                    FuelCostsProvider::load,
-                )?;
+                let fuel_pos = self.stack.fuel_pos();
+                self.instrs
+                    .encode_op(op, fuel_pos, FuelCostsProvider::load)?;
             }
         }
         let v128 = self.copy_operand_to_slot(v128)?;
