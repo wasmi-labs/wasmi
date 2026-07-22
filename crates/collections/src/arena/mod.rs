f09b21fe1efdc1bca9d30ec9ec028cbe6fdae068
Robin Freyler <robin.freyler@gmail.com>
2026-07-22 16:51:40 +0200

    move ArenaKey into its own submodule

--- crates/collections/src/arena/mod.rs
+++ crates/collections/src/arena/mod.rs
@@ -7,6 +7,7 @@
 mod component_vec;
 mod dedup;
 mod error;
+mod key;
 mod stable_arena;
 mod stable_vec;
 
@@ -14,6 +15,7 @@ pub use self::{
     component_vec::ComponentVec,
     dedup::DedupArena,
     error::ArenaError,
+    key::ArenaKey,
     stable_arena::{Iter as StableArenaIter, IterMut as StableArenaIterMut, StableArena},
     stable_vec::{Iter as StableVecIter, IterMut as StableVecIterMut, StableVec},
 };
@@ -21,83 +23,9 @@ use alloc::vec::Vec;
 use core::{
     iter::{self, Enumerate},
     marker::PhantomData,
-    num::NonZero,
     ops::{Index, IndexMut, Range},
     slice::{self, GetDisjointMutError},
 };
-
-/// Types that can be used as indices for arenas.
-pub trait ArenaKey: Copy {
-    /// Converts the [`ArenaKey`] into the underlying `usize` value.
-    ///
-    /// # Note
-    ///
-    /// - No checks need to be applied here since it is assumed that `ArenaKey`
-    ///   items are only valid if they can be converted into a valid `usize`.
-    /// - The `from_usize` constructor is supposed to make sure that this
-    ///   invariant is conserved.
-    fn into_usize(self) -> usize;
-
-    /// Converts the `usize` value into the associated [`ArenaKey`].
-    ///
-    /// Returns `None` if `Self` cannot represent `value`.
-    fn from_usize(value: usize) -> Option<Self>;
-}
-
-impl ArenaKey for usize {
-    #[inline]
-    fn into_usize(self) -> usize {
-        self
-    }
-
-    #[inline]
-    fn from_usize(value: usize) -> Option<Self> {
-        Some(value)
-    }
-}
-
-impl ArenaKey for u32 {
-    #[inline]
-    fn into_usize(self) -> usize {
-        // Note: there is no need to perform checks for the cast as those
-        //       have already been applied and implied in `from_usize`.
-        self as usize
-    }
-
-    #[inline]
-    fn from_usize(value: usize) -> Option<Self> {
-        u32::try_from(value).ok()
-    }
-}
-
-impl ArenaKey for NonZero<usize> {
-    #[inline]
-    fn into_usize(self) -> usize {
-        // Note: there is no need to perform checks for the cast as those
-        //       have already been applied and implied in `from_usize`.
-        self.get().wrapping_sub(1)
-    }
-
-    #[inline]
-    fn from_usize(value: usize) -> Option<Self> {
-        Self::new(value.wrapping_add(1))
-    }
-}
-
-impl ArenaKey for NonZero<u32> {
-    #[inline]
-    fn into_usize(self) -> usize {
-        // Note: there is no need to perform checks for the cast as those
-        //       have already been applied and implied in `from_usize`.
-        self.get().wrapping_sub(1) as usize
-    }
-
-    #[inline]
-    fn from_usize(value: usize) -> Option<Self> {
-        let value = u32::try_from(value).ok()?;
-        Self::new(value.wrapping_add(1))
-    }
-}
 
 /// An arena allocator with a given key and entity type.
 ///
