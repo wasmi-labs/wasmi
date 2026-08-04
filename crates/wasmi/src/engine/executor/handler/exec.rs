ad98c5a8a2fdddd0c3cbf13c4ffa9affb149bb7b
Robin Freyler <robin.freyler@gmail.com>
2026-08-04 20:00:48 +0200

    move return exec handler in front of call_{imported,indirect}

--- crates/wasmi/src/engine/executor/handler/exec.rs
+++ crates/wasmi/src/engine/executor/handler/exec.rs
@@ -192,6 +192,24 @@ global_set_execution_handler! {
 }
 
 execution_handler! {
+    fn r#return(
+        state: &mut VmState,
+        ip: Ip,
+        sp: Sp,
+        mem0: Mem0Ptr,
+        mem0_len: Mem0Len,
+        instance: Inst,
+        ireg: Ireg,
+        freg32: Freg32,
+        freg64: Freg64,
+    ) -> Done = {
+        let mut args = Args::from_parts(ip, sp, mem0, mem0_len, instance, ireg, freg32, freg64);
+        args.pop_frame(state)?;
+        dispatch!(state, args)
+    }
+}
+
+execution_handler! {
     fn call_internal(
         state: &mut VmState,
         ip: Ip,
@@ -346,24 +364,6 @@ return_call_indirect_execution_handler! {
     fn return_call_indirect_s(ReturnCallIndirect_S);
     fn return_call_indirect_table0_r(ReturnCallIndirectTable0_R);
     fn return_call_indirect_table0_s(ReturnCallIndirectTable0_S);
-}
-
-execution_handler! {
-    fn r#return(
-        state: &mut VmState,
-        ip: Ip,
-        sp: Sp,
-        mem0: Mem0Ptr,
-        mem0_len: Mem0Len,
-        instance: Inst,
-        ireg: Ireg,
-        freg32: Freg32,
-        freg64: Freg64,
-    ) -> Done = {
-        let mut args = Args::from_parts(ip, sp, mem0, mem0_len, instance, ireg, freg32, freg64);
-        args.pop_frame(state)?;
-        dispatch!(state, args)
-    }
 }
 
 execution_handler! {
