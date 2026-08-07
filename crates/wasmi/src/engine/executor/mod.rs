09a5730a99630566635840cc353b8bf1ce46e196
Robin Freyler <robin.freyler@gmail.com>
2026-08-07 15:17:17 +0200

    remove now unused EngineExecutor

--- crates/wasmi/src/engine/executor/mod.rs
+++ crates/wasmi/src/engine/executor/mod.rs
@@ -33,7 +33,6 @@ use crate::{
         executor::handler::{init_host_func_call, init_wasm_func_call},
         resumable::ResumableCallCommon,
     },
-    ir::SlotSpan,
 };
 use core::mem::{self, ManuallyDrop};
 
@@ -214,111 +213,6 @@ impl EngineInner {
         };
         self.stacks.lock().recycle(invocation.common.take_stack());
         Ok(ResumableCallBase::Finished(results))
-    }
-}
-
-/// The internal state of the Wasmi engine.
-#[derive(Debug)]
-pub struct EngineExecutor<'engine> {
-    /// The value and call stacks.
-    stack: &'engine mut Stack,
-}
-
-impl<'engine> EngineExecutor<'engine> {
-    /// Creates a new [`EngineExecutor`] for the given [`Stack`].
-    fn new(stack: &'engine mut Stack) -> Self {
-        Self { stack }
-    }
-
-    /// Executes the given [`Func`] using the given `params`.
-    ///
-    /// Stores the execution result into `results` upon a successful execution.
-    ///
-    /// # Errors
-    ///
-    /// - If the given `params` do not match the expected parameters of `func`.
-    /// - If the given `results` do not match the length of the expected results of `func`.
-    /// - When encountering a Wasm or host trap during the execution of `func`.
-    fn execute_root_func<T, Params, Results>(
-        &mut self,
-        store: &mut Store<T>,
-        func: &Func,
-        params: Params,
-        results: Results,
-    ) -> Result<Results::Value, ExecutionOutcome>
-    where
-        Params: LowerToCells,
-        Results: LiftFromCells,
-    {
-        self.stack.reset();
-        let results = match store.inner.resolve_func(func) {
-            FuncEntity::Wasm(wasm_func) => {
-                // We reserve space on the stack to write the results of the root function execution.
-                let instance = wasm_func.instance();
-                let func_entry = wasm_func.func_entry_ptr();
-                let call = init_wasm_func_call(store, func_entry, instance)?;
-                call.write_params(params).execute()?.write_results(results)
-            }
-            FuncEntity::Host(host_func) => {
-                // The host function signature is required for properly
-                // adjusting, inspecting and manipulating the value stack.
-                // In case the host function returns more values than it takes
-                // we are required to extend the value stack.
-                let host_func = *host_func;
-                let call = init_host_func_call(store, host_func)?;
-                call.write_params(params).execute()?.write_results(results)
-            }
-        };
-        Ok(results)
-    }
-
-    /// Resumes the execution of the given [`Func`] using `params` after a host function trapped.
-    ///
-    /// Stores the execution result into `results` upon a successful execution.
-    ///
-    /// # Errors
-    ///
-    /// - If the given `params` do not match the expected parameters of `func`.
-    /// - If the given `results` do not match the length of the expected results of `func`.
-    /// - When encountering a Wasm or host trap during the execution of `func`.
-    fn resume_func_host_trap<T, Params, Results>(
-        &mut self,
-        store: &mut Store<T>,
-        params: Params,
-        params_slots: SlotSpan,
-        results: Results,
-    ) -> Result<Results::Value, ExecutionOutcome>
-    where
-        Params: LowerToCells,
-        Results: LiftFromCells,
-    {
-        let value = resume_wasm_func_call(store)?
-            .provide_host_results(params, params_slots)
-            .execute()?
-            .write_results(results);
-        Ok(value)
-    }
-
-    /// Resumes the execution of the given [`Func`] using `params` after running out of fuel.
-    ///
-    /// Stores the execution result into `results` upon a successful execution.
-    ///
-    /// # Errors
-    ///
-    /// - If the given `results` do not match the length of the expected results of `func`.
-    /// - When encountering a Wasm or host trap during the execution of `func`.
-    fn resume_func_out_of_fuel<T, Results>(
-        &mut self,
-        store: &mut Store<T>,
-        results: Results,
-    ) -> Result<Results::Value, ExecutionOutcome>
-    where
-        Results: LiftFromCells,
-    {
-        let value = resume_wasm_func_call(store)?
-            .execute()?
-            .write_results(results);
-        Ok(value)
     }
 }
 
